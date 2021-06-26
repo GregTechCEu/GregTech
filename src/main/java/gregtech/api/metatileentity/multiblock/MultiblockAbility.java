f53a1cc3a94b579a993e1d270f62b5513de43a93
BraggestSage833 <46819052+BraggestSage833@users.noreply.github.com>
2021-06-26 00:43:22 -0400

    Update MultiblockAbility.java

--- src/main/java/gregtech/api/metatileentity/multiblock/MultiblockAbility.java
+++ src/main/java/gregtech/api/metatileentity/multiblock/MultiblockAbility.java
@@ -1,6 +1,7 @@
 package gregtech.api.metatileentity.multiblock;
 
 import gregtech.api.capability.IEnergyContainer;
+import gregtech.common.pipelike.laser.tile.LaserContainer;
 import net.minecraftforge.fluids.IFluidTank;
 import net.minecraftforge.items.IItemHandlerModifiable;
 
@@ -15,4 +16,6 @@ public class MultiblockAbility<T> {
     public static final MultiblockAbility<IEnergyContainer> INPUT_ENERGY = new MultiblockAbility<>();
     public static final MultiblockAbility<IEnergyContainer> OUTPUT_ENERGY = new MultiblockAbility<>();
 
+    public static MultiblockAbility<LaserContainer> INPUT_LASER = new MultiblockAbility();
+    public static MultiblockAbility<LaserContainer> OUTPUT_LASER= new MultiblockAbility();
 }
