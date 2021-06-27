c504c0f32bdefa61f253bf2d32363cffcf912437
BraggestSage833 <46819052+BraggestSage833@users.noreply.github.com>
2021-06-27 19:30:28 -0400

    Update MultiblockAbility.java

--- src/main/java/gregtech/api/metatileentity/multiblock/MultiblockAbility.java
+++ src/main/java/gregtech/api/metatileentity/multiblock/MultiblockAbility.java
@@ -16,6 +16,6 @@ public class MultiblockAbility<T> {
     public static final MultiblockAbility<IEnergyContainer> INPUT_ENERGY = new MultiblockAbility<>();
     public static final MultiblockAbility<IEnergyContainer> OUTPUT_ENERGY = new MultiblockAbility<>();
 
-    public static MultiblockAbility<LaserContainer> INPUT_LASER = new MultiblockAbility();
-    public static MultiblockAbility<LaserContainer> OUTPUT_LASER= new MultiblockAbility();
+    public static MultiblockAbility<LaserContainer> INPUT_LASER = new MultiblockAbility<>();
+    public static MultiblockAbility<LaserContainer> OUTPUT_LASER= new MultiblockAbility<>();
 }
