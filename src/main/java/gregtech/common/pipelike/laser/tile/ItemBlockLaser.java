7e5229ec2c7c6634e72e6ea344e0987aee6f5324
BraggestSage833 <46819052+BraggestSage833@users.noreply.github.com>
2021-06-27 20:38:40 -0400

    Update ItemBlockLaser.java

--- src/main/java/gregtech/common/pipelike/laser/tile/ItemBlockLaser.java
+++ src/main/java/gregtech/common/pipelike/laser/tile/ItemBlockLaser.java
@@ -2,7 +2,11 @@ package gregtech.common.pipelike.laser.tile;
 import gregtech.api.GTValues;
 import gregtech.api.pipenet.block.BlockPipe;
 import gregtech.api.pipenet.block.ItemBlockPipe;
+import gregtech.api.pipenet.block.material.BlockMaterialPipe;
+import gregtech.api.pipenet.block.material.ItemBlockMaterialPipe;
 import gregtech.api.unification.ore.OrePrefix;
+import gregtech.api.util.GTUtility;
+import gregtech.common.pipelike.laser.net.BlockLaser;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.ItemStack;
@@ -11,26 +15,21 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import javax.annotation.Nullable;
 import java.util.List;
-public class ItemBlockLaser extends ItemBlockPipe<LaserSize, LaserProperties>  {
+public class ItemBlockLaser extends ItemBlockMaterialPipe<LaserSize, LaserProperties> {
 
-    public ItemBlockLaser(BlockPipe block) {
+
+    public ItemBlockLaser(BlockLaser block) {
         super(block);
     }
 
-    @SideOnly(Side.CLIENT)
-    public String getItemStackDisplayName(ItemStack stack) {
-        LaserSize pipeType = blockPipe.getItemPipeType(stack);
-        OrePrefix orePrefix = pipeType.getOrePrefix();
-        String specfiedUnlocalized = "item.oreprefix." + orePrefix.name();
-        return I18n.format(specfiedUnlocalized);
-    }
 
     @Override
     @SideOnly(Side.CLIENT)
     public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
-        LaserProperties opticalFiberProperties = blockPipe.createItemProperties(stack);
-       // String voltageName = String.valueOf(GTValues.V[opticalFiberProperties.laserVoltage]);
-        tooltip.add(I18n.format("laser_voltage", opticalFiberProperties.laserVoltage));
-        tooltip.add(I18n.format("laser_parallel", opticalFiberProperties.parallel));
+        LaserProperties wireProperties = blockPipe.createItemProperties(stack);
+        String voltageName = GTValues.VN[GTUtility.getTierByVoltage(wireProperties.laserVoltage)];
+        tooltip.add(I18n.format("gregtech.cable.voltage", wireProperties.laserVoltage, voltageName));
+        tooltip.add(I18n.format("gregtech.cable.amperage", wireProperties.parallel));
+
     }
 }
