88d341976c420cd1daf11e342a834d6413bd6889
Tech22 <swarren2022@gmail.com>
2021-06-30 04:15:30 -0400

    start work on recipe division for distilleries

--- src/main/java/gregtech/api/recipes/builders/UniversalDistillationRecipeBuilder.java
+++ src/main/java/gregtech/api/recipes/builders/UniversalDistillationRecipeBuilder.java
@@ -4,6 +4,7 @@ import gregtech.api.recipes.Recipe;
 import gregtech.api.recipes.RecipeBuilder;
 import gregtech.api.recipes.RecipeMap;
 import gregtech.api.recipes.RecipeMaps;
+import gregtech.api.util.GTUtility;
 import gregtech.api.util.ValidationResult;
 import net.minecraftforge.fluids.FluidStack;
 
@@ -28,12 +29,20 @@ public class UniversalDistillationRecipeBuilder extends RecipeBuilder<UniversalD
     @Override
     public void buildAndRegister() {
         IntCircuitRecipeBuilder builder = RecipeMaps.DISTILLERY_RECIPES.recipeBuilder()
-                .fluidInputs(this.fluidInputs.toArray(new FluidStack[0]))
-                .duration(this.duration * 2)
                 .EUt(this.EUt / 4);
 
+        //todo do this on a per-output basis
+        int ratio = GTUtility.getRatioForDistillation(this.fluidInputs, this.fluidOutputs, this.outputs);
+
         for (int i = 0; i < fluidOutputs.size(); i++) {
-            builder.copy().circuitMeta(i).fluidOutputs(this.fluidOutputs.get(i)).buildAndRegister();
+            builder.copy()
+                    .circuitMeta(i + 1)
+                    .fluidInputs(new FluidStack(this.fluidInputs.get(0), this.fluidInputs.get(0).amount / ratio)) //todo fluid inputs must not be reduced to smaller than 25mB
+                    .fluidOutputs(new FluidStack(this.fluidOutputs.get(i), this.fluidOutputs.get(i).amount / ratio))
+                    .duration(this.EUt > 16 ? (int) (this.duration * 2.8f / ratio) : this.duration * 2 / ratio)
+                    //todo figure out how to do outputs without creating broken recipes
+                    .outputs(this.outputs)
+                    .buildAndRegister();
         }
 
         super.buildAndRegister();
