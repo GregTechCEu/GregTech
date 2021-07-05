54a0dd0060bf19dd40ca8df67304b47d85d8ae1b
htmlcsjs <46023024+htmlcsjs@users.noreply.github.com>
2021-07-05 18:27:22 +0100

    Combine with master

--- src/main/java/gregtech/loaders/recipe/chemistry/LCRCombined.java
+++ src/main/java/gregtech/loaders/recipe/chemistry/LCRCombined.java
@@ -10,20 +10,21 @@ public class LCRCombined {
 
     static void init() {
         LARGE_CHEMICAL_RECIPES.recipeBuilder()
-            .fluidInputs(Epichlorohydrin.getFluid(1000))
-            .fluidInputs(Phenol.getFluid(2000))
-            .fluidInputs(Acetone.getFluid(1000))
-            .fluidInputs(HydrochloricAcid.getFluid(1000))
-            .input(dust, SodiumHydroxide, 3)
-            .fluidOutputs(Epoxy.getFluid(1000))
-            .fluidOutputs(SaltWater.getFluid(1000))
-            .fluidOutputs(DilutedHydrochloricAcid.getFluid(1000))
-            .notConsumable(new IntCircuitIngredient(27))
-            .EUt(30)
-            .duration(24*20)
-            .buildAndRegister();
+                .notConsumable(new IntCircuitIngredient(27))
+                .fluidInputs(Epichlorohydrin.getFluid(1000))
+                .fluidInputs(Phenol.getFluid(2000))
+                .fluidInputs(Acetone.getFluid(1000))
+                .fluidInputs(HydrochloricAcid.getFluid(1000))
+                .input(dust, SodiumHydroxide, 3)
+                .fluidOutputs(Epoxy.getFluid(1000))
+                .fluidOutputs(SaltWater.getFluid(1000))
+                .fluidOutputs(DilutedHydrochloricAcid.getFluid(1000))
+                .EUt(30)
+                .duration(24*20)
+                .buildAndRegister();
 
         LARGE_CHEMICAL_RECIPES.recipeBuilder()
+                .notConsumable(new IntCircuitIngredient(27))
                 .fluidInputs(Chlorine.getFluid(2000))
                 .fluidInputs(Propene.getFluid(1000))
                 .fluidInputs(HypochlorousAcid.getFluid(1000))
@@ -31,9 +32,29 @@ public class LCRCombined {
                 .fluidOutputs(HydrochloricAcid.getFluid(1000))
                 .fluidOutputs(Epichlorohydrin.getFluid(1000))
                 .fluidOutputs(SaltWater.getFluid(1000))
-                .notConsumable(new IntCircuitIngredient(27))
                 .EUt(30)
                 .duration(24*20)
+                .buildAndRegister();
+
+        LARGE_CHEMICAL_RECIPES.recipeBuilder()
+                .notConsumable(new IntCircuitIngredient(27))
+                .fluidInputs(Methane.getFluid(3000))
+                .fluidInputs(Nitrogen.getFluid(4000))
+                .fluidInputs(Oxygen.getFluid(3000))
+                .fluidOutputs(Ammonia.getFluid(4000))
+                .fluidOutputs(CarbonMonoxide.getFluid(3000))
+                .EUt(30)
+                .duration(320)
+                .buildAndRegister();
+
+        LARGE_CHEMICAL_RECIPES.recipeBuilder()
+                .notConsumable(new IntCircuitIngredient(27))
+                .fluidInputs(Hydrogen.getFluid(6000))
+                .fluidInputs(CarbonDioxide.getFluid(1000))
+                .fluidOutputs(Methane.getFluid(1000))
+                .fluidOutputs(Water.getFluid(1000))
+                .EUt(30)
+                .duration(160)
                 .buildAndRegister();
     }
 }
