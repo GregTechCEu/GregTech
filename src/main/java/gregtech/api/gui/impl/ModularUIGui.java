ddde8c5575d40d4f31e1fa2c1cd92cf546b3ab3e
bruberu <80226372+bruberu@users.noreply.github.com>
2021-07-17 21:26:32 -0500

    Fix weirdness with tinted GUIs, and un-revert

--- src/main/java/gregtech/api/gui/impl/ModularUIGui.java
+++ src/main/java/gregtech/api/gui/impl/ModularUIGui.java
@@ -27,9 +27,9 @@ import java.io.IOException;
 public class ModularUIGui extends GuiContainer implements IRenderContext {
 
     private final ModularUI modularUI;
-    private static float rColorForOverlay = ((ConfigHolder.U.GT5u.defaultPaintingColor >> 16) & 0xff) / 255.0F;
-    private static float gColorForOverlay = ((ConfigHolder.U.GT5u.defaultPaintingColor >> 8) & 0xff) / 255.0F;
-    private static float bColorForOverlay = (ConfigHolder.U.GT5u.defaultPaintingColor & 0xff) / 255.0F;
+    public static float rColorForOverlay = ((ConfigHolder.U.GT5u.defaultPaintingColor >> 16) & 0xff) / 255.0F;
+    public static float gColorForOverlay = ((ConfigHolder.U.GT5u.defaultPaintingColor >> 8) & 0xff) / 255.0F;
+    public static float bColorForOverlay = (ConfigHolder.U.GT5u.defaultPaintingColor & 0xff) / 255.0F;
 
     public ModularUI getModularUI() {
         return modularUI;
