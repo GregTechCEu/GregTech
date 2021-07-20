GregTechCEu/GregTech

commit b03b8f11897bb8fcf580ef5063f9dd4ec5d0a57c

Fix misc issues

src/main/java/gregtech/api/pipenet/block/material/BlockMaterialPipe.java
@@ -44,13 +44,13 @@ public abstract class BlockMaterialPipe<PipeType extends Enum<PipeType> & IPipeT
     public PipeType getItemPipeType(ItemStack itemStack) {
         GTLog.logger.info(itemStack.getMetadata());
         if (itemStack.getMetadata() < 32000) {
-            return getPipeTypeClass().getEnumConstants()[itemStack.getMetadata() / 1000];
+            return getPipeTypeClass().getEnumConstants()[itemStack.getMetadata()];
         }
         return getPipeTypeClass().getEnumConstants()[0];
     }
 
     public Material getItemMaterial(ItemStack itemStack) {
-        return Material.MATERIAL_REGISTRY.getObjectById(itemStack.getMetadata() % 1000);
+        return Material.MATERIAL_REGISTRY.getObjectById(itemStack.getMetadata());
     }
 
     @Override

src/main/java/gregtech/api/unification/ore/OrePrefix.java
@@ -349,7 +349,7 @@ public enum OrePrefix {
 
     public byte maxStackSize = 64;
     public final List<MaterialStack> secondaryMaterials = new ArrayList<>();
-    public float heatDamage = 0.0F; // Negative for Frost Damage;
+    public float heatDamage = 0.0F; // Negative for Frost Damage
 
     OrePrefix(String categoryName, long materialAmount, IMaterial<?> material, MaterialIconType materialIconType, long flags, Predicate<Material> condition) {
         this.categoryName = categoryName;

commit 5be9aa763b8aacce8693cb1a929563fd4a6602b9

Fix crash caused by b03b8f1

src/main/java/gregtech/api/pipenet/block/material/BlockMaterialPipe.java
@@ -44,7 +44,7 @@ public abstract class BlockMaterialPipe<PipeType extends Enum<PipeType> & IPipeT
     public PipeType getItemPipeType(ItemStack itemStack) {
         GTLog.logger.info(itemStack.getMetadata());
         if (itemStack.getMetadata() < 32000) {
-            return getPipeTypeClass().getEnumConstants()[itemStack.getMetadata()];
+            return getPipeTypeClass().getEnumConstants()[itemStack.getMetadata() / 1000];
         }
         return getPipeTypeClass().getEnumConstants()[0];
     }