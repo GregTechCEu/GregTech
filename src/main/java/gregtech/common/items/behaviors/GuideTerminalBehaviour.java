899515f597cd29efe798ff5fb7bdf6cddf3fd71e
KilaBash <yefancy@foxmail.com>
2021-07-22 23:14:09 +0800

    add item Terminal

---
+++ src/main/java/gregtech/common/items/behaviors/GuideTerminalBehaviour.java
@@ -0,0 +1,12 @@
+package gregtech.common.items.behaviors;
+
+import gregtech.api.items.metaitem.stats.IItemBehaviour;
+import net.minecraft.item.ItemStack;
+
+import java.util.List;
+
+public class GuideTerminalBehaviour implements IItemBehaviour {
+    @Override
+    public void addInformation(ItemStack itemStack, List<String> lines) {
+    }
+}
