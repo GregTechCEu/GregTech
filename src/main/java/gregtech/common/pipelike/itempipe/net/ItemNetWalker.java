81ece2d39cb9763f91f02a727b1a2d64d943a18b
brachy84 <brachy84@gmail.com>
2021-07-22 21:04:59 +0200

    clean up walker

--- src/main/java/gregtech/common/pipelike/itempipe/net/ItemNetWalker.java
+++ src/main/java/gregtech/common/pipelike/itempipe/net/ItemNetWalker.java
@@ -1,7 +1,6 @@
 package gregtech.common.pipelike.itempipe.net;
 
 import gregtech.api.pipenet.Node;
-import gregtech.api.util.GTLog;
 import gregtech.common.covers.CoverConveyor;
 import gregtech.common.pipelike.itempipe.ItemPipeProperties;
 import gregtech.common.pipelike.itempipe.tile.TileEntityItemPipe;
@@ -11,27 +10,20 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
-import org.apache.logging.log4j.Logger;
 
 import java.util.*;
 
 public class ItemNetWalker {
 
     public static List<ItemPipeNet.Inventory> createNetData(ItemPipeNet net, World world, BlockPos sourcePipe) {
-        log.info("------------------- creating net data for {} --------------------", sourcePipe);
         ItemNetWalker walker = new ItemNetWalker(net, world, sourcePipe, 1, new ArrayList<>(), null);
-        int i = 0;
-        while (!walker.walk() && i++ < 50) ;
+        while (!walker.walk()) ;
         walker.walked.forEach(TileEntityItemPipe::resetWalk);
-        log.info("------------------- returning {} invs --------------------", walker.inventories.size());
         return walker.inventories;
     }
 
-    private static final Logger log = GTLog.logger;
-
     private final ItemPipeNet net;
     private final World world;
-    private final BlockPos sourcePipe;
     private final List<EnumFacing> pipes = new ArrayList<>();
     private List<ItemNetWalker> walkers;
 
@@ -46,7 +38,6 @@ public class ItemNetWalker {
     protected ItemNetWalker(ItemPipeNet net, World world, BlockPos sourcePipe, int distance, List<ItemPipeNet.Inventory> inventories, ItemPipeProperties properties) {
         this.world = world;
         this.net = net;
-        this.sourcePipe = sourcePipe;
         this.distance = distance;
         this.inventories = inventories;
         this.currentPos = sourcePipe;
@@ -54,10 +45,8 @@ public class ItemNetWalker {
     }
 
     private boolean walk() {
-        if (walkers == null) {
-            log.info("Walking at {}", currentPos);
+        if (walkers == null)
             checkPos(currentPos);
-        }
 
         if (pipes.size() == 0)
             return true;
@@ -95,7 +84,7 @@ public class ItemNetWalker {
         if (!(thisPipe instanceof TileEntityItemPipe))
             return;
         ItemPipeProperties pipeProperties = ((TileEntityItemPipe) thisPipe).getNodeData();
-        if(minProperties == null)
+        if (minProperties == null)
             minProperties = pipeProperties;
         else
             minProperties = new ItemPipeProperties(minProperties.resistance + pipeProperties.resistance, Math.min(minProperties.transferRate, pipeProperties.transferRate));
@@ -110,17 +99,13 @@ public class ItemNetWalker {
             TileEntity tile = world.getTileEntity(pos.offset(accessSide));
             if (tile == null) continue;
             if (tile instanceof TileEntityItemPipe) {
-                if (!((TileEntityItemPipe) tile).isWalked()) {
-                    log.info("   - found pipe at " + accessSide);
+                if (!((TileEntityItemPipe) tile).isWalked())
                     pipes.add(accessSide);
-                }
                 continue;
             }
             IItemHandler handler = tile.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, accessSide);
-            if (handler != null) {
-                log.info("   - found handler at {}, with facing {}", pos.offset(accessSide), accessSide);
+            if (handler != null)
                 inventories.add(new ItemPipeNet.Inventory(pos, accessSide, distance, minProperties));
-            }
         }
     }
 }
