28ba5669e9afda7f434c2c2eea3090315e4e8706
KilaBash <yefancy@foxmail.com>
2021-07-22 23:14:09 +0800

    add item Terminal

--- src/main/java/gregtech/common/items/MetaItems.java
+++ src/main/java/gregtech/common/items/MetaItems.java
@@ -416,6 +416,7 @@ public final class MetaItems {
     public static MetaItem<?>.MetaValueItem NANO_SABER;
     public static MetaItem<?>.MetaValueItem ENERGY_FIELD_PROJECTOR;
     public static MetaItem<?>.MetaValueItem SCANNER;
+    public static MetaItem<?>.MetaValueItem GUIDE_TERMINAL;
 
     public static MetaItem<?>.MetaValueItem[] DYE_ONLY_ITEMS = new MetaItem.MetaValueItem[EnumDyeColor.values().length];
     public static MetaItem<?>.MetaValueItem[] SPRAY_CAN_DYES = new MetaItem.MetaValueItem[EnumDyeColor.values().length];
