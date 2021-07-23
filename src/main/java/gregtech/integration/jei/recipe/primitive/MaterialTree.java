1574680908b0f4f46e2ab50d9df8baffd037ee8f
kumquat-ir <66188216+kumquat-ir@users.noreply.github.com>
2021-07-23 14:36:01 -0400

    More material info, revert to DustMaterial, remove fluid containers, better layout

--- src/main/java/gregtech/integration/jei/recipe/primitive/MaterialTree.java
+++ src/main/java/gregtech/integration/jei/recipe/primitive/MaterialTree.java
@@ -2,16 +2,13 @@ package gregtech.integration.jei.recipe.primitive;
 
 import com.google.common.collect.ImmutableList;
 import gregtech.api.unification.OreDictUnifier;
-import gregtech.api.unification.material.type.FluidMaterial;
+import gregtech.api.unification.material.type.DustMaterial;
+import gregtech.api.unification.material.type.IngotMaterial;
 import gregtech.api.unification.ore.OrePrefix;
-import gregtech.api.util.GTLog;
-import gregtech.integration.jei.GTJeiPlugin;
 import mezz.jei.api.ingredients.IIngredients;
 import mezz.jei.api.recipe.IRecipeWrapper;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
-import net.minecraftforge.fluids.FluidUtil;
-import net.minecraftforge.fluids.capability.IFluidHandlerItem;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -49,9 +46,15 @@ public class MaterialTree implements IRecipeWrapper {
 
 	private final List<List<ItemStack>> itemInputs = new ArrayList<>();
 	private final List<List<FluidStack>> fluidInputs = new ArrayList<>();
-	private final List<ItemStack> outputs = new ArrayList<>();
 
-	public MaterialTree(FluidMaterial material) {
+	private final String name;
+	private final String formula;
+	private final String blastTemp;
+	private final long avgM;
+	private final long avgP;
+	private final long avgN;
+
+	public MaterialTree(DustMaterial material) {
 		List<ItemStack> inputDusts = new ArrayList<>();
 		for (OrePrefix prefix : PREFIXES) {
 			inputDusts.add(OreDictUnifier.get(prefix, material));
@@ -64,25 +67,21 @@ public class MaterialTree implements IRecipeWrapper {
 
 		FluidStack matFluid = material.getFluid(1000);
 		if (matFluid != null) {
-			GTLog.logger.info("gof	fluid" + matFluid.getUnlocalizedName());
 			List<FluidStack> matFluidsStack = new ArrayList<>();
 			matFluidsStack.add(matFluid);
 			this.fluidInputs.add(matFluidsStack);
+		}
 
-			//fluid containers, code mostly from EnderIO
-			List<ItemStack> filledContainers = new ArrayList<>();
-			for (ItemStack stack : GTJeiPlugin.ingredientRegistry.getIngredients(ItemStack.class)) {
-				ItemStack drainedStack = stack.copy();
-				IFluidHandlerItem fluidHandler = FluidUtil.getFluidHandler(drainedStack);
-				if (fluidHandler != null) {
-					FluidStack drain = fluidHandler.drain(1000, true);
-					drainedStack = fluidHandler.getContainer();
-					if (drain != null && drain.amount > 0 && drain.isFluidEqual(matFluid)) {
-						filledContainers.add(stack);
-					}
-				}
-			}
-			this.itemInputs.add(filledContainers);
+		name = material.getLocalizedName();
+		formula = material.getChemicalFormula();
+		avgM = material.getAverageMass();
+		avgP = material.getAverageProtons();
+		avgN = material.getAverageNeutrons();
+		if (material instanceof IngotMaterial) {
+			blastTemp = String.valueOf(((IngotMaterial) material).blastFurnaceTemperature);
+		}
+		else {
+			blastTemp = "N/A";
 		}
 	}
 
@@ -90,9 +89,35 @@ public class MaterialTree implements IRecipeWrapper {
     public void getIngredients(IIngredients ingredients) {
 		ingredients.setInputLists(ItemStack.class, this.itemInputs);
 		ingredients.setInputLists(FluidStack.class, this.fluidInputs);
+		ingredients.setOutputLists(ItemStack.class, this.itemInputs);
+		ingredients.setOutputLists(FluidStack.class, this.fluidInputs);
     }
 
     public int getPrefixListSize(){
 		return PREFIXES.size();
+	}
+
+	public String getMaterialName() {
+		return name;
+	}
+
+	public String getMaterialFormula() {
+		return formula;
+	}
+
+	public long getAvgM() {
+		return avgM;
+	}
+
+	public long getAvgP() {
+		return avgP;
+	}
+
+	public long getAvgN() {
+		return avgN;
+	}
+
+	public String getBlastTemp() {
+		return blastTemp;
 	}
 }
