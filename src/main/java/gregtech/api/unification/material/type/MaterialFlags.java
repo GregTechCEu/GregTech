f8908e8ffe6637bc5160ce746ba70538aeadbad1
DStrand1 <dane@strandboge.com>
2021-07-30 20:42:50 -0500

    remove some irrelevant flags

--- src/main/java/gregtech/api/unification/material/type/MaterialFlags.java
+++ src/main/java/gregtech/api/unification/material/type/MaterialFlags.java
@@ -66,107 +66,83 @@ public class MaterialFlags {
     /**
      * Add to material if it is some kind of explosive
      */
-    public static final long EXPLOSIVE = createFlag(4);
+    public static final long EXPLOSIVE = createFlag(4); // base
 
     /**
      * Add to material to disable it's unification fully
      * todo implement
      */
-    public static final long NO_UNIFICATION = createFlag(5);
-
-    /**
-     * Add to material if any of it's items cannot be recycled to get scrub
-     */
-    public static final long NO_RECYCLING = createFlag(6);
-
-    /**
-     * Add to material if it has constantly burning aura
-     */
-    public static final long BURNING = createFlag(7);
+    public static final long NO_UNIFICATION = createFlag(5); // base
 
     /**
      * Decomposition recipe requires hydrogen as additional input. Amount is equal to input amount
      */
-    public static final long DECOMPOSITION_REQUIRES_HYDROGEN = createFlag(8);
+    public static final long DECOMPOSITION_REQUIRES_HYDROGEN = createFlag(8); // base
 
     /**
      * Enables electrolyzer decomposition recipe generation
      */
-    public static final long DECOMPOSITION_BY_ELECTROLYZING = createFlag(40);
+    public static final long DECOMPOSITION_BY_ELECTROLYZING = createFlag(40); // base
 
     /**
      * Enables centrifuge decomposition recipe generation
      */
-    public static final long DECOMPOSITION_BY_CENTRIFUGING = createFlag(41);
+    public static final long DECOMPOSITION_BY_CENTRIFUGING = createFlag(41); // base
 
     /**
      * Add to material if it is some kind of flammable
      */
-    public static final long FLAMMABLE = createFlag(42);
+    public static final long FLAMMABLE = createFlag(42); // base
 
     /**
      * Disables decomposition recipe generation for this material and all materials that has it as component
      */
-    public static final long DISABLE_DECOMPOSITION = createFlag(43);
+    public static final long DISABLE_DECOMPOSITION = createFlag(43); // base
 
     /**
      * Add to material if it cannot be worked by any other means, than smashing or smelting. This is used for coated Materials.
      */
-    public static final long NO_WORKING = createFlag(13);
+    public static final long NO_WORKING = createFlag(13); // ingot
     /**
      * Add to material if it cannot be used for regular Metal working techniques since it is not possible to bend it.
      */
-    public static final long NO_SMASHING = createFlag(14);
+    public static final long NO_SMASHING = createFlag(14); // ingot
 
     /**
      * Add to material if it's impossible to smelt it
      */
-    public static final long NO_SMELTING = createFlag(15);
-
-    /**
-     * Add to material if it melts into fluid (and it will also generate fluid for this material)
-     */
-    public static final long SMELT_INTO_FLUID = createFlag(17);
+    public static final long NO_SMELTING = createFlag(15); // ingot
 
     /**
      * This will prevent material from creating Shapeless recipes for dust to block and vice versa
      * Also preventing extruding and alloy smelting recipes via SHAPE_EXTRUDING/MOLD_BLOCK
      */
-    public static final long EXCLUDE_BLOCK_CRAFTING_RECIPES = createFlag(18);
+    public static final long EXCLUDE_BLOCK_CRAFTING_RECIPES = createFlag(18); // dust
 
-    public static final long EXCLUDE_PLATE_COMPRESSOR_RECIPE = createFlag(19);
+    public static final long EXCLUDE_PLATE_COMPRESSOR_RECIPE = createFlag(19); // dust
 
     /**
      * This will prevent material from creating Shapeless recipes for dust to block and vice versa
      */
-    public static final long EXCLUDE_BLOCK_CRAFTING_BY_HAND_RECIPES = createFlag(46);
-
-    /**
-     * Marks material state as gas
-     * Examples: Air, Argon, Refinery Gas, Oxygen, Hydrogen
-     * todo remove
-     */
-    public static final long STATE_GAS = createFlag(10);
+    public static final long EXCLUDE_BLOCK_CRAFTING_BY_HAND_RECIPES = createFlag(46); // dust
 
     /**
      * If this material can be crystallized.
      */
-    public static final long CRYSTALLIZABLE = createFlag(34);
+    public static final long CRYSTALLIZABLE = createFlag(34); // gem
 
-    public static final long HIGH_SIFTER_OUTPUT = createFlag(38);
+    public static final long HIGH_SIFTER_OUTPUT = createFlag(38); // ore
 
     /**
      * Add this to your Material if you want to have its Ore Calcite heated in a Blast Furnace for more output. Already listed are:
      * Iron, Pyrite, PigIron, WroughtIron.
      */
-    public static final long BLAST_FURNACE_CALCITE_DOUBLE = createFlag(35);
-    public static final long BLAST_FURNACE_CALCITE_TRIPLE = createFlag(36);
+    public static final long BLAST_FURNACE_CALCITE_DOUBLE = createFlag(35); // ingot
+    public static final long BLAST_FURNACE_CALCITE_TRIPLE = createFlag(36); // ingot
 
-    public static final long MORTAR_GRINDABLE = createFlag(24);
+    public static final long MORTAR_GRINDABLE = createFlag(24); // dust, and [ingot or gem]
 
     // TODO Generation Flags TODO
-
-    public static final long GENERATE_ORE = createFlag(11); // todo remove
 
     /**
      * Generate a plate for this material
@@ -174,37 +150,23 @@ public class MaterialFlags {
      * If it's metal material, bending machine recipes will be generated
      * If block is found, cutting machine recipe will be also generated
      */
-    public static final long GENERATE_PLATE = createFlag(12);
+    public static final long GENERATE_PLATE = createFlag(12); // dust
 
-    /**
-     * Whenever system should generate fluid block for this fluid material
-     * Renamed to GENERATE_FLUID_BLOCK to avoid confusion with dust blocks
-     * todo remove
-     */
-    public static final long GENERATE_FLUID_BLOCK = createFlag(44);
-    /**
-     * Add this flag to enable plasma generation for this material
-     * todo remove
-     */
-    public static final long GENERATE_PLASMA = createFlag(9);
-
-
-    public static final long GENERATE_LENS = createFlag(37);
-    public static final long GENERATE_FOIL = createFlag(25);
-    public static final long GENERATE_BOLT_SCREW = createFlag(26);
-    public static final long GENERATE_RING = createFlag(27);
-    public static final long GENERATE_SPRING = createFlag(28);
-    public static final long GENERATE_FINE_WIRE = createFlag(29);
-    public static final long GENERATE_ROTOR = createFlag(30);
-    public static final long GENERATE_SMALL_GEAR = createFlag(31);
-    public static final long GENERATE_DENSE = createFlag(32);
-    public static final long GENERATE_SPRING_SMALL = createFlag(33);
-    public static final long GENERATE_ROUND = createFlag(51);
-    public static final long GENERATE_DOUBLE_PLATE = createFlag(52);
-    public static final long GENERATE_ROD = createFlag(20);
-    public static final long GENERATE_GEAR = createFlag(21);
-    public static final long GENERATE_LONG_ROD = createFlag(22);
-    public static final long GENERATE_FRAME = createFlag(45);
+    public static final long GENERATE_LENS = createFlag(37); // gem
+    public static final long GENERATE_FOIL = createFlag(25); // ingot
+    public static final long GENERATE_BOLT_SCREW = createFlag(26); // ingot
+    public static final long GENERATE_RING = createFlag(27); // ingot
+    public static final long GENERATE_SPRING = createFlag(28); // ingot
+    public static final long GENERATE_FINE_WIRE = createFlag(29); // wire? ingot?
+    public static final long GENERATE_ROTOR = createFlag(30); // ingot
+    public static final long GENERATE_SMALL_GEAR = createFlag(31); // ingot
+    public static final long GENERATE_DENSE = createFlag(32); // ingot
+    public static final long GENERATE_SPRING_SMALL = createFlag(33); // ingot
+    public static final long GENERATE_ROUND = createFlag(51); // ingot
+    public static final long GENERATE_ROD = createFlag(20); // dust
+    public static final long GENERATE_GEAR = createFlag(21); // ingot
+    public static final long GENERATE_LONG_ROD = createFlag(22); // ingot
+    public static final long GENERATE_FRAME = createFlag(45); // dust
 
     static {
         registerMaterialFlagsHolder(Material.MatFlags.class, Material.class);
