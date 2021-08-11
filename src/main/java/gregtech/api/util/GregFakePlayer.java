60b409ed1ba5452281a9fcc8bf5dab4ad086a72f
bruberu <80226372+bruberu@users.noreply.github.com>
2021-08-10 21:37:52 -0500

    Reformat GregFakePlayer

--- src/main/java/gregtech/api/util/GregFakePlayer.java
+++ src/main/java/gregtech/api/util/GregFakePlayer.java
@@ -36,24 +36,71 @@ public class GregFakePlayer extends EntityPlayer {
         super(worldIn, GREGTECH);
     }
 
-    @Override public boolean isSpectator() {
+    @Override
+    public boolean isSpectator() {
         return false;
     }
-    @Override public boolean isCreative() {
+
+    @Override
+    public boolean isCreative() {
         return false;
     }
-    @Override public Vec3d getPositionVector(){ return new Vec3d(0, 0, 0); }
-    @Override public boolean canUseCommand(int i, String s){ return false; }
-    @Override public void sendStatusMessage(ITextComponent chatComponent, boolean actionBar){}
-    @Override public void sendMessage(ITextComponent component) {}
-    @Override public void addStat(StatBase par1StatBase, int par2){}
-    @Override public void openGui(Object mod, int modGuiId, World world, int x, int y, int z){}
-    @Override public boolean isEntityInvulnerable(DamageSource source){ return true; }
-    @Override public boolean canAttackPlayer(EntityPlayer player){ return false; }
-    @Override public void onDeath(DamageSource source){ return; }
-    @Override public void onUpdate(){ return; }
-    @Override public Entity changeDimension(int dim, ITeleporter teleporter){ return this; }
-    @Override public MinecraftServer getServer() { return FMLCommonHandler.instance().getMinecraftServerInstance(); }
+
+    @Override
+    public Vec3d getPositionVector() {
+        return new Vec3d(0, 0, 0);
+    }
+
+    @Override
+    public boolean canUseCommand(int i, String s) {
+        return false;
+    }
+
+    @Override
+    public void sendStatusMessage(ITextComponent chatComponent, boolean actionBar) {
+    }
+
+    @Override
+    public void sendMessage(ITextComponent component) {
+    }
+
+    @Override
+    public void addStat(StatBase par1StatBase, int par2) {
+    }
+
+    @Override
+    public void openGui(Object mod, int modGuiId, World world, int x, int y, int z) {
+    }
+
+    @Override
+    public boolean isEntityInvulnerable(DamageSource source) {
+        return true;
+    }
+
+    @Override
+    public boolean canAttackPlayer(EntityPlayer player) {
+        return false;
+    }
+
+    @Override
+    public void onDeath(DamageSource source) {
+        return;
+    }
+
+    @Override
+    public void onUpdate() {
+        return;
+    }
+
+    @Override
+    public Entity changeDimension(int dim, ITeleporter teleporter) {
+        return this;
+    }
+
+    @Override
+    public MinecraftServer getServer() {
+        return FMLCommonHandler.instance().getMinecraftServerInstance();
+    }
 
 
 }
