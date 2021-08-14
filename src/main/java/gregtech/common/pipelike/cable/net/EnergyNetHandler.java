dc86e76fd4821fa10be1df49ca3dca7ca740ec82
brachy84 <brachy84@gmail.com>
2021-08-14 22:48:56 +0200

    cable burning

--- src/main/java/gregtech/common/pipelike/cable/net/EnergyNetHandler.java
+++ src/main/java/gregtech/common/pipelike/cable/net/EnergyNetHandler.java
@@ -6,7 +6,12 @@ import gregtech.api.util.GTUtility;
 import gregtech.common.pipelike.cable.tile.TileEntityCable;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 
@@ -44,8 +49,10 @@ public class EnergyNetHandler implements IEnergyContainer {
             long amps = dest.acceptEnergyFromNetwork(facing, voltage - path.getMaxLoss(), amperage - amperesUsed);
             amperesUsed += amps;
             for (TileEntityCable cable : path.getPath()) {
-                if (cable.getMaxVoltage() < voltage || !cable.incrementAmperage(amps, voltage)) {
-                    cable.getWorld().setBlockState(cable.getPipePos(), Blocks.AIR.getDefaultState());
+                boolean overVolt = cable.getMaxVoltage() < voltage;
+                boolean overAmp = !cable.incrementAmperage(amps, voltage);
+                if (overVolt || overAmp) {
+                    burnCables(path.getPath(), overVolt, overAmp, amps);
                     break;
                 }
             }
@@ -53,6 +60,22 @@ public class EnergyNetHandler implements IEnergyContainer {
                 break;
         }
         return amperesUsed;
+    }
+
+    public void burnCables(Collection<TileEntityCable> cables, boolean overAmp, boolean overVolt, long amps) {
+        if(cables == null || cables.size() == 0) return;
+        World world = cables.iterator().next().getWorld();
+        for(TileEntityCable cable : cables) {
+            if(overVolt || (overAmp && amps > cable.getNodeData().amperage)) {
+                BlockPos pos = cable.getPos();
+                world.setBlockState(pos, Blocks.FIRE.getDefaultState());
+                if (!world.isRemote) {
+                    ((WorldServer) world).spawnParticle(EnumParticleTypes.SMOKE_LARGE,
+                            pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5,
+                            5 + world.rand.nextInt(3), 0.0, 0.0, 0.0, 0.1);
+                }
+            }
+        }
     }
 
     @Override
@@ -91,5 +114,10 @@ public class EnergyNetHandler implements IEnergyContainer {
     @Override
     public long getEnergyStored() {
         return 0;
+    }
+
+    @Override
+    public boolean isOneProbeHidden() {
+        return true;
     }
 }
