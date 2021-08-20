2b7232e3c6e02050609f6be87c87ec255a8afaa2
bruberu <80226372+bruberu@users.noreply.github.com>
2021-08-20 10:09:06 -0500

    Allow large miners to macerate their ores

--- src/main/java/gregtech/common/tools/ToolUtility.java
+++ src/main/java/gregtech/common/tools/ToolUtility.java
@@ -1,6 +1,10 @@
 package gregtech.common.tools;
 
 import codechicken.lib.raytracer.RayTracer;
+import gregtech.api.recipes.MatchingMode;
+import gregtech.api.recipes.Recipe;
+import gregtech.api.recipes.RecipeMap;
+import gregtech.api.recipes.RecipeMaps;
 import gregtech.api.unification.OreDictUnifier;
 import gregtech.api.unification.material.properties.PropertyKey;
 import gregtech.api.unification.ore.OrePrefix;
