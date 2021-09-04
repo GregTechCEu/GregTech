756606b8daac335c7f5ac27fd9af72b1017c390d
DStrand1 <dane@strandboge.com>
2021-09-04 17:26:25 -0500

    re-order armor items in JEI

--- src/main/java/gregtech/common/items/Armor/MetaArmor.java
+++ src/main/java/gregtech/common/items/Armor/MetaArmor.java
@@ -13,33 +13,32 @@ public class MetaArmor extends ArmorMetaItem<ArmorMetaItem<?>.ArmorMetaValueItem
     public void registerSubItems() {
         ConfigHolder.UnofficialOptions.Equipment e = ConfigHolder.U.equipment;
 
+        MetaItems.NIGHTVISION_GOGGLES = addItem(1, "nightvision_goggles").setArmorLogic(new NightvisionGoggles()).setRarity(EnumRarity.UNCOMMON);
+
+        MetaItems.SEMIFLUID_JETPACK = addItem(2, "liquid_fuel_jetpack").setArmorLogic(new PowerlessJetpack()).setRarity(EnumRarity.UNCOMMON);
+        MetaItems.IMPELLER_JETPACK = addItem(3, "impeller_jetpack").setArmorLogic(new Jetpack(125, 2520000L * (long) Math.max(1, Math.pow(4, e.voltageTierImpeller - 2)), e.voltageTierImpeller)).setModelAmount(8).setRarity(EnumRarity.UNCOMMON);
+        MetaItems.ADVANCED_IMPELLER_JETPACK = addItem(4, "advanced_impeller_jetpack").setArmorLogic(new AdvancedJetpack(512, 11400000L * (long) Math.max(1, Math.pow(4, e.voltageTierAdvImpeller - 3)), e.voltageTierAdvImpeller)).setRarity(EnumRarity.RARE);
+
         int energyPerUse = 5000;
         int tier = e.voltageTierNanoSuit;
         long maxCapacity = 1600000L * (long) Math.max(1, Math.pow(4, tier - 3));
-        MetaItems.NANO_MUSCLE_SUITE_CHESTPLATE = addItem(0, "nms.chestplate").setArmorLogic(new NanoMuscleSuite(EntityEquipmentSlot.CHEST, energyPerUse, maxCapacity, tier)).setRarity(EnumRarity.UNCOMMON);
-        MetaItems.NANO_MUSCLE_SUITE_LEGGINGS = addItem(1, "nms.leggings").setArmorLogic(new NanoMuscleSuite(EntityEquipmentSlot.LEGS, energyPerUse, maxCapacity, tier)).setRarity(EnumRarity.UNCOMMON);
-        MetaItems.NANO_MUSCLE_SUITE_HELMET = addItem(2, "nms.helmet").setArmorLogic(new NanoMuscleSuite(EntityEquipmentSlot.HEAD, energyPerUse, maxCapacity, tier)).setRarity(EnumRarity.UNCOMMON);
-        MetaItems.NANO_MUSCLE_SUITE_BOOTS = addItem(3, "nms.boots").setArmorLogic(new NanoMuscleSuite(EntityEquipmentSlot.FEET, energyPerUse, maxCapacity, tier)).setRarity(EnumRarity.UNCOMMON);
+        MetaItems.NANO_MUSCLE_SUITE_HELMET = addItem(20, "nms.helmet").setArmorLogic(new NanoMuscleSuite(EntityEquipmentSlot.HEAD, energyPerUse, maxCapacity, tier)).setRarity(EnumRarity.UNCOMMON);
+        MetaItems.NANO_MUSCLE_SUITE_CHESTPLATE = addItem(21, "nms.chestplate").setArmorLogic(new NanoMuscleSuite(EntityEquipmentSlot.CHEST, energyPerUse, maxCapacity, tier)).setRarity(EnumRarity.UNCOMMON);
+        MetaItems.NANO_MUSCLE_SUITE_LEGGINGS = addItem(22, "nms.leggings").setArmorLogic(new NanoMuscleSuite(EntityEquipmentSlot.LEGS, energyPerUse, maxCapacity, tier)).setRarity(EnumRarity.UNCOMMON);
+        MetaItems.NANO_MUSCLE_SUITE_BOOTS = addItem(23, "nms.boots").setArmorLogic(new NanoMuscleSuite(EntityEquipmentSlot.FEET, energyPerUse, maxCapacity, tier)).setRarity(EnumRarity.UNCOMMON);
+        MetaItems.ADVANCED_NANO_MUSCLE_CHESTPLATE = addItem(30, "nms.advanced_chestplate").setArmorLogic(new AdvancedNanoMuscleSuite(5000, 11400000L * (long) Math.max(1, Math.pow(4, e.voltageTierAdvNanoSuit - 4)), e.voltageTierAdvNanoSuit)).setRarity(EnumRarity.RARE);
 
         energyPerUse = 10000;
         tier = e.voltageTierQuarkTech;
         maxCapacity = 8000000L * (long) Math.max(1, Math.pow(4, tier - 5));
-        MetaItems.QUARK_TECH_SUITE_CHESTPLATE = addItem(4, "qts.chestplate").setArmorLogic(new QuarkTechSuite(EntityEquipmentSlot.CHEST, energyPerUse, maxCapacity, tier)).setRarity(EnumRarity.RARE);
-        MetaItems.QUARK_TECH_SUITE_LEGGINGS = addItem(5, "qts.leggings").setArmorLogic(new QuarkTechSuite(EntityEquipmentSlot.LEGS, energyPerUse, maxCapacity, tier)).setRarity(EnumRarity.RARE);
-        MetaItems.QUARK_TECH_SUITE_HELMET = addItem(6, "qts.helmet").setArmorLogic(new QuarkTechSuite(EntityEquipmentSlot.HEAD, energyPerUse, maxCapacity, tier)).setRarity(EnumRarity.RARE);
-        MetaItems.QUARK_TECH_SUITE_BOOTS = addItem(7, "qts.boots").setArmorLogic(new QuarkTechSuite(EntityEquipmentSlot.FEET, energyPerUse, maxCapacity, tier)).setRarity(EnumRarity.RARE);
+        MetaItems.QUARK_TECH_SUITE_HELMET = addItem(40, "qts.helmet").setArmorLogic(new QuarkTechSuite(EntityEquipmentSlot.HEAD, energyPerUse, maxCapacity, tier)).setRarity(EnumRarity.RARE);
+        MetaItems.QUARK_TECH_SUITE_CHESTPLATE = addItem(41, "qts.chestplate").setArmorLogic(new QuarkTechSuite(EntityEquipmentSlot.CHEST, energyPerUse, maxCapacity, tier)).setRarity(EnumRarity.RARE);
+        MetaItems.QUARK_TECH_SUITE_LEGGINGS = addItem(42, "qts.leggings").setArmorLogic(new QuarkTechSuite(EntityEquipmentSlot.LEGS, energyPerUse, maxCapacity, tier)).setRarity(EnumRarity.RARE);
+        MetaItems.QUARK_TECH_SUITE_BOOTS = addItem(43, "qts.boots").setArmorLogic(new QuarkTechSuite(EntityEquipmentSlot.FEET, energyPerUse, maxCapacity, tier)).setRarity(EnumRarity.RARE);
+        MetaItems.ADVANCED_QUARK_TECH_SUITE_CHESTPLATE = addItem(50, "qts.advanced_chestplate").setArmorLogic(new AdvancedQuarkTechSuite(10000, 100000000L * (long) Math.max(1, Math.pow(4, e.voltageTierAdvQuarkTech - 5)), e.voltageTierAdvQuarkTech)).setRarity(EnumRarity.EPIC);
 
-        MetaItems.SEMIFLUID_JETPACK = addItem(8, "liquid_fuel_jetpack").setArmorLogic(new PowerlessJetpack()).setRarity(EnumRarity.UNCOMMON);
-        MetaItems.IMPELLER_JETPACK = addItem(9, "impeller_jetpack").setArmorLogic(new Jetpack(125, 2520000L * (long) Math.max(1, Math.pow(4, e.voltageTierImpeller - 2)), e.voltageTierImpeller)).setModelAmount(8).setRarity(EnumRarity.UNCOMMON);
-
-        MetaItems.BATPACK_LV = addItem(10, "battery_pack.lv").setArmorLogic(new BatteryPack(0, e.batpack.capacityLV, GTValues.LV)).setModelAmount(8);
-        MetaItems.BATPACK_MV = addItem(11, "battery_pack.mv").setArmorLogic(new BatteryPack(0, e.batpack.capacityMV, GTValues.MV)).setModelAmount(8);
-        MetaItems.BATPACK_HV = addItem(12, "battery_pack.hv").setArmorLogic(new BatteryPack(0, e.batpack.capacityHV, GTValues.HV)).setModelAmount(8);
-
-        MetaItems.ADVANCED_QUARK_TECH_SUITE_CHESTPLATE = addItem(13, "qts.advanced_chestplate").setArmorLogic(new AdvancedQuarkTechSuite(10000, 100000000L * (long) Math.max(1, Math.pow(4, e.voltageTierAdvQuarkTech - 5)), e.voltageTierAdvQuarkTech)).setRarity(EnumRarity.EPIC);
-        MetaItems.ADVANCED_NANO_MUSCLE_CHESTPLATE = addItem(14, "nms.advanced_chestplate").setArmorLogic(new AdvancedNanoMuscleSuite(5000, 11400000L * (long) Math.max(1, Math.pow(4, e.voltageTierAdvNanoSuit - 4)), e.voltageTierAdvNanoSuit)).setRarity(EnumRarity.RARE);
-        MetaItems.ADVANCED_IMPELLER_JETPACK = addItem(15, "advanced_impeller_jetpack").setArmorLogic(new AdvancedJetpack(512, 11400000L * (long) Math.max(1, Math.pow(4, e.voltageTierAdvImpeller - 3)), e.voltageTierAdvImpeller)).setRarity(EnumRarity.RARE);
-
-        MetaItems.NIGHTVISION_GOGGLES = addItem(16, "nightvision_goggles").setArmorLogic(new NightvisionGoggles()).setRarity(EnumRarity.UNCOMMON);
+        MetaItems.BATPACK_LV = addItem(70, "battery_pack.lv").setArmorLogic(new BatteryPack(0, e.batpack.capacityLV, GTValues.LV)).setModelAmount(8);
+        MetaItems.BATPACK_MV = addItem(71, "battery_pack.mv").setArmorLogic(new BatteryPack(0, e.batpack.capacityMV, GTValues.MV)).setModelAmount(8);
+        MetaItems.BATPACK_HV = addItem(72, "battery_pack.hv").setArmorLogic(new BatteryPack(0, e.batpack.capacityHV, GTValues.HV)).setModelAmount(8);
     }
 }
