2b2eedf528a60cc9083260b7fa37ba917d81171d
Yefancy <yefancy@foxmail.com>
2021-09-11 20:53:53 +0800

    clean  up

--- src/main/java/gregtech/common/EventHandlers.java
+++ src/main/java/gregtech/common/EventHandlers.java
@@ -1,21 +1,11 @@
 package gregtech.common;
 
 import gregtech.api.GTValues;
-import gregtech.common.blocks.BlockConcrete;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.entity.monster.EntityEnderman;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.init.MobEffects;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.MovementInput;
-import net.minecraft.util.MovementInputFromOptions;
-import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.event.entity.living.EnderTeleportEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
