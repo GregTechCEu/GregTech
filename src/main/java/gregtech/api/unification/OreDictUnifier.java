8403747662d3e149ca3c13f1cc46560246b7ed1d
DStrand1 <dane@strandboge.com>
2021-11-18 20:28:58 -0600

    lots more work on recycling recipe logic

--- src/main/java/gregtech/api/unification/OreDictUnifier.java
+++ src/main/java/gregtech/api/unification/OreDictUnifier.java
@@ -194,12 +194,6 @@ public class OreDictUnifier {
     }
 
     @Nullable
-    public static MaterialStack getMaterialWithCount(ItemStack stack) {
-        MaterialStack ms = getMaterial(stack);
-        return ms == null ? null : new MaterialStack(ms.material, ms.amount * stack.getCount());
-    }
-
-    @Nullable
     public static ItemMaterialInfo getMaterialInfo(ItemStack itemStack) {
         if (itemStack.isEmpty()) return null;
         ItemAndMetadata simpleItemStack = new ItemAndMetadata(itemStack);
@@ -281,7 +275,6 @@ public class OreDictUnifier {
         return ItemStack.EMPTY;
     }
 
-
     public static ItemStack getDust(MaterialStack materialStack) {
         return getDust(materialStack.material, materialStack.amount);
     }
@@ -289,11 +282,31 @@ public class OreDictUnifier {
     public static ItemStack getIngot(Material material, long materialAmount) {
         if (!material.hasProperty(PropertyKey.INGOT) || materialAmount <= 0)
             return ItemStack.EMPTY;
+        if (materialAmount % (M * 9) == 0)
+            return get(OrePrefix.block, material, (int) (materialAmount / (M * 9)));
         if (materialAmount % M == 0 || materialAmount >= M * 16)
             return get(OrePrefix.ingot, material, (int) (materialAmount / M));
         else if ((materialAmount * 9) >= M)
             return get(OrePrefix.nugget, material, (int) ((materialAmount * 9) / M));
         return ItemStack.EMPTY;
+    }
+
+    public static ItemStack getIngot(MaterialStack materialStack) {
+        return getIngot(materialStack.material, materialStack.amount);
+    }
+
+    /**
+     * Returns an Ingot of the material if it exists. Otherwise it returns a Dust.
+     * Returns ItemStack.EMPTY if neither exist.
+     */
+    public static ItemStack getIngotOrDust(Material material, long materialAmount) {
+        ItemStack ingotStack = getIngot(material, materialAmount);
+        if (ingotStack != ItemStack.EMPTY) return ingotStack;
+        return getDust(material, materialAmount);
+    }
+
+    public static ItemStack getIngotOrDust(MaterialStack materialStack) {
+        return getIngotOrDust(materialStack.material, materialStack.amount);
     }
 
     synchronized private static <T> void addAndSort(List<T> list, T itemToAdd, Comparator<T> comparator) {
--- src/main/java/gregtech/loaders/recipe/RecyclingRecipes.java
+++ src/main/java/gregtech/loaders/recipe/RecyclingRecipes.java
@@ -1,5 +1,6 @@
 package gregtech.loaders.recipe;
 
+import com.google.common.collect.ImmutableList;
 import gregtech.api.recipes.RecipeBuilder;
 import gregtech.api.recipes.RecipeMaps;
 import gregtech.api.unification.OreDictUnifier;
@@ -8,13 +9,16 @@ import gregtech.api.unification.material.Materials;
 import gregtech.api.unification.material.properties.BlastProperty;
 import gregtech.api.unification.material.properties.PropertyKey;
 import gregtech.api.unification.ore.OrePrefix;
-import gregtech.api.unification.stack.ItemAndMetadata;
 import gregtech.api.unification.stack.ItemMaterialInfo;
 import gregtech.api.unification.stack.MaterialStack;
+import gregtech.api.unification.stack.UnificationEntry;
+import gregtech.api.util.GTUtility;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Tuple;
 
 import java.util.*;
 import java.util.Map.Entry;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import static gregtech.api.GTValues.L;
@@ -23,10 +27,10 @@ import static gregtech.api.unification.material.info.MaterialFlags.FLAMMABLE;
 
 public class RecyclingRecipes {
 
-    // TODO - Durations are still too long
     // TODO - Fix recipe order with some things (noticed Hermetic Casings)
     // TODO - Figure out solution to LuV+ components
     // TODO - (to remember) Do NOT calculate any material component lists for circuits, they are simply totally lost
+    // TODO - Work on durations and EUt's
 
     public static void init() {
         for (Entry<ItemStack, ItemMaterialInfo> entry : OreDictUnifier.getAllItemInfos()) {
@@ -70,29 +74,21 @@ public class RecyclingRecipes {
 
     private static void registerMaceratorRecycling(ItemStack input, List<MaterialStack> materials, int multiplier) {
 
-        // Filter down the materials list.
-        // - Filter out Materials that cannot create Dusts
-        // - Limit to the maximum number of outputs for the RecipeMap
-        materials = materials.stream()
-                .filter(ms -> OreDictUnifier.getDust(ms) != ItemStack.EMPTY)
-                .limit(RecipeMaps.MACERATOR_RECIPES.getMaxOutputs())
-                .collect(Collectors.toList());
+        // Finalize the output list.
+        List<ItemStack> outputs = finalizeOutputs(
+                materials,
+                RecipeMaps.MACERATOR_RECIPES.getMaxOutputs(),
+                OreDictUnifier::getDust
+        );
 
         // Exit if no valid Materials exist for this recycling Recipe.
-        if (materials.size() == 0) return;
-
-        // Calculate the duration of the Recipe.
-        // - Sum the Material amounts together
-        // - Multiply by 30, and divide by M
-        long duration = 0;
-        for (MaterialStack ms : materials) duration += (ms.amount * ms.material.getAverageMass());
-        duration = Math.max(1L, duration / M);
+        if (outputs.size() == 0) return;
 
         // Build the final Recipe.
         RecipeMaps.MACERATOR_RECIPES.recipeBuilder()
                 .inputs(input.copy())
-                .outputs(materials.stream().map(OreDictUnifier::getDust).collect(Collectors.toList()))
-                .duration((int) duration)
+                .outputs(outputs)
+                .duration(calculateDuration(materials))
                 .EUt(8 * multiplier)
                 .buildAndRegister();
     }
@@ -111,7 +107,7 @@ public class RecyclingRecipes {
 
         // Calculate the duration based off of those two possible outputs.
         // - Sum the two Material amounts together (if both exist)
-        // - Multiply by 80 and divide by M
+        // - Divide the sum by M
         long duration = fluidMs.amount * fluidMs.material.getAverageMass();
         if (itemMs != null) duration += (itemMs.amount * itemMs.material.getAverageMass());
         duration = Math.max(1L, duration / M);
@@ -123,9 +119,11 @@ public class RecyclingRecipes {
                 .duration((int) duration)
                 .EUt(32 * multiplier);
 
-        // Null check the Item before adding it to the Builder
+        // Null check the Item before adding it to the Builder.
+        // - Try to output an Ingot, otherwise output a Dust.
         if (itemMs != null) {
-            extractorBuilder.output(OrePrefix.dust, itemMs.material, (int) (itemMs.amount / M));
+            OrePrefix prefix = itemMs.material.hasProperty(PropertyKey.INGOT) ? OrePrefix.ingot : OrePrefix.dust;
+            extractorBuilder.output(prefix, itemMs.material, (int) (itemMs.amount / M));
         }
 
         extractorBuilder.buildAndRegister();
@@ -135,68 +133,37 @@ public class RecyclingRecipes {
 
         // Filter down the materials list.
         // - Map to the Arc Smelting result as defined below
-        // - Filter out empty ItemStacks
-        List<ItemStack> arcItemStacks = materials.stream()
+        // - Combine any MaterialStacks that have the same Material
+        materials = combineStacks(materials.stream()
                 .map(RecyclingRecipes::getArcSmeltingResult)
-                .filter(is -> !is.isEmpty())
-                .collect(Collectors.toList());
+                .collect(Collectors.toList()));
 
-        // Exit if no valid Materials exist for this recycling Recipe.
-        if (materials.size() == 0) return;
+        // Finalize the output List
+        List<ItemStack> outputs = finalizeOutputs(
+                materials,
+                RecipeMaps.ARC_FURNACE_RECIPES.getMaxOutputs(),
+                OreDictUnifier::getIngotOrDust
+        );
 
-        // Combine stacks in the List that have the same Item.
-        Map<ItemAndMetadata, Integer> outputsExploded = new HashMap<>();
-        for (ItemStack stack : arcItemStacks) {
-            ItemAndMetadata item = new ItemAndMetadata(stack.getItem(), stack.getItemDamage());
-            int amount = outputsExploded.getOrDefault(item, 0);
-            outputsExploded.put(item, stack.getCount() + amount);
-        }
-
-        // Map and sort the output list.
-        List<ItemStack> outputs = outputsExploded.entrySet().stream()
-
-                // Map <ItemAndMetadata, Integer> to ItemStack
-                .map(e -> e.getKey().toItemStack(e.getValue()))
-
-                // Sort based on MaterialStack.amount.
-                // We have to re-sort here since calling getArcSmeltingResult can
-                // change the material amounts of the list.
-                .sorted(Comparator.comparingLong(is -> -Objects.requireNonNull(OreDictUnifier.getMaterialWithCount(is)).amount))
-
-                // Limit to the maximum amount of outputs for the Arc Furnace
-                .limit(RecipeMaps.ARC_FURNACE_RECIPES.getMaxOutputs())
-                .collect(Collectors.toList());
-
-        // Calculate the duration of the Recipe.
-        // - Sum (materialStack.amount * material.getAverageMass()) together for the list, excluding
-        //   the arc-furnaced materials (Carbon, Dark Ash, Ash)
-        // - Divide by M
-        long duration = 0;
-        for (ItemStack is : outputs) {
-            MaterialStack ms = OreDictUnifier.getMaterialWithCount(is);
-            if (ms != null && ms.material != Materials.Carbon && ms.material != Materials.Ash && ms.material != Materials.DarkAsh) {
-                duration += (ms.amount * ms.material.getAverageMass());
-            }
-        }
-        duration = Math.max(1L, duration / M);
+        // Exit if no valid outputs exist for this recycling Recipe.
+        if (outputs.size() == 0) return;
 
         // Build the final Recipe.
         RecipeMaps.ARC_FURNACE_RECIPES.recipeBuilder()
                 .inputs(input)
                 .outputs(outputs)
-                .duration((int) duration)
+                .duration(calculateDuration(materials))
                 .EUt(30 * multiplier)
                 .buildAndRegister();
     }
 
-    // TODO Mess with material amount conversion for Ash/Dark Ash/Carbon
-    private static ItemStack getArcSmeltingResult(MaterialStack materialStack) {
+    private static MaterialStack getArcSmeltingResult(MaterialStack materialStack) {
         Material material = materialStack.material;
         long amount = materialStack.amount;
 
         // If the Material is Flammable, return Ash
         if (material.hasFlag(FLAMMABLE)) {
-            return OreDictUnifier.getDust(Materials.Ash, amount);
+            return new MaterialStack(Materials.Ash, amount);
         }
 
         // Else if the Material is a Gem, process its output (see below)
@@ -204,37 +171,35 @@ public class RecyclingRecipes {
             return getGemArcSmeltResult(materialStack);
         }
 
-        // Else if the Material is an Ingot, return an Ingot of the Arc Smelting
-        // result if it exists, otherwise return an Ingot of the Material itself
+        // Else if the Material is an Ingot, return  the Arc Smelting
+        // result if it exists, otherwise return the Material itself.
         if (material.hasProperty(PropertyKey.INGOT)) {
             Material arcSmelt = material.getProperty(PropertyKey.INGOT).getArcSmeltInto();
-            if (arcSmelt != null)
-                return OreDictUnifier.getIngot(arcSmelt, amount);
-            return OreDictUnifier.getIngot(material, amount);
+            if (arcSmelt != null) {
+                return new MaterialStack(arcSmelt, amount);
+            }
         }
-
-        // Else return a Dust of the Material
-        return OreDictUnifier.getDust(material, amount);
+        return materialStack;
     }
 
-    private static ItemStack getGemArcSmeltResult(MaterialStack materialStack) {
+    private static MaterialStack getGemArcSmeltResult(MaterialStack materialStack) {
         Material material = materialStack.material;
         long amount = materialStack.amount;
 
         // If the Gem Material has Oxygen in it, return Ash
         if (material.getMaterialComponents().stream()
                 .anyMatch(stack -> stack.material == Materials.Oxygen)) {
-            return OreDictUnifier.getDust(Materials.Ash, amount);
+            return new MaterialStack(Materials.Ash, amount);
         }
 
         // Else if the Gem Material has Carbon in it, return Carbon
         if (material.getMaterialComponents().stream()
                 .anyMatch(stack -> stack.material == Materials.Carbon)) {
-            return OreDictUnifier.getDust(Materials.Carbon, amount);
+            return new MaterialStack(Materials.Carbon, amount);
         }
 
         // Else return Dark Ash
-        return OreDictUnifier.getDust(Materials.DarkAsh, amount);
+        return new MaterialStack(Materials.DarkAsh, amount);
     }
 
     private static int calculateVoltageMultiplier(List<MaterialStack> materials) {
@@ -259,5 +224,159 @@ public class RecyclingRecipes {
 
         // If above 2000K, multiplier of 16
         return 16;
+    }
+
+    /**
+     * This method calculates the duration for a recycling method. It:
+     * - Sums the amount of material times the mass of the material for the List
+     * - Divides that by M
+     */
+    private static int calculateDuration(List<MaterialStack> materials) {
+        long duration = 0;
+        for (MaterialStack ms : materials) duration += (ms.amount * ms.material.getAverageMass());
+        return (int) Math.max(1L, duration / M);
+    }
+
+    /**
+     * Combines any matching Materials in the List into one MaterialStack
+     */
+    private static List<MaterialStack> combineStacks(List<MaterialStack> rawList) {
+
+        // Combine any stacks in the List that have the same Item.
+        Map<Material, Long> materialStacksExploded = new HashMap<>();
+        for (MaterialStack ms : rawList) {
+            long amount = materialStacksExploded.getOrDefault(ms.material, 0L);
+            materialStacksExploded.put(ms.material, ms.amount + amount);
+        }
+        return materialStacksExploded.entrySet().stream()
+                .map(e -> new MaterialStack(e.getKey(), e.getValue()))
+                .collect(Collectors.toList());
+    }
+
+    private static List<ItemStack> finalizeOutputs(List<MaterialStack> materials, int maxOutputs, Function<MaterialStack, ItemStack> toItemStackMapper) {
+
+        // Map of ItemStack, Long to properly sort by the true material amount for outputs
+        List<Tuple<ItemStack, MaterialStack>> outputs = new ArrayList<>();
+
+        for (MaterialStack ms : materials) {
+            ItemStack stack = toItemStackMapper.apply(ms);
+            if (stack == ItemStack.EMPTY) continue;
+            if (stack.getCount() > 64) {
+                UnificationEntry entry = OreDictUnifier.getUnificationEntry(stack);
+                if (entry != null) { // should always be true
+                    OrePrefix prefix = entry.orePrefix;
+
+                    // These are the highest forms that a Material can have (for Ingot and Dust, respectively),
+                    // so simply split the stacks and continue.
+                    if (prefix == OrePrefix.block || prefix == OrePrefix.dust) {
+                        splitStacks(outputs, stack, entry);
+                    } else {
+                        // Attempt to split and to shrink the stack, and choose the option that creates the
+                        // "larger" single stack, in terms of raw material amount.
+                        List<Tuple<ItemStack, MaterialStack>> split = new ArrayList<>();
+                        List<Tuple<ItemStack, MaterialStack>> shrink = new ArrayList<>();
+                        splitStacks(split, stack, entry);
+                        shrinkStacks(shrink, stack, entry);
+
+                        if (split.get(0).getSecond().amount > shrink.get(0).getSecond().amount) {
+                            outputs.addAll(split);
+                        } else outputs.addAll(shrink);
+                    }
+                }
+            } else outputs.add(new Tuple<>(stack, ms));
+        }
+
+        // Sort the List by total material amount descending.
+        outputs.sort(Comparator.comparingLong(e -> -e.getSecond().amount));
+
+        // Cut out "duplicate" outputs.
+        // For example, if there are blocks of Steel and nuggets of Steel, and the nuggets
+        // are preventing some other output from occupying one of the final slots of the machine,
+        // cut the nuggets out to favor the newer item instead of having 2 slots occupied by Steel.
+        Map<MaterialStack, ItemStack> temp = new HashMap<>();
+        for (Tuple<ItemStack, MaterialStack> t : outputs) {
+            if (temp.containsKey(t.getSecond())) continue;
+            temp.put(t.getSecond(), t.getFirst());
+        }
+
+        // Not enough unique elements in the result list map.
+        if (temp.size() < maxOutputs) {
+            int limit = maxOutputs - temp.size(); // maximum number of outputs to append
+            temp.putAll(outputs.stream()
+                    .filter(t -> !temp.containsKey(t.getSecond()))
+                    .limit(limit)
+                    .collect(Collectors.toMap(Tuple::getSecond, Tuple::getFirst)));
+        }
+
+        return temp.entrySet().stream()
+                .sorted(Comparator.comparingLong(e -> -e.getKey().amount))
+                .limit(maxOutputs)
+                .map(Entry::getValue)
+                .collect(Collectors.toList());
+    }
+
+    private static void splitStacks(List<Tuple<ItemStack, MaterialStack>> list, ItemStack originalStack, UnificationEntry entry) {
+        int amount = originalStack.getCount();
+        while (amount > 64) {
+            list.add(new Tuple<>(GTUtility.copyAmount(64, originalStack), new MaterialStack(entry.material, entry.orePrefix.materialAmount * 64)));
+            amount -= 64;
+        }
+        list.add(new Tuple<>(GTUtility.copyAmount(amount, originalStack), new MaterialStack(entry.material, entry.orePrefix.materialAmount * amount)));
+    }
+
+    private static final List<OrePrefix> DUST_ORDER = ImmutableList.of(OrePrefix.dust, OrePrefix.dustSmall, OrePrefix.dustTiny);
+    private static final List<OrePrefix> INGOT_ORDER = ImmutableList.of(OrePrefix.block, OrePrefix.ingot, OrePrefix.nugget);
+
+    private static void shrinkStacks(List<Tuple<ItemStack, MaterialStack>> list, ItemStack originalStack, UnificationEntry entry) {
+        Material material = entry.material;
+        long materialAmount = originalStack.getCount() * entry.orePrefix.materialAmount;
+
+        //noinspection ConstantConditions
+        final List<OrePrefix> chosenList = material.hasProperty(PropertyKey.INGOT) ? INGOT_ORDER : DUST_ORDER;
+
+        // Break materialAmount into a maximal stack
+        Map<OrePrefix, MaterialStack> tempList = new HashMap<>();
+        for (OrePrefix prefix : chosenList) {
+
+            // Current prefix too large to "compact" into
+            if (materialAmount / prefix.materialAmount == 0) continue;
+
+            long newAmount = materialAmount / prefix.materialAmount;
+            tempList.put(prefix, new MaterialStack(material, newAmount * prefix.materialAmount));
+            materialAmount = materialAmount % prefix.materialAmount;
+        }
+
+        // Split the "highest level" stack (either Blocks or Dusts) if needed, as it is
+        // the only stack that could possibly be above 64.
+        if (tempList.containsKey(chosenList.get(0))) {
+            OrePrefix prefix = chosenList.get(0);
+            MaterialStack ms = tempList.get(prefix);
+            splitStacks(list, OreDictUnifier.get(chosenList.get(0), ms.material, (int) (ms.amount / prefix.materialAmount)), new UnificationEntry(prefix, material));
+        }
+
+        OrePrefix mediumPrefix = chosenList.get(1); // dustSmall or ingot
+        OrePrefix smallestPrefix = chosenList.get(2); // dustTiny or nugget
+        MaterialStack mediumMS = tempList.get(mediumPrefix); // dustSmall or ingot
+        MaterialStack smallestMS = tempList.get(smallestPrefix); // dustTiny or nugget
+
+        // Try to compact the two "lower form" prefixes into one stack, if it doesn't exceed stack size
+        if (mediumMS != null && smallestMS != null) {
+            long singleStackAmount = mediumMS.amount + smallestMS.amount;
+            if (singleStackAmount / smallestPrefix.materialAmount <= 64) {
+                list.add(new Tuple<>(OreDictUnifier.get(smallestPrefix, material, (int) (singleStackAmount / smallestPrefix.materialAmount)), new MaterialStack(material, singleStackAmount)));
+                return;
+            }
+        }
+
+        // Otherwise simply add the stacks to the List if they exist
+        if (mediumMS != null) list.add(new Tuple<>(
+                OreDictUnifier.get(mediumPrefix, material, (int) (mediumMS.amount / mediumPrefix.materialAmount)),
+                new MaterialStack(material, mediumMS.amount)
+        ));
+
+        if (smallestMS != null) list.add(new Tuple<>(
+                OreDictUnifier.get(smallestPrefix, material, (int) (smallestMS.amount / smallestPrefix.materialAmount)),
+                new MaterialStack(material, smallestMS.amount)
+        ));
     }
 }
