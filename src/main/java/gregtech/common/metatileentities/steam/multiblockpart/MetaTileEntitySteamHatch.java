0cee0fd1d4d6a4cdd645f49443e92fc5d6f92264
BraggestSage833 <46819052+BraggestSage833@users.noreply.github.com>
2021-12-12 22:19:29 -0500

    fix two tanks for steam hatchs

--- src/main/java/gregtech/common/metatileentities/steam/multiblockpart/MetaTileEntitySteamHatch.java
+++ src/main/java/gregtech/common/metatileentities/steam/multiblockpart/MetaTileEntitySteamHatch.java
@@ -152,6 +152,12 @@ public class MetaTileEntitySteamHatch extends MetaTileEntityMultiblockPart imple
     }
 
     @Override
+    protected void initializeInventory() {
+        super.initializeInventory();
+        this.fluidInventory = importFluids;
+    }
+
+    @Override
     public MultiblockAbility<IFluidTank> getAbility() {
         return MultiblockAbility.STEAM;
     }
