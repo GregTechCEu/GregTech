1b06c2254f721e828f6a6c3f56755e1125fc3b8f
Yefancy <yefancy@foxmail.com>
2021-12-17 14:00:53 +0800

    Countable + OrePrefix BracketHandler

--- src/main/java/gregtech/api/recipes/CountableIngredient.java
+++ src/main/java/gregtech/api/recipes/CountableIngredient.java
@@ -1,15 +1,22 @@
 package gregtech.api.recipes;
 
+import crafttweaker.annotations.ZenRegister;
 import gregtech.api.unification.material.Material;
 import gregtech.api.unification.ore.OrePrefix;
 import gregtech.api.unification.stack.UnificationEntry;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraftforge.oredict.OreIngredient;
+import stanhebben.zenscript.annotations.OperatorType;
+import stanhebben.zenscript.annotations.ZenClass;
+import stanhebben.zenscript.annotations.ZenMethod;
+import stanhebben.zenscript.annotations.ZenOperator;
 
 import java.util.Arrays;
 import java.util.Objects;
 
+@ZenClass("mods.gregtech.recipe.CountableIngredient")
+@ZenRegister
 public class CountableIngredient {
 
     public static CountableIngredient from(ItemStack stack) {
@@ -20,20 +27,29 @@ public class CountableIngredient {
         return new CountableIngredient(Ingredient.fromStacks(stack), amount);
     }
 
+    @ZenMethod
     public static CountableIngredient from(String oredict) {
         return new CountableIngredient(new OreIngredient(oredict), 1);
     }
 
+    @ZenMethod
     public static CountableIngredient from(String oredict, int count) {
         return new CountableIngredient(new OreIngredient(oredict), count);
     }
 
+    @ZenMethod
     public static CountableIngredient from(OrePrefix prefix, Material material) {
         return from(prefix, material, 1);
     }
 
+    @ZenMethod
     public static CountableIngredient from(OrePrefix prefix, Material material, int count) {
         return new CountableIngredient(new OreIngredient(new UnificationEntry(prefix, material).toString()), count);
+    }
+
+    @ZenOperator(OperatorType.MUL)
+    public CountableIngredient amount(int amount) {
+        return new CountableIngredient(ingredient, amount * count);
     }
 
     private final Ingredient ingredient;
---
+++ src/main/java/gregtech/api/unification/crafttweaker/OrePrefixBracketHandler.java
@@ -0,0 +1,45 @@
+package gregtech.api.unification.crafttweaker;
+
+
+import crafttweaker.CraftTweakerAPI;
+import crafttweaker.annotations.BracketHandler;
+import crafttweaker.annotations.ZenRegister;
+import crafttweaker.zenscript.IBracketHandler;
+import gregtech.api.unification.ore.OrePrefix;
+import stanhebben.zenscript.annotations.ZenMethod;
+import stanhebben.zenscript.compiler.IEnvironmentGlobal;
+import stanhebben.zenscript.expression.ExpressionCallStatic;
+import stanhebben.zenscript.expression.ExpressionString;
+import stanhebben.zenscript.parser.Token;
+import stanhebben.zenscript.symbols.IZenSymbol;
+import stanhebben.zenscript.type.natives.IJavaMethod;
+
+import java.util.List;
+
+@BracketHandler
+@ZenRegister
+public class OrePrefixBracketHandler implements IBracketHandler {
+    private final IJavaMethod method;
+
+    public OrePrefixBracketHandler() {
+        this.method = CraftTweakerAPI.getJavaMethod(OrePrefixBracketHandler.class, "get", String.class);
+    }
+
+    @ZenMethod
+    public static OrePrefix get(String member) {
+        return OrePrefix.getPrefix(member);
+    }
+
+    @Override
+    public IZenSymbol resolve(IEnvironmentGlobal environment, List<Token> tokens) {
+        if ((tokens.size() < 3)) return null;
+        if (!tokens.get(0).getValue().equalsIgnoreCase("ore_prefix")) return null;
+        if (!tokens.get(1).getValue().equals(":")) return null;
+        StringBuilder nameBuilder = new StringBuilder();
+        for (int i = 2; i < tokens.size(); i++) {
+            nameBuilder.append(tokens.get(i).getValue());
+        }
+        return position -> new ExpressionCallStatic(position, environment, method,
+                new ExpressionString(position, nameBuilder.toString()));
+    }
+}
