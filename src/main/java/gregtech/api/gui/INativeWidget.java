ecc8b2a202f630e52abe382acf7dabbd108c5f4c
Yefancy <yefancy@foxmail.com>
2021-12-19 15:05:26 +0800

    fix phantomwidget

--- src/main/java/gregtech/api/gui/INativeWidget.java
+++ src/main/java/gregtech/api/gui/INativeWidget.java
@@ -1,7 +1,6 @@
 package gregtech.api.gui;
 
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Items;
 import net.minecraft.inventory.ClickType;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
