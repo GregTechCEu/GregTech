22d79dd3136dd484e86699fffca251ec41652791
tech22 <37029404+TechLord22@users.noreply.github.com>
2021-12-24 18:33:59 -0500

    fix rebase issues 2

--- src/main/java/gregtech/loaders/recipe/handlers/PartsRecipeHandler.java
+++ src/main/java/gregtech/loaders/recipe/handlers/PartsRecipeHandler.java
@@ -17,7 +17,6 @@ import gregtech.api.unification.stack.UnificationEntry;
 import gregtech.api.util.GTUtility;
 import gregtech.common.ConfigHolder;
 import gregtech.common.items.MetaItems;
-import gregtech.common.items.behaviors.TurbineRotorBehavior;
 import gregtech.common.items.behaviors.TurbineRotorBehavior2;
 import net.minecraft.item.EnumDyeColor;
 import net.minecraft.item.ItemStack;
@@ -433,7 +432,7 @@ public class PartsRecipeHandler {
     public static void processTurbine(OrePrefix toolPrefix, Material material, IngotProperty property) {
         ItemStack rotorStack = MetaItems.TURBINE_ROTOR.getStackForm();
         //noinspection ConstantConditions
-        TurbineRotorBehavior.getInstanceFor(rotorStack).setPartMaterial(rotorStack, material);
+        TurbineRotorBehavior2.getInstanceFor(rotorStack).setPartMaterial(rotorStack, material);
 
         RecipeMaps.ASSEMBLER_RECIPES.recipeBuilder()
                 .input(OrePrefix.turbineBlade, material, 8)
