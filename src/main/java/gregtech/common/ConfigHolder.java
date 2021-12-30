9f4af7a61441b07333efa91f5a8bfe6f52037254
KilaBash <yefancy@foxmail.com>
2021-12-30 16:13:04 +0800

    custom fusion bloom (#431)

--- src/main/java/gregtech/common/ConfigHolder.java
+++ src/main/java/gregtech/common/ConfigHolder.java
@@ -297,6 +297,10 @@ public class ConfigHolder {
 
         public static class ShaderOptions {
 
+            @Config.Comment("Bloom config options for the fusion reactor.")
+            @Config.Name("Fusion Reactor")
+            public FusionBloom fusionBloom = new FusionBloom();
+
             @Config.Comment({"Whether to use shader programs.", "Default: true"})
             public boolean useShader = true;
 
@@ -329,12 +333,40 @@ public class ConfigHolder {
 
             @Config.Comment({"Bloom Strength", "OUTPUT = BACKGROUND + BLOOM * {strength} * (base + LT + (1 - BACKGROUND_BRIGHTNESS)*(HT-LT)))", "Default: 2"})
             @Config.RangeDouble(min = 0)
-            public double strength = 2;
+            public double strength = 1.5;
 
             @Config.Comment({"Blur Step (bloom range)", "Default: 1"})
             @Config.RangeDouble(min = 0)
             public double step = 1;
         }
+    }
+
+    public static class FusionBloom {
+        @Config.Comment({"Whether to use shader programs.", "Default: true"})
+        public boolean useShader = true;
+
+        @Config.Comment({"Bloom Strength", "OUTPUT = BACKGROUND + BLOOM * {strength} * (base + LT + (1 - BACKGROUND_BRIGHTNESS)*(HT-LT)))", "Default: 2"})
+        @Config.RangeDouble(min = 0)
+        public double strength = 1.5;
+
+        @Config.Comment({"Bloom Algorithm", "0 - Simple Gaussian Blur Bloom (Fast)", "1 - Unity Bloom", "2 - Unreal Bloom", "Default: 2"})
+        @Config.RangeInt(min = 0, max = 2)
+        @Config.SlidingOption
+        public int bloomStyle = 1;
+
+        @Config.Comment({"The brightness after bloom should not exceed this value. It can be used to limit the brightness of highlights " +
+                "(e.g., daytime).", "OUTPUT = BACKGROUND + BLOOM * strength * (base + LT + (1 - BACKGROUND_BRIGHTNESS)*({HT}-LT)))", "This value should be greater than lowBrightnessThreshold.", "Default: 0.5"})
+        @Config.RangeDouble(min = 0)
+        public double highBrightnessThreshold = 1.3;
+
+        @Config.Comment({"The brightness after bloom should not smaller than this value. It can be used to limit the brightness of dusky parts " +
+                "(e.g., night/caves).", "OUTPUT = BACKGROUND + BLOOM * strength * (base + {LT} + (1 - BACKGROUND_BRIGHTNESS)*(HT-{LT})))", "This value should be smaller than highBrightnessThreshold.", "Default: 0.2"})
+        @Config.RangeDouble(min = 0)
+        public double lowBrightnessThreshold = 0.3;
+
+        @Config.Comment({"The base brightness of the bloom.", "It is similar to strength", "This value should be smaller than highBrightnessThreshold.", "OUTPUT = BACKGROUND + BLOOM * strength * ({base} + LT + (1 - BACKGROUND_BRIGHTNESS)*(HT-LT)))", "Default: 0.1"})
+        @Config.RangeDouble(min = 0)
+        public double baseBrightness = 0;
     }
 
     public static class ToolOptions {
