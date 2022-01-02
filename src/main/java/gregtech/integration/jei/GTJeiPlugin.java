c80eb7bd5cd9bf32f092ef112f281dcea6b9bde8
htmlcsjs <46023024+htmlcsjs@users.noreply.github.com>
2022-01-02 18:08:08 +0000

    fix rebase

--- src/main/java/gregtech/integration/jei/GTJeiPlugin.java
+++ src/main/java/gregtech/integration/jei/GTJeiPlugin.java
@@ -14,7 +14,6 @@ import gregtech.api.recipes.Recipe;
 import gregtech.api.recipes.RecipeMap;
 import gregtech.api.recipes.RecipeMaps;
 import gregtech.api.recipes.ingredients.IntCircuitIngredient;
-import gregtech.api.recipes.machines.FuelRecipeMap;
 import gregtech.api.recipes.machines.RecipeMapFurnace;
 import gregtech.api.unification.material.Material;
 import gregtech.api.unification.material.properties.PropertyKey;
@@ -25,7 +24,6 @@ import gregtech.common.items.MetaItems;
 import gregtech.common.metatileentities.MetaTileEntities;
 import gregtech.integration.jei.multiblock.MultiblockInfoCategory;
 import gregtech.integration.jei.recipe.*;
-import gregtech.integration.jei.recipe.fuel.FuelRecipeMapCategory;
 import gregtech.integration.jei.recipe.primitive.MaterialTree;
 import gregtech.integration.jei.recipe.primitive.MaterialTreeCategory;
 import gregtech.integration.jei.recipe.primitive.OreByProduct;
@@ -33,6 +31,7 @@ import gregtech.integration.jei.recipe.primitive.OreByProductCategory;
 import gregtech.integration.jei.utils.CustomItemReturnRecipeWrapper;
 import gregtech.integration.jei.utils.MachineSubtypeHandler;
 import gregtech.integration.jei.utils.MetaItemSubtypeHandler;
+import gregtech.integration.jei.utils.MultiblockInfoRecipeFocusShower;
 import gregtech.loaders.recipe.CustomItemReturnShapedOreRecipeRecipe;
 import mezz.jei.Internal;
 import mezz.jei.api.*;
@@ -83,9 +82,6 @@ public class GTJeiPlugin implements IModPlugin {
             if (!recipeMap.isHidden) {
                 registry.addRecipeCategories(new RecipeMapCategory(recipeMap, registry.getJeiHelpers().getGuiHelper()));
             }
-        }
-        for (FuelRecipeMap fuelRecipeMap : FuelRecipeMap.getRecipeMaps()) {
-            registry.addRecipeCategories(new FuelRecipeMapCategory(fuelRecipeMap, registry.getJeiHelpers().getGuiHelper()));
         }
         registry.addRecipeCategories(new RecipeMapCategory(RecipeMaps.SCANNER_RECIPES, registry.getJeiHelpers().getGuiHelper())); // Really jank solution to put scanner jei after assline
         registry.addRecipeCategories(new OreByProductCategory(registry.getJeiHelpers().getGuiHelper()));
