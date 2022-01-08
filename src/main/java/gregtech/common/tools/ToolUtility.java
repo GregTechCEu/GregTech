8d2244ff73fb557e318da4ffa85eea77fa3fab6f
ALongStringOfNumbers <31759736+ALongStringOfNumbers@users.noreply.github.com>
2022-01-07 18:53:48 -0700

    Make the Hammer and enchant respect nether and end ore doubling (#491)

--- src/main/java/gregtech/common/tools/ToolUtility.java
+++ src/main/java/gregtech/common/tools/ToolUtility.java
@@ -99,11 +99,13 @@ public class ToolUtility {
         MaterialStack input = OreDictUnifier.getMaterial(inputStack);
         if (input != null && input.material.hasProperty(PropertyKey.ORE) && GTUtility.isOre(blockState.getBlock()) && !(player instanceof FakePlayer)) {
             drops.clear();
+            OrePrefix prefix = OreDictUnifier.getPrefix(new ItemStack(blockState.getBlock()));
+            int multiplier = (prefix == OrePrefix.oreEndstone || prefix == OrePrefix.oreNetherrack) ? 2 : 1;
             ItemStack output = OreDictUnifier.get(OrePrefix.crushed, input.material);
 
             if(fortuneLevel > 0){
                 if(fortuneLevel > 3) fortuneLevel = 3;
-                output.setCount((input.material.getProperty((PropertyKey.ORE)).getOreMultiplier()) * Math.max(1, random.nextInt(fortuneLevel + 2) - 1));
+                output.setCount((input.material.getProperty((PropertyKey.ORE)).getOreMultiplier() * multiplier) * Math.max(1, random.nextInt(fortuneLevel + 2) - 1));
                 if (output.getCount() == 0) output.setCount(1);
             }
             else{
