1575fcd5de549c57f3d7f731f16cb41ca3b0a25e
Rongmario <rongmario@protonmail.com>
2022-01-20 01:31:02 +0000

    Fixed duplicate sprite registration

--- src/main/java/gregtech/common/blocks/BlockOre.java
+++ src/main/java/gregtech/common/blocks/BlockOre.java
@@ -182,14 +182,7 @@ public class BlockOre extends Block implements IBlockOre, IModelSupplier {
 
     @Override
     @SideOnly(Side.CLIENT)
-    public void onTextureStitch(TextureStitchEvent.Pre event) {
-        event.getMap().registerSprite(MaterialIconType.block.getBlockPath(material.getMaterialIconSet()));
-        for (IBlockState state : this.getBlockState().getValidStates()) {
-            StoneType stoneType = state.getValue(STONE_TYPE);
-            // event.getMap().registerSprite(stoneType.backgroundTopTexture);
-            // event.getMap().registerSprite(stoneType.backgroundSideTexture);
-        }
-    }
+    public void onTextureStitch(TextureStitchEvent.Pre event) { }
 
     @Override
     @SideOnly(Side.CLIENT)
