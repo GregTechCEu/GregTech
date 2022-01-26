089ab02d7dc7a6247b44f1215d82809c1be3f22f
ALongStringOfNumbers <31759736+ALongStringOfNumbers@users.noreply.github.com>
2022-01-25 21:14:55 -0700

    Add additional Fluid tooltips (#555)

--- src/main/java/gregtech/api/util/FluidTooltipUtil.java
+++ src/main/java/gregtech/api/util/FluidTooltipUtil.java
@@ -5,18 +5,17 @@ import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class FluidTooltipUtil {
 
     /**
      * Registry Mapping of <Fluid, Tooltip>
      */
-    private static final Map<Fluid, String> tooltips = new HashMap<>();
+    private static final Map<Fluid, List<String>> tooltips = new HashMap<>();
 
     /**
-     * Used to register a tooltip to a Fluid. A Fluid can only have one tooltip, on one line.
+     * Used to register a tooltip to a Fluid.
      * <p>
      * Ignores a tooltip applied for Water, so that it will be handled correctly for the chemical formula.
      *
@@ -25,6 +24,23 @@ public class FluidTooltipUtil {
      * @return False if either parameter is null or if tooltip is empty, true otherwise.
      */
     public static boolean registerTooltip(Fluid fluid, String tooltip) {
+        if (fluid != null && tooltip != null && !tooltip.trim().isEmpty()) {
+            tooltips.computeIfAbsent(fluid, k -> new ArrayList<>()).add(tooltip);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Used to register a tooltip to a Fluid.
+     * <p>
+     * Ignores a tooltip applied for Water, so that it will be handled correctly for the chemical formula.
+     *
+     * @param fluid   The fluid to register a tooltip for.
+     * @param tooltip The tooltip.
+     * @return False if either parameter is null or if tooltip is empty, true otherwise.
+     */
+    public static boolean registerTooltip(Fluid fluid, List<String> tooltip) {
         if (fluid != null && tooltip != null && !tooltip.isEmpty()) {
             tooltips.put(fluid, tooltip);
             return true;
@@ -38,7 +54,7 @@ public class FluidTooltipUtil {
      * @param fluid The Fluid to get the tooltip of.
      * @return The tooltip.
      */
-    public static String getFluidTooltip(Fluid fluid) {
+    public static List<String> getFluidTooltip(Fluid fluid) {
         if (fluid == null)
             return null;
 
@@ -51,7 +67,7 @@ public class FluidTooltipUtil {
      * @param stack A FluidStack, containing the Fluid to get the tooltip of.
      * @return The tooltip.
      */
-    public static String getFluidTooltip(FluidStack stack) {
+    public static List<String> getFluidTooltip(FluidStack stack) {
         if (stack == null)
             return null;
 
@@ -64,7 +80,7 @@ public class FluidTooltipUtil {
      * @param fluidName A String representing a Fluid to get the tooltip of.
      * @return The tooltip.
      */
-    public static String getFluidTooltip(String fluidName) {
+    public static List<String> getFluidTooltip(String fluidName) {
         if (fluidName == null || fluidName.isEmpty())
             return null;
 
@@ -76,7 +92,13 @@ public class FluidTooltipUtil {
      *
      * @return "H₂O"
      */
-    public static String getWaterTooltip() {
-        return Materials.Water.getChemicalFormula();
+    public static List<String> getWaterTooltip() {
+        List<String> tooltip = new ArrayList<>();
+
+        tooltip.add(Materials.Water.getChemicalFormula());
+        tooltip.add(String.valueOf(Materials.Water.getFluid().getTemperature()));
+        tooltip.add(String.valueOf(Materials.Water.getFluid().isGaseous()));
+
+        return tooltip;
     }
 }
