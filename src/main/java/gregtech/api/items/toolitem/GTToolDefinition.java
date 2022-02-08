1231ba83f978ecdd29bc296e26431824ac408d7f
Rongmario <rongmario@protonmail.com>
2022-02-08 11:43:33 +0000

    Added fallback and logging when material or tool property doesn't exist

--- src/main/java/gregtech/api/items/toolitem/GTToolDefinition.java
+++ src/main/java/gregtech/api/items/toolitem/GTToolDefinition.java
@@ -5,11 +5,14 @@ import gregtech.api.unification.material.Material;
 import gregtech.api.unification.material.Materials;
 import gregtech.api.unification.material.properties.PropertyKey;
 import gregtech.api.unification.material.properties.ToolProperty;
+import gregtech.api.util.GTLog;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.SoundEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.Nullable;
 
 /**
  * Backing of every variation of a GT Tool
@@ -18,6 +21,7 @@ public interface GTToolDefinition {
 
     IToolStats getToolStats();
 
+    @Nullable
     SoundEvent getSound();
 
     default NBTTagCompound getToolTag(ItemStack stack) {
@@ -27,11 +31,21 @@ public interface GTToolDefinition {
     default Material getToolMaterial(ItemStack stack) {
         String string = getToolTag(stack).getString("Material");
         Material material = GregTechAPI.MaterialRegistry.get(string);
-        return material != null ? material : Materials.Darmstadtium;
+        if (material == null) {
+            GTLog.logger.error("Attempt to get {} as a tool material, but material does not exist. Using Darmstadtium instead.", string);
+            material = Materials.Darmstadtium;
+        }
+        return material;
     }
 
     default ToolProperty getToolProperty(ItemStack stack) {
-        return getToolMaterial(stack).getProperty(PropertyKey.TOOL);
+        Material material = getToolMaterial(stack);
+        ToolProperty property = material.getProperty(PropertyKey.TOOL);
+        if (property == null) {
+            GTLog.logger.error("Tool property for {} does not exist. Using Darmstadtium's tool property instead.", material.getId());
+            property = Materials.Darmstadtium.getProperty(PropertyKey.TOOL);
+        }
+        return property;
     }
 
     default float getMaterialToolSpeed(ItemStack stack) {
