b9a59be7c00bd5a1e360ad5c929781fbe213bae6
YoungOnion <39562198+YoungOnionMC@users.noreply.github.com>
2022-02-15 09:10:02 -0700

    Added a overridable minumum time in ticks for the pattern checker to run

--- src/main/java/gregtech/api/metatileentity/multiblock/MultiblockControllerBase.java
+++ src/main/java/gregtech/api/metatileentity/multiblock/MultiblockControllerBase.java
@@ -72,7 +72,7 @@ public abstract class MultiblockControllerBase extends MetaTileEntity implements
     public void update() {
         super.update();
         if (!getWorld().isRemote) {
-            if (getOffsetTimer() % 20 == 0 || isFirstTick()) {
+            if (getOffsetTimer() % minimumOffsetInTicks() == 0 || isFirstTick()) {
                 checkStructurePattern();
             }
             if (isStructureFormed()) {
@@ -215,6 +215,13 @@ public abstract class MultiblockControllerBase extends MetaTileEntity implements
      */
     public boolean canShare() {
         return true;
+    }
+
+    /**
+     * Override to change the minimum time in ticks that pattern checker runs
+     */
+    public long minumumOffsetInTicks() {
+        return 20L;
     }
 
     /**
