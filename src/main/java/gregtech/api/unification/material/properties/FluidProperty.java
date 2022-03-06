494e0d71c497bcbc90592bfe114a64e2cbb92f92
TechLord22 <37029404+TechLord22@users.noreply.github.com>
2022-03-06 00:15:38 -0500

    Fix crash when other mods register fluids with negative temperatures (#747)

--- src/main/java/gregtech/api/unification/material/properties/FluidProperty.java
+++ src/main/java/gregtech/api/unification/material/properties/FluidProperty.java
@@ -70,7 +70,12 @@ public class FluidProperty implements IMaterialProperty<FluidProperty> {
     }
 
     public void setFluidTemperature(int fluidTemperature) {
-        Preconditions.checkArgument(fluidTemperature > 0, "Invalid temperature");
+        setFluidTemperature(fluidTemperature, true);
+    }
+
+    public void setFluidTemperature(int fluidTemperature, boolean isKelvin) {
+        if (isKelvin) Preconditions.checkArgument(fluidTemperature >= 0, "Invalid temperature");
+        else fluidTemperature += 273;
         this.fluidTemperature = fluidTemperature;
         if (fluid != null)
             fluid.setTemperature(fluidTemperature);
