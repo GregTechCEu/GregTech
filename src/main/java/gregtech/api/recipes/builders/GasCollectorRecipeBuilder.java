f7eec5181c5f2f53c4d5684c172aa041130766f5
brachy84 <45517902+brachy84@users.noreply.github.com>
2022-04-11 06:12:06 +0200

    fix ct crash (#868)

--- src/main/java/gregtech/api/recipes/builders/GasCollectorRecipeBuilder.java
+++ src/main/java/gregtech/api/recipes/builders/GasCollectorRecipeBuilder.java
@@ -1,5 +1,6 @@
 package gregtech.api.recipes.builders;
 
+import crafttweaker.CraftTweakerAPI;
 import gregtech.api.recipes.Recipe;
 import gregtech.api.recipes.RecipeBuilder;
 import gregtech.api.recipes.RecipeMap;
@@ -9,6 +10,7 @@ import gregtech.api.util.ValidationResult;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class GasCollectorRecipeBuilder extends RecipeBuilder<GasCollectorRecipeBuilder> {
@@ -35,7 +37,20 @@ public class GasCollectorRecipeBuilder extends RecipeBuilder<GasCollectorRecipeB
     @Override
     public boolean applyProperty(String key, Object value) {
         if (key.equals(GasCollectorDimensionProperty.KEY)) {
-            this.dimension(((List<Integer>) value).get(0));
+            if (value instanceof Integer) {
+                this.dimension((Integer) value);
+            } else if (value instanceof List && !((List<?>) value).isEmpty() && ((List<?>) value).get(0) instanceof Integer) {
+                if (this.dimensionIDs == null) {
+                    this.dimensionIDs = new ArrayList<>();
+                }
+                this.dimensionIDs.addAll((Collection<? extends Integer>) value);
+            } else {
+                if (isCTRecipe) {
+                    CraftTweakerAPI.logError("Dimension for Gas Collector needs to be a Integer");
+                    return false;
+                }
+                throw new IllegalArgumentException("Invalid Dimension Property Type!");
+            }
             return true;
         }
         return false;
