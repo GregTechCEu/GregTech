ce4b510b4ccfd5d76229984860e3569ed404a890
PrototypeTrousers <salomaoo@gmail.com>
2022-05-25 17:26:37 -0300

    misc cleaning

--- src/main/java/gregtech/api/recipes/Recipe.java
+++ src/main/java/gregtech/api/recipes/Recipe.java
@@ -289,6 +289,7 @@ public class Recipe {
         if (this.inputs.size() != otherRecipe.inputs.size()) return false;
         for (int i = 0; i < inputs.size(); i++) {
             if (!inputs.get(i).isOreDict() && !otherRecipe.inputs.get(i).isOreDict()) {
+                if (inputs.get(i).getInputStacks().length != otherRecipe.inputs.get(i).getInputStacks().length) return false;
                 for (int j = 0; j < inputs.get(i).getInputStacks().length; j++) {
                     if (!hashStrategy.equals(this.inputs.get(i).getInputStacks()[j],
                             otherRecipe.inputs.get(i).getInputStacks()[j])) {
--- src/main/java/gregtech/common/command/CommandRecipeCheck.java
+++ src/main/java/gregtech/common/command/CommandRecipeCheck.java
@@ -31,7 +31,6 @@ import net.minecraftforge.oredict.OreDictionary;
 
 import javax.annotation.Nonnull;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
