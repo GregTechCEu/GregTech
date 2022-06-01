4fdfef151c5197aa201ad999361f8b987d117c82
brachy84 <brachy84@gmail.com>
2022-06-01 11:24:27 +0200

    improve some code

--- src/main/java/gregtech/api/gui/GregTechUI.java
+++ src/main/java/gregtech/api/gui/GregTechUI.java
@@ -1,12 +1,8 @@
 package gregtech.api.gui;
 
-import com.cleanroommc.modularui.api.screen.ModularUIContext;
-import com.cleanroommc.modularui.api.screen.ModularWindow;
-import com.cleanroommc.modularui.api.screen.UIBuildContext;
+import com.cleanroommc.modularui.ModularUI;
 import com.cleanroommc.modularui.common.builder.UIBuilder;
 import com.cleanroommc.modularui.common.builder.UIInfo;
-import com.cleanroommc.modularui.common.internal.wrapper.ModularGui;
-import com.cleanroommc.modularui.common.internal.wrapper.ModularUIContainer;
 import gregtech.api.capability.GregtechTileCapabilities;
 import gregtech.api.cover.CoverBehavior;
 import gregtech.api.cover.CoverWithUI;
@@ -16,14 +12,11 @@ import gregtech.api.items.metaitem.MetaItem;
 import gregtech.api.items.metaitem.stats.IItemBehaviour;
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.MetaTileEntityHolder;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
-
-import java.util.function.Function;
 
 public class GregTechUI {
 
@@ -38,19 +31,13 @@ public class GregTechUI {
         return PLAYER_ITEM_UI[hand.ordinal()];
     }
 
-    private static ModularUIContainer createModularContainer(EntityPlayer player, Function<UIBuildContext, ModularWindow> windowCreator) {
-        UIBuildContext buildContext = new UIBuildContext(player);
-        ModularWindow window = windowCreator.apply(buildContext);
-        return new ModularUIContainer(new ModularUIContext(buildContext), window);
-    }
-
     public static final UIInfo<?, ?> MTE_UI = UIBuilder.of()
             .gui(((player, world, x, y, z) -> {
                 TileEntity te = world.getTileEntity(new BlockPos(x, y, z));
                 if (te instanceof MetaTileEntityHolder && !te.isInvalid()) {
                     MetaTileEntity mte = ((MetaTileEntityHolder) te).getMetaTileEntity();
                     if (mte != null) {
-                        return new ModularGui(createModularContainer(player, mte::createWindow));
+                        return ModularUI.createGuiScreen(player, mte::createWindow);
                     }
                 }
                 return null;
@@ -60,7 +47,7 @@ public class GregTechUI {
                 if (te instanceof MetaTileEntityHolder && !te.isInvalid()) {
                     MetaTileEntity mte = ((MetaTileEntityHolder) te).getMetaTileEntity();
                     if (mte != null) {
-                        return createModularContainer(player, mte::createWindow);
+                        return ModularUI.createContainer(player, mte::createWindow);
                     }
                 }
                 return null;
@@ -76,7 +63,7 @@ public class GregTechUI {
                             for (IItemBehaviour behaviour : ((MetaItem<?>) item.getItem()).getBehaviours(item)) {
                                 if (behaviour instanceof ItemUIFactory) {
                                     ItemUIFactory uiFactory = (ItemUIFactory) behaviour;
-                                    return new ModularGui(createModularContainer(player, uiFactory::createWindow));
+                                    return ModularUI.createGuiScreen(player, buildContext -> uiFactory.createWindow(buildContext, item));
                                 }
                             }
                         }
@@ -88,7 +75,7 @@ public class GregTechUI {
                             for (IItemBehaviour behaviour : ((MetaItem<?>) item.getItem()).getBehaviours(item)) {
                                 if (behaviour instanceof ItemUIFactory) {
                                     ItemUIFactory uiFactory = (ItemUIFactory) behaviour;
-                                    return createModularContainer(player, uiFactory::createWindow);
+                                    return ModularUI.createContainer(player, buildContext -> uiFactory.createWindow(buildContext, item));
                                 }
                             }
                         }
@@ -109,7 +96,7 @@ public class GregTechUI {
                         CoverBehavior cover = coverable.getCoverAtSide(facing);
                         if (cover instanceof CoverWithUI) {
                             CoverWithUI coverWithUI = (CoverWithUI) cover;
-                            return new ModularGui(createModularContainer(player, coverWithUI::createWindow));
+                            return ModularUI.createGuiScreen(player, coverWithUI::createWindow);
                         }
                         return null;
                     }))
@@ -125,7 +112,7 @@ public class GregTechUI {
                         CoverBehavior cover = coverable.getCoverAtSide(facing);
                         if (cover instanceof CoverWithUI) {
                             CoverWithUI coverWithUI = (CoverWithUI) cover;
-                            return createModularContainer(player, coverWithUI::createWindow);
+                            return ModularUI.createContainer(player, coverWithUI::createWindow);
                         }
                         return null;
                     })
--- src/main/java/gregtech/api/items/gui/ItemUIFactory.java
+++ src/main/java/gregtech/api/items/gui/ItemUIFactory.java
@@ -5,6 +5,7 @@ import com.cleanroommc.modularui.api.screen.UIBuildContext;
 import gregtech.api.guiOld.ModularUI;
 import gregtech.api.items.metaitem.stats.IItemComponent;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 
 public interface ItemUIFactory extends IItemComponent {
 
@@ -12,9 +13,12 @@ public interface ItemUIFactory extends IItemComponent {
      * Creates new UI basing on given holder. Holder contains information
      * about item stack and hand, and also player
      */
-    ModularUI createUI(PlayerInventoryHolder holder, EntityPlayer entityPlayer);
+    @Deprecated
+    default ModularUI createUI(PlayerInventoryHolder holder, EntityPlayer entityPlayer) {
+        return null;
+    }
 
-    default ModularWindow createWindow(UIBuildContext buildContext) {
+    default ModularWindow createWindow(UIBuildContext buildContext, ItemStack item) {
         return null;
     }
 
