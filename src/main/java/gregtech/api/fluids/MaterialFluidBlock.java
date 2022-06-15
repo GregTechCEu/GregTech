3b655ec66ff6d21a6818898f35a8f2067f0f7d14
TechLord22 <37029404+TechLord22@users.noreply.github.com>
2022-06-15 18:06:45 -0400

    fix explosions, correct submerged overlay

--- src/main/java/gregtech/api/fluids/MaterialFluidBlock.java
+++ src/main/java/gregtech/api/fluids/MaterialFluidBlock.java
@@ -3,6 +3,7 @@ package gregtech.api.fluids;
 import gregtech.api.GTValues;
 import gregtech.api.unification.material.Material;
 import gregtech.api.unification.material.info.MaterialFlags;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockFire;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
@@ -21,10 +22,16 @@ import javax.annotation.Nullable;
 public class MaterialFluidBlock extends BlockFluidClassic {
 
     private final Material gtMaterial;
+    private final boolean isFlammable;
+    private final boolean isExplosive;
+    private final boolean isSticky;
 
-    public MaterialFluidBlock(Fluid fluid, net.minecraft.block.material.Material material, Material gtMaterial) {
+    public MaterialFluidBlock(@Nonnull Fluid fluid, @Nonnull net.minecraft.block.material.Material material, @Nonnull Material gtMaterial) {
         super(fluid, material);
         this.gtMaterial = gtMaterial;
+        this.isFlammable = gtMaterial.hasFlag(MaterialFlags.FLAMMABLE);
+        this.isExplosive = gtMaterial.hasFlag(MaterialFlags.EXPLOSIVE);
+        this.isSticky = gtMaterial.hasFlag(MaterialFlags.STICKY);
 
         boolean displaces = fluid.getDensity() > 1000; // water density
         this.displacements.put(Blocks.WATER, displaces);
@@ -44,22 +51,29 @@ public class MaterialFluidBlock extends BlockFluidClassic {
 
     @Override
     public int getFlammability(@Nonnull IBlockAccess world, @Nonnull BlockPos pos, @Nonnull EnumFacing face) {
-        return this.gtMaterial.hasFlag(MaterialFlags.FLAMMABLE) ? 200 : 0;
+        return this.isFlammable ? 200 : 0;
     }
 
     @Override
-    public void onNeighborChange(@Nonnull IBlockAccess blockAccess, @Nonnull BlockPos pos, @Nonnull BlockPos neighbor) {
-        if (this.gtMaterial.hasFlag(MaterialFlags.EXPLOSIVE) && blockAccess instanceof World &&
-                blockAccess.getBlockState(neighbor).getBlock() instanceof BlockFire) {
-            if (GTValues.RNG.nextInt(10) == 0) {
-                ((World) blockAccess).setBlockToAir(pos);
-                ((World) blockAccess).createExplosion(null, pos.getX() + 0.5F, pos.getY() + 0.5F, pos.getZ() + 0.5F, 1.0F, true);
+    public void neighborChanged(@Nonnull IBlockState state, @Nonnull World world, @Nonnull BlockPos pos, @Nonnull Block neighborBlock, @Nonnull BlockPos neighbourPos) {
+        super.neighborChanged(state, world, pos, neighborBlock, neighbourPos);
+        if (this.isExplosive) {
+            // explosive fluids are unstable, can just explode whenever disrupted
+            // flammable explosives are way more likely to blow up
+            if (GTValues.RNG.nextInt(this.isFlammable && neighborBlock instanceof BlockFire ? 5 : 10) == 0) {
+                world.setBlockToAir(pos);
+                world.createExplosion(null, pos.getX() + 0.5F, pos.getY() + 0.5F, pos.getZ() + 0.5F, 3.0F, true);
             }
         }
     }
 
     @Override
     public void onEntityCollision(@Nonnull World worldIn, @Nonnull BlockPos pos, @Nonnull IBlockState state, @Nonnull Entity entityIn) {
-        if (this.gtMaterial.hasFlag(MaterialFlags.STICKY)) entityIn.setInWeb();
+        if (this.isSticky) entityIn.setInWeb();
+    }
+
+    @Nonnull
+    public Material getGTMaterial() {
+        return this.gtMaterial;
     }
 }
