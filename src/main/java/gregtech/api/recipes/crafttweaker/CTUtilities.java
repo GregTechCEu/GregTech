c1d6d33763f57cbbf3062a1f6d41f7114bb2fce0
Gary Bryson Luis Jr <garybluis@outlook.com>
2022-07-18 17:46:06 +0800

    Add extra ZenMethod to get meta item.

--- src/main/java/gregtech/api/recipes/crafttweaker/CTUtilities.java
+++ src/main/java/gregtech/api/recipes/crafttweaker/CTUtilities.java
@@ -1,6 +1,7 @@
 package gregtech.api.recipes.crafttweaker;
 
 import crafttweaker.annotations.ZenRegister;
+import crafttweaker.api.item.IItemStack;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
 
@@ -13,5 +14,15 @@ public class CTUtilities {
     @ZenMethod("RemoveMultiblockPreviewFromJei")
     public static void removeMulti(String name) {
         REGISTER.removeIf(multi -> multi.metaTileEntityId.toString().equals(name));
+    }
+
+    @ZenMethod("GetMetaItem")
+    public static IItemStack getMetaItem(String metaId) {
+        return MetaItemBracketHandler.getMetaItem(metaId);
+    }
+
+    @ZenMethod("GetMetaTileEntity")
+    public static IItemStack getMetaTileEntityItem(String metaId) {
+        return MetaTileEntityBracketHandler.getMetaTileEntityItem(metaId);
     }
 }
