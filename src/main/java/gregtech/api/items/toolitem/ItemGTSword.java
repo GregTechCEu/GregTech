a8ac6272e5df2c1f3965cf144bc21faeb9936da8
Tech22 <37029404+TechLord22@users.noreply.github.com>
2022-09-10 16:27:24 -0400

    fix electric tool use when charging

--- src/main/java/gregtech/api/items/toolitem/ItemGTSword.java
+++ src/main/java/gregtech/api/items/toolitem/ItemGTSword.java
@@ -1,7 +1,6 @@
 package gregtech.api.items.toolitem;
 
 import com.google.common.collect.Multimap;
-import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
@@ -13,21 +12,23 @@ import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.Collections;
 import java.util.Set;
 
 public class ItemGTSword extends ItemSword implements IGTTool {
 
-    private final String domain, id;
+    private final String domain;
+    private final String id;
 
     private final int tier;
     private final IGTToolDefinition toolStats;
     private final Set<String> toolClasses;
     private final SoundEvent sound;
-    private final Set<Block> effectiveBlocks;
+    private final Set<IBlockState> effectiveBlocks;
 
-    protected ItemGTSword(String domain, String id, int tier, IGTToolDefinition toolStats, SoundEvent sound, Set<String> toolClasses, Set<Block> effectiveBlocks) {
+    protected ItemGTSword(String domain, String id, int tier, IGTToolDefinition toolStats, SoundEvent sound, Set<String> toolClasses, Set<IBlockState> effectiveBlocks) {
         super(ToolMaterial.STONE);
         this.domain = domain;
         this.id = id;
@@ -76,48 +77,51 @@ public class ItemGTSword extends ItemSword implements IGTTool {
     }
 
     @Override
-    public float getDestroySpeed(ItemStack stack, IBlockState state) {
+    public float getDestroySpeed(@Nonnull ItemStack stack, @Nonnull IBlockState state) {
         return definition$getDestroySpeed(stack, state);
     }
 
     @Override
-    public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker) {
+    public boolean hitEntity(@Nonnull ItemStack stack, @Nonnull EntityLivingBase target, @Nonnull EntityLivingBase attacker) {
         return definition$hitEntity(stack, target, attacker);
     }
 
     @Override
-    public boolean onBlockDestroyed(ItemStack stack, World worldIn, IBlockState state, BlockPos pos, EntityLivingBase entityLiving) {
+    public boolean onBlockDestroyed(@Nonnull ItemStack stack, @Nonnull World worldIn, @Nonnull IBlockState state, @Nonnull BlockPos pos, @Nonnull EntityLivingBase entityLiving) {
         return definition$onBlockDestroyed(stack, worldIn, state, pos, entityLiving);
     }
 
     @Override
-    public int getItemEnchantability(ItemStack stack) {
+    public int getItemEnchantability(@Nonnull ItemStack stack) {
         return getTotalEnchantability(stack);
     }
 
     @Override
-    public boolean getIsRepairable(ItemStack toRepair, ItemStack repair) {
+    public boolean getIsRepairable(@Nonnull ItemStack toRepair, @Nonnull ItemStack repair) {
         return definition$getIsRepairable(toRepair, repair);
     }
 
+    @Nonnull
     @Override
-    public Multimap<String, AttributeModifier> getAttributeModifiers(EntityEquipmentSlot slot, ItemStack stack) {
+    public Multimap<String, AttributeModifier> getAttributeModifiers(@Nonnull EntityEquipmentSlot slot, @Nonnull ItemStack stack) {
         return definition$getAttributeModifiers(slot, stack);
     }
 
     @Override
-    public int getHarvestLevel(ItemStack stack, String toolClass, @Nullable EntityPlayer player, @Nullable IBlockState blockState) {
+    public int getHarvestLevel(@Nonnull ItemStack stack, @Nonnull String toolClass, @Nullable EntityPlayer player, @Nullable IBlockState blockState) {
         return definition$getHarvestLevel(stack, toolClass, player, blockState);
     }
 
+    @Nonnull
     @Override
-    public Set<String> getToolClasses(ItemStack stack) {
+    public Set<String> getToolClasses(@Nonnull ItemStack stack) {
         return toolClasses;
     }
 
     @Override
-    public boolean canHarvestBlock(IBlockState blockIn, ItemStack stack) {
-        return getToolClasses(stack).stream().anyMatch(s -> blockIn.getBlock().isToolEffective(s, blockIn)) || super.canHarvestBlock(blockIn, stack);
+    public boolean canHarvestBlock(@Nonnull IBlockState blockIn, @Nonnull ItemStack stack) {
+        return effectiveBlocks.contains(blockIn) ||
+                getToolClasses(stack).stream().anyMatch(s -> blockIn.getBlock().isToolEffective(s, blockIn)) ||
+                super.canHarvestBlock(blockIn, stack);
     }
-
 }
