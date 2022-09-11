d9b92f3252a1c43b7a362d4f4e1e12b9f382d951
lordclips <72330305+lordclips@users.noreply.github.com>
2022-09-11 14:17:07 -0400

    Fix Configurable Maintenance Hatch not always applying bonuses (#1185)

--- src/main/java/gregtech/api/capability/impl/HeatingCoilRecipeLogic.java
+++ src/main/java/gregtech/api/capability/impl/HeatingCoilRecipeLogic.java
@@ -4,6 +4,7 @@ import gregtech.api.capability.IHeatingCoil;
 import gregtech.api.metatileentity.multiblock.RecipeMapMultiblockController;
 import gregtech.api.recipes.recipeproperties.IRecipePropertyStorage;
 import gregtech.api.recipes.recipeproperties.TemperatureProperty;
+import net.minecraft.util.Tuple;
 
 import javax.annotation.Nonnull;
 
@@ -21,9 +22,12 @@ public class HeatingCoilRecipeLogic extends MultiblockRecipeLogic {
 
     @Override
     protected int[] runOverclockingLogic(@Nonnull IRecipePropertyStorage propertyStorage, int recipeEUt, long maxVoltage, int duration, int maxOverclocks) {
+        // apply maintenance penalties
+        Tuple<Integer, Double> maintenanceValues = getMaintenanceValues();
+
         return heatingCoilOverclockingLogic(Math.abs(recipeEUt),
                 maxVoltage,
-                duration,
+                (int) Math.round(duration * maintenanceValues.getSecond()),
                 maxOverclocks,
                 ((IHeatingCoil) metaTileEntity).getCurrentTemperature(),
                 propertyStorage.getRecipePropertyValue(TemperatureProperty.getInstance(), 0)
