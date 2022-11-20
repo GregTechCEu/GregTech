dc88c59a00689cb7d4e3339c33e79c5158f2c6ea
Dane Strandboge <dane@strandboge.com>
2022-11-20 02:18:37 -0600

    rename `core` package to `asm` (#1280)

---
+++ src/main/java/gregtech/asm/GregTechLoadingPlugin.java
@@ -0,0 +1,42 @@
+package gregtech.asm;
+
+import net.minecraftforge.common.ForgeVersion;
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin.MCVersion;
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin.Name;
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin.SortingIndex;
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin.TransformerExclusions;
+
+import javax.annotation.Nullable;
+import java.util.Map;
+
+@Name("GregTechLoadingPlugin")
+@MCVersion(ForgeVersion.mcVersion)
+@TransformerExclusions("gregtech.asm.")
+@SortingIndex(1001)
+public class GregTechLoadingPlugin implements IFMLLoadingPlugin {
+    @Override
+    public String[] getASMTransformerClass() {
+        return new String[]{"gregtech.asm.GregTechTransformer"};
+    }
+
+    @Override
+    public String getModContainerClass() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data) {
+    }
+
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
+    }
+}
