9b9bb1f4cc07155d62211912be5ca9511cbf81db
DStrand1 <dane@strandboge.com>
2022-12-22 17:52:43 -0600

    2.4.5

--- src/main/java/gregtech/GregTechVersion.java
+++ src/main/java/gregtech/GregTechVersion.java
@@ -7,7 +7,7 @@ public final class GregTechVersion {
     //This number is incremented every major feature update
     public static final int MINOR = 4;
     //This number is incremented every time the feature is added, or bug is fixed. resets every major version change
-    public static final int REVISION = 4;
+    public static final int REVISION = 5;
     //This String is for additional info on the release version if needed (like alpha, beta, rc). Comment out when unused
     public static final String EXTRA = "beta";
     //This is the String formatted version, used in builds
