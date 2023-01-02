052d907e0637421bc24ed24a5152584113142cb1
Tech22 <37029404+TechLord22@users.noreply.github.com>
2023-01-02 03:07:33 -0500

    use nbt for calculation in resultGTTool

--- src/main/java/gregtech/asm/hooks/RecipeRepairItemHooks.java
+++ src/main/java/gregtech/asm/hooks/RecipeRepairItemHooks.java
@@ -6,7 +6,6 @@ import gregtech.api.items.toolitem.ToolHelper;
 import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.world.World;
 
 import java.util.List;
 
@@ -55,6 +54,7 @@ public class RecipeRepairItemHooks {
                     if (itemstack.getItem() != stack.getItem()) return ItemStack.EMPTY;
                     if (stack.getCount() != 1 || itemstack.getCount() != 1) return ItemStack.EMPTY;
                     if (!stack.getItem().isRepairable()) return ItemStack.EMPTY;
+                    if (!(itemstack.getItem() instanceof IGTTool) || !(stack.getItem() instanceof IGTTool)) return ItemStack.EMPTY;
                 }
             }
         }
@@ -64,23 +64,39 @@ public class RecipeRepairItemHooks {
             ItemStack second = list.get(1);
 
             if (first.getItem() == second.getItem() && first.getCount() == 1 && second.getCount() == 1 && first.getItem().isRepairable()) {
-                int j = first.getMaxDamage() - first.getItemDamage();
-                int k = first.getMaxDamage() - second.getItemDamage();
-                int l = j + k + first.getMaxDamage() * 5 / 100;
-                int i1 = first.getMaxDamage() - l;
-
-                if (i1 < 0) {
-                    i1 = 0;
-                }
-
                 if (first.getItem() instanceof IGTTool && second.getItem() instanceof IGTTool) {
-                    ItemStack output = first.copy();
-                    NBTTagCompound toolTag = ToolHelper.getToolTag(output);
-                    toolTag.setInteger(ToolHelper.DURABILITY_KEY, i1);
-                    return output;
-                }
+                    NBTTagCompound firstTag = ToolHelper.getToolTag(first);
+                    NBTTagCompound secondTag = ToolHelper.getToolTag(second);
 
-                return new ItemStack(first.getItem(), 1, i1);
+                    int firstMaxDurability = firstTag.getInteger(ToolHelper.MAX_DURABILITY_KEY);
+                    int firstDurability = firstTag.getInteger(ToolHelper.DURABILITY_KEY);
+
+                    int secondDurability = secondTag.getInteger(ToolHelper.DURABILITY_KEY);
+
+                    int j = firstTag.getInteger(ToolHelper.MAX_DURABILITY_KEY) - firstDurability;
+                    int k = firstMaxDurability - secondDurability;
+                    int l = j + k + firstMaxDurability * 5 / 100;
+                    int i1 = firstMaxDurability - l;
+
+                    if (i1 < 0) i1 = 0;
+
+                    IGTTool tool = (IGTTool) first.getItem();
+                    ItemStack output = tool.get(tool.getToolMaterial(first));
+
+                    NBTTagCompound outputTag = ToolHelper.getToolTag(output);
+                    outputTag.setInteger(ToolHelper.DURABILITY_KEY, i1);
+                    return output;
+                } else {
+                    int j = first.getMaxDamage() - first.getItemDamage();
+                    int k = first.getMaxDamage() - second.getItemDamage();
+                    int l = j + k + first.getMaxDamage() * 5 / 100;
+                    int i1 = first.getMaxDamage() - l;
+
+                    if (i1 < 0) {
+                        i1 = 0;
+                    }
+                    return new ItemStack(first.getItem(), 1, i1);
+                }
             }
         }
 
