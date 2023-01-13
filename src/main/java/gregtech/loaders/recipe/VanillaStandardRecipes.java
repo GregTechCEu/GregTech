d474c58ffb3748968b77b6a337201fb805e0ae03
xnossisx <xnossisx@gmail.com>
2023-01-12 21:29:58 -0600

    Fixed issue where stone rod recipe did not exist; I also made sure to include consideration of the harder rods setting.

--- src/main/java/gregtech/loaders/recipe/VanillaStandardRecipes.java
+++ src/main/java/gregtech/loaders/recipe/VanillaStandardRecipes.java
@@ -377,6 +377,21 @@ public class VanillaStandardRecipes {
                 .duration(50).EUt(VA[ULV])
                 .buildAndRegister();
 
+        if (ConfigHolder.recipes.harderRods) {
+            LATHE_RECIPES.recipeBuilder()
+                    .inputs(new ItemStack(Blocks.COBBLESTONE))
+                    .output(OrePrefix.stick, Materials.Stone, 1)
+                    .output(dustSmall, Stone, 2)
+                    .duration(20).EUt(VA[ULV])
+                    .buildAndRegister();
+        } else {
+            LATHE_RECIPES.recipeBuilder()
+                    .inputs(new ItemStack(Blocks.COBBLESTONE))
+                    .output(OrePrefix.stick, Materials.Stone, 2)
+                    .duration(20).EUt(VA[ULV])
+                    .buildAndRegister();
+        }
+
         ASSEMBLER_RECIPES.recipeBuilder()
                 .input(plank, Wood, 6)
                 .inputs(new ItemStack(Items.BOOK, 3))
