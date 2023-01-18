156dabfcc4ae7310161c0bfea7eabe32d9b9a482
tracer4b <61507029+tracer4b@users.noreply.github.com>
2023-01-18 21:22:10 +0800

    looping purified dust in flotation

--- src/main/java/gregtech/loaders/recipe/handlers/oreproc/PureDustRecipeHandler.java
+++ src/main/java/gregtech/loaders/recipe/handlers/oreproc/PureDustRecipeHandler.java
@@ -31,14 +31,25 @@ public class PureDustRecipeHandler {
                 .chancedOutput(dust, material, property.getOreMultiplier(), 5000, 0)
                 .duration(400).EUt(VA[ULV]).buildAndRegister();
 
-        // TODO flotation fluid
         // Flotation recipe
-        // Purified Dust -> 2 Dust + 0.5 3rd Byproduct (4 Dust per Ore)
-        FLOTATION_RECIPES.recipeBuilder()
-                .input(dustPure, material)
-                .output(dust, material, 2 * property.getOreMultiplier())
-                .chancedOutput(dust, byproduct, byproductMultiplier, 5000, 0)
-                .fluidOutputs(SluiceJuice.getFluid(1500))
-                .buildAndRegister();
+        if (byproduct.hasProperty(PropertyKey.ORE)) {
+            // Purified Dust -> 2 Dust + 0.15 3rd Byproduct Purified Dust (4 Dust per Ore)
+            FLOTATION_RECIPES.recipeBuilder()
+                    .input(dustPure, material)
+                    .fluidInputs(SodiumEthylXanthate.getFluid(1000))
+                    .output(dust, material, 2 * property.getOreMultiplier())
+                    .chancedOutput(dustPure, byproduct, byproductMultiplier, 1500, 0)
+                    .fluidOutputs(SluiceJuice.getFluid(1500))
+                    .buildAndRegister();
+        } else {
+            // Purified Dust -> 2 Dust + 0.5 3rd Byproduct (4 Dust per Ore)
+            FLOTATION_RECIPES.recipeBuilder()
+                    .input(dustPure, material)
+                    .fluidInputs(SodiumEthylXanthate.getFluid(1000))
+                    .output(dust, material, 2 * property.getOreMultiplier())
+                    .chancedOutput(dust, byproduct, byproductMultiplier, 5000, 0)
+                    .fluidOutputs(SluiceJuice.getFluid(1500))
+                    .buildAndRegister();
+        }
     }
 }
