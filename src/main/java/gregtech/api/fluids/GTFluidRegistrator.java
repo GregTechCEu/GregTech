3946e8e24269120e4d6f1a8f33f55b2367650085
Tech22 <37029404+TechLord22@users.noreply.github.com>
2023-01-24 16:54:25 -0500

    refactor GTFluidRegistrator

--- src/main/java/gregtech/api/fluids/GTFluidRegistrator.java
+++ src/main/java/gregtech/api/fluids/GTFluidRegistrator.java
@@ -19,34 +19,45 @@ import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fluids.BlockFluidBase;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidRegistry;
+import net.minecraftforge.fml.common.eventhandler.Event;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
+/**
+ * Handles registration and creation of all GregTech Fluids
+ */
 public final class GTFluidRegistrator {
 
     private static final Collection<ResourceLocation> fluidSprites = new ObjectOpenHashSet<>();
-    private static final Map<String, String> alternativeFluidNames = new Object2ObjectOpenHashMap<>();
-    private static final Map<String, Material> fluidToMaterialMappings = new Object2ObjectOpenHashMap<>();
+    private static final Map<String, Set<String>> alternativeFluidNames = new Object2ObjectOpenHashMap<>();
+    private static final Map<String, Material> materialByFluidName = new Object2ObjectOpenHashMap<>();
 
     private GTFluidRegistrator() {/**/}
 
-    public static void registerSprites(TextureMap textureMap) {
+    public static void registerSprites(@Nonnull TextureMap textureMap) {
         for (ResourceLocation spriteLocation : fluidSprites) {
             textureMap.registerSprite(spriteLocation);
         }
     }
 
+    /**
+     * Internal use only. Initializes the registrator.
+     */
     public static void init() {
         overrideMaterialFluid(Materials.Water, FluidTypes.LIQUID, FluidRegistry.WATER);
         overrideMaterialFluid(Materials.Lava, FluidTypes.LIQUID, FluidRegistry.LAVA);
+
+        MinecraftForge.EVENT_BUS.post(new MaterialFluidOverrideEvent());
+
+        addAlternativeNames();
+
+        MinecraftForge.EVENT_BUS.post(new AlternativeFluidNameEvent());
 
         registerMaterialFluids();
     }
@@ -54,45 +65,72 @@ public final class GTFluidRegistrator {
     /**
      * Override a material's fluid with another
      *
-     * @param material the material whose fluid should be overriden
+     * @param material the material owning the fluid to override
      * @param type     the type corresponding to the fluid
      * @param fluid    the fluid to use
      */
     public static void overrideMaterialFluid(@Nonnull Material material, @Nonnull FluidType type, @Nonnull Fluid fluid) {
         material.getProperty(PropertyKey.FLUID).setFluid(type, fluid);
-        createMaterialFluidTooltip(material, fluid);
+        FluidTooltipUtil.registerTooltip(fluid, createMaterialFluidTooltip(material, fluid));
     }
 
     private static void addAlternativeNames() {
-        setAlternativeFluidName(Materials.Ethanol, FluidTypes.LIQUID, "bio.ethanol");
-        setAlternativeFluidName(Materials.SeedOil, FluidTypes.LIQUID, "seed.oil");
-        setAlternativeFluidName(Materials.Ice, FluidTypes.LIQUID, "fluid.ice");
-        setAlternativeFluidName(Materials.Diesel, FluidTypes.LIQUID, "fuel");
+        addAlternativeFluidName(Materials.Ethanol, FluidTypes.LIQUID, "bio.ethanol");
+        addAlternativeFluidName(Materials.SeedOil, FluidTypes.LIQUID, "seed.oil");
+        addAlternativeFluidName(Materials.Ice, FluidTypes.LIQUID, "fluid.ice");
+        addAlternativeFluidName(Materials.Diesel, FluidTypes.LIQUID, "fuel");
     }
 
     /**
-     * Set an alternative name for a fluid, to use if found instead of the default
+     * Add an alternative name for a fluid, to use if found instead of the default
      *
      * @param material        the material whose fluid to get
      * @param type            the type for the fluid
      * @param alternativeName the alternative name
      */
-    public static void setAlternativeFluidName(@Nonnull Material material, @Nonnull FluidType type, @Nonnull String alternativeName) {
-        setAlternativeFluidName(type.getFluidNameForMaterial(material), alternativeName);
+    public static void addAlternativeFluidName(@Nonnull Material material, @Nonnull FluidType type, @Nonnull String alternativeName) {
+        addAlternativeFluidName(type.getFluidNameForMaterial(material), alternativeName);
     }
 
     /**
-     * Set an alternative name for a fluid, to use if found instead of the default
+     * Add an alternative name for a fluid, to use if found instead of the default
      *
      * @param registeredName  the default name of the fluid
      * @param alternativeName the alternative name
      */
-    public static void setAlternativeFluidName(@Nonnull String registeredName, @Nonnull String alternativeName) {
-        alternativeFluidNames.put(registeredName, alternativeName);
+    public static void addAlternativeFluidName(@Nonnull String registeredName, @Nonnull String alternativeName) {
+        Set<String> set = alternativeFluidNames.get(registeredName);
+        if (set == null) {
+            set = new ObjectOpenHashSet<>();
+            alternativeFluidNames.put(registeredName, set);
+        }
+        set.add(alternativeName);
     }
 
     /**
-     * Registers the fluid texture for use
+     * Remove all alternative names for a fluid
+     *
+     * @param registeredName the fluid's registered name
+     */
+    @SuppressWarnings("unused")
+    public static void removeAlternativeFluidName(@Nonnull String registeredName) {
+        alternativeFluidNames.remove(registeredName);
+    }
+
+    /**
+     * Remove an alternative name for a fluid
+     *
+     * @param registeredName  the fluid's registered name
+     * @param alternativeName the name to remove
+     */
+    @SuppressWarnings("unused")
+    public static void removeAlternativeFluidName(@Nonnull String registeredName, @Nonnull String alternativeName) {
+        Set<String> set = alternativeFluidNames.get(registeredName);
+        if (set != null) set.remove(alternativeName);
+    }
+
+    /**
+     * Registers a fluid texture for use
      *
      * @param textureLocation the location of the texture to use
      */
@@ -100,28 +138,70 @@ public final class GTFluidRegistrator {
         fluidSprites.add(textureLocation);
     }
 
+    /**
+     * @param fluidName the name of the fluid
+     * @return the fluid associated with the name
+     */
+    @Nullable
+    public static Fluid getExistingFluid(@Nonnull String fluidName) {
+        // if the fluid already exists
+        Fluid fluid = FluidRegistry.getFluid(fluidName);
+        if (fluid != null) return fluid;
+
+        // check if fluid is registered from elsewhere under an alternative name, if not already found
+        if (alternativeFluidNames.containsKey(fluidName)) {
+            for (String altName : alternativeFluidNames.get(fluidName)) {
+                // if the fluid already exists
+                fluid = FluidRegistry.getFluid(altName);
+                if (fluid != null) return fluid;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Create a standard Fluid compatible with the GT Fluid System from a definition.
+     *
+     * @param registryName the registry name for the fluid
+     * @param definition   the definition of the fluid
+     * @return the fluid
+     */
+    @SuppressWarnings("unused")
+    @Nonnull
+    public static Fluid createFluid(@Nonnull String registryName, @Nonnull FluidDefinition definition) {
+        // if the fluid already exists, exit
+        Fluid existing = getExistingFluid(registryName);
+        if (existing != null) return existing;
+
+        registerFluidTexture(definition.getStill());
+        registerFluidTexture(definition.getFlowing());
+
+        Fluid fluid = definition.constructFluid(registryName);
+        FluidTooltipUtil.registerTooltip(fluid, createFluidTooltip(fluid));
+
+        return fluid;
+    }
+
     private static void registerMaterialFluids() {
         for (Material material : GregTechAPI.MATERIAL_REGISTRY) {
             FluidProperty fluidProperty = material.getProperty(PropertyKey.FLUID);
             if (fluidProperty == null) continue;
-            createMaterialFluid(material, fluidProperty);
+            registerMaterialFluid(material, fluidProperty);
         }
     }
 
-    private static void createMaterialFluid(@Nonnull Material material, @Nonnull FluidProperty property) {
+    /**
+     * Register and create all of a material's fluids
+     *
+     * @param material the material owning the fluid
+     * @param property the FluidProperty of the material
+     */
+    public static void registerMaterialFluid(@Nonnull Material material, @Nonnull FluidProperty property) {
         for (MaterialFluidDefinition definition : property.getDefinitions()) {
-
             String fluidName = definition.getType().getFluidNameForMaterial(material);
 
-            // if the fluid already exists, exit
-            if (FluidRegistry.getFluid(fluidName) != null) return;
-
-            // check if fluid is registered from elsewhere under an alternative name, if not already found
-            if (alternativeFluidNames.containsKey(fluidName)) {
-                String altName = alternativeFluidNames.get(fluidName);
-                // if the fluid already exists, exit
-                if (FluidRegistry.getFluid(altName) != null) return;
-            }
+            // if the fluid already exists, skip it
+            if (getExistingFluid(fluidName) != null) continue;
 
             // construct the fluid
             Fluid fluid = definition.constructFluid(material, fluidName);
@@ -149,7 +229,7 @@ public final class GTFluidRegistrator {
                 MetaBlocks.FLUID_BLOCKS.add(fluidBlock);
             }
 
-            fluidToMaterialMappings.put(fluid.getName(), material);
+            materialByFluidName.put(fluid.getName(), material);
         }
     }
 
@@ -185,8 +265,32 @@ public final class GTFluidRegistrator {
 
     @Nullable
     public static Material getMaterialFromFluid(@Nonnull Fluid fluid) {
-        Material material = fluidToMaterialMappings.get(fluid.getName());
-        if (material.hasFluid()) return material;
+        Material material = materialByFluidName.get(fluid.getName());
+        if (material != null && material.hasFluid()) return material;
         return null;
+    }
+
+    /**
+     * Called during the fluid registration phase, where fluids for materials can be overridden with another.
+     * <br>
+     * Use {@link GTFluidRegistrator#overrideMaterialFluid(Material, FluidType, Fluid)} to add overrides.
+     */
+    public static class MaterialFluidOverrideEvent extends Event {
+        public MaterialFluidOverrideEvent() {/**/}
+    }
+
+    /**
+     * Called during the fluid registration phase where alternative registry names for fluids can be added or removed.
+     * <br>
+     * Use any of the following to achieve this:
+     * <ul>
+     * <li>{@link GTFluidRegistrator#addAlternativeFluidName(String, String)}</li>
+     * <li>{@link GTFluidRegistrator#addAlternativeFluidName(Material, FluidType, String)}</li>
+     * <li>{@link GTFluidRegistrator#removeAlternativeFluidName(String)}</li>
+     * <li>{@link GTFluidRegistrator#removeAlternativeFluidName(String, String)}</li>
+     * </ul>
+     */
+    public static class AlternativeFluidNameEvent extends Event {
+        public AlternativeFluidNameEvent() {/**/}
     }
 }
