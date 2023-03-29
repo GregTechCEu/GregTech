d7b9e24fce1308e225cd21940e92e1422296bcd4
LAGIdiot <bordel.muj@gmail.com>
2023-03-29 12:17:08 +0800

    Update Activity Detector Covers to extend CoverDetectorBase

--- src/main/java/gregtech/common/covers/detector/CoverActivityDetectorAdvanced.java
+++ src/main/java/gregtech/common/covers/detector/CoverActivityDetectorAdvanced.java
@@ -1,6 +1,5 @@
 package gregtech.common.covers.detector;
 
-import codechicken.lib.raytracer.CuboidRayTraceResult;
 import codechicken.lib.render.CCRenderState;
 import codechicken.lib.render.pipeline.IVertexOperation;
 import codechicken.lib.vec.Cuboid6;
@@ -9,12 +8,8 @@ import gregtech.api.capability.GregtechTileCapabilities;
 import gregtech.api.capability.IWorkable;
 import gregtech.api.cover.ICoverable;
 import gregtech.client.renderer.texture.Textures;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.BlockRenderLayer;
-import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
-import net.minecraft.util.text.TextComponentTranslation;
 
 public class CoverActivityDetectorAdvanced extends CoverActivityDetector {
 
@@ -26,23 +21,7 @@ public class CoverActivityDetectorAdvanced extends CoverActivityDetector {
     public void renderCover(CCRenderState renderState, Matrix4 translation, IVertexOperation[] pipeline, Cuboid6 plateBox, BlockRenderLayer layer) {
         Textures.DETECTOR_ACTIVITY_ADVANCED.renderSided(attachedSide, plateBox, renderState, pipeline, translation);
     }
-
-    @Override
-    public EnumActionResult onScrewdriverClick(EntityPlayer playerIn, EnumHand hand, CuboidRayTraceResult hitResult) {
-        if (this.coverHolder.getWorld().isRemote) {
-            return EnumActionResult.SUCCESS;
-        }
-
-        if (this.isInverted) {
-            this.setInverted();
-            playerIn.sendMessage(new TextComponentTranslation("gregtech.cover.activity_detector_advanced.message_activity_normal"));
-        } else {
-            this.setInverted();
-            playerIn.sendMessage(new TextComponentTranslation("gregtech.cover.activity_detector_advanced.message_activity_inverted"));
-        }
-        return EnumActionResult.SUCCESS;
-    }
-
+    
     @Override
     public void update() {
         if (this.coverHolder.getOffsetTimer() % 20 != 0)
@@ -56,7 +35,7 @@ public class CoverActivityDetectorAdvanced extends CoverActivityDetector {
 
         if (!workable.isWorkingEnabled())
             outputAmount = 0;
-        else if (this.isInverted)
+        else if (this.isInverted())
             outputAmount = 15 - outputAmount;
 
         setRedstoneSignalOutput(outputAmount);
