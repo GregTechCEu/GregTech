e2706cd2c8b5e4bb7d59d2fef552c06e170e3b74
bruberu <80226372+bruberu@users.noreply.github.com>
2023-04-01 14:51:38 -0500

    Start (TOP) ore rework

--- src/main/java/gregtech/common/blocks/BlockOre.java
+++ src/main/java/gregtech/common/blocks/BlockOre.java
@@ -17,12 +17,14 @@ import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
@@ -136,6 +138,13 @@ public class BlockOre extends Block implements IBlockOre {
             return super.getItem(worldIn, pos, state);
         }
         return new ItemStack(this, 1, 0);
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public ItemStack getPickBlock(IBlockState state, RayTraceResult target, World world, BlockPos pos, EntityPlayer player) {
+        // Still get correct block even if shouldBeDroppedAsItem is false
+        return super.getItem(world, pos, state);
     }
 
     @Override
