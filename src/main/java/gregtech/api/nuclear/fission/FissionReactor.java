b98199ec738fd90d6630a43f67b26e3e85b115cd
Zalgo239 <67443118+Zalgo239@users.noreply.github.com>
2023-04-07 16:30:51 +0200

    Start of work on nuclear fission

---
+++ src/main/java/gregtech/api/nuclear/fission/FissionReactor.java
@@ -0,0 +1,51 @@
+package gregtech.api.nuclear.fission;
+
+import gregtech.api.nuclear.IReactorState;
+
+public class FissionReactor {
+
+    private IReactorState state;
+
+
+    /**
+     * Thresholds important for determining the evolution of the reactor
+     */
+    protected final int criticalRodInsertion;
+    protected final int criticalCoolantFlow;
+    protected final int criticalPressure;
+    protected final double criticalPower;
+
+    /**
+     * Integers used on variables with direct player control for easier adjustments
+     */
+    protected int controlRodInsertion;
+    protected int coolantFlowRate;
+    protected int pressure;
+
+    protected double powerProductionFactor;
+    protected double temperature;
+    protected double fuelDepletion;
+    protected double neutronPoisoning;
+
+    protected double envTemperature;
+
+    protected double maxTemperature;
+    protected double maxPower;
+    protected double maxCoolantFlow;
+
+    public FissionReactor(int criticalRodInsertion, int criticalCoolantFlow, int criticalPressure, double criticalPower) {
+        this.criticalRodInsertion = criticalRodInsertion;
+        this.criticalCoolantFlow = criticalCoolantFlow;
+        this.criticalPower = criticalPower;
+        this.criticalPressure = criticalPressure;
+    }
+
+    public boolean canCoolantBoil() {
+        return false;
+    }
+
+    public double voidFactor() {
+        return this.canCoolantBoil() ? (this.temperature - this.envTemperature) / (double) this.pressure : 0.D;
+    }
+
+}
