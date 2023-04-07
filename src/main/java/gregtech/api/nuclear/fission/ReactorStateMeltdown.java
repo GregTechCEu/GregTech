9ff346d6244f86388ec5dcf13131656b30849faa
Zalgo239 <67443118+Zalgo239@users.noreply.github.com>
2023-04-07 16:44:15 +0200

    Start implementation of reactor states

---
+++ src/main/java/gregtech/api/nuclear/fission/ReactorStateMeltdown.java
@@ -0,0 +1,9 @@
+package gregtech.api.nuclear.fission;
+
+public class ReactorStateMeltdown extends ReactorStateBase {
+
+    public ReactorStateMeltdown(FissionReactor reactor) {
+        super(reactor);
+    }
+
+}
---
+++ src/main/java/gregtech/api/nuclear/fission/ReactorStateRampDown.java
@@ -0,0 +1,9 @@
+package gregtech.api.nuclear.fission;
+
+public class ReactorStateRampDown extends ReactorStateBase {
+
+    public ReactorStateRampDown(FissionReactor reactor) {
+        super(reactor);
+    }
+
+}
