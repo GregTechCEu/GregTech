2164c9daa479f4c4401638b4c675d8b136a3bb56
Zalgo239 <67443118+Zalgo239@users.noreply.github.com>
2023-04-16 17:45:30 +0200

    More reactor states

--- src/main/java/gregtech/api/nuclear/fission/ReactorStateOverheating.java
+++ src/main/java/gregtech/api/nuclear/fission/ReactorStateOverheating.java
@@ -8,4 +8,21 @@ public class ReactorStateOverheating extends ReactorStateBase {
         super(reactor);
     }
 
+    @Override
+    public IReactorState getNextState() {
+        if (this.reactor.temperature > this.reactor.maxTemperature * 1.05) {
+            return new ReactorStateMeltdown(this.reactor);
+        } else if (this.reactor.controlRodInsertion >= this.reactor.criticalRodInsertion && this.reactor.temperature < this.reactor.maxTemperature) {
+            return new ReactorStateRampUpBegin(this.reactor);
+        } else {
+            return this;
+        }
+    }
+
+    @Override
+    public void runStateEvolution() {
+        this.reactor.powerProductionFactor = responseFunction(this.reactor.maxPower, this.reactor.powerProductionFactor, this.reactor.criticalRodInsertion, this.reactor.controlRodInsertion);
+        this.reactor.temperature = responseFunction(this.reactor.maxTemperature, this.reactor.temperature, this.reactor.criticalCoolantFlow(), this.reactor.coolantFlowRate);
+        this.reactor.fuelDepletion -= Math.max(0.D, this.reactor.powerProductionFactor/this.reactor.maxPower);
+    }
 }
--- src/main/java/gregtech/api/nuclear/fission/ReactorStateStalling.java
+++ src/main/java/gregtech/api/nuclear/fission/ReactorStateStalling.java
@@ -8,4 +8,22 @@ public class ReactorStateStalling extends ReactorStateBase {
         super(reactor);
     }
 
+    @Override
+    public IReactorState getNextState() {
+        if (this.reactor.temperature > this.reactor.maxTemperature) {
+            return new ReactorStateOverheating(this.reactor);
+        } else if (this.reactor.neutronPoisoning == 1.) {
+            return new ReactorStateStalled(this.reactor);
+        } else {
+            return this;
+        }
+    }
+
+    @Override
+    public void runStateEvolution() {
+        this.reactor.powerProductionFactor = Math.max(0.1 * this.reactor.maxPower, this.reactor.powerProductionFactor * 0.9);
+        this.reactor.temperature = responseFunction(this.reactor.maxTemperature, this.reactor.temperature, this.reactor.criticalCoolantFlow(), this.reactor.coolantFlowRate);
+        this.reactor.fuelDepletion -= Math.max(0.D, this.reactor.powerProductionFactor/this.reactor.maxPower);
+        this.reactor.neutronPoisoning += 0.01;
+    }
 }
