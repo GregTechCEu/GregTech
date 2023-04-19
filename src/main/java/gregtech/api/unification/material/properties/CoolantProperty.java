51fde733e92e4cc281c35f963c1eae01de991aa3
Zalgo239 <67443118+Zalgo239@users.noreply.github.com>
2023-04-19 17:45:23 +0200

    Basic stats for nuclear properties

--- src/main/java/gregtech/api/unification/material/properties/CoolantProperty.java
+++ src/main/java/gregtech/api/unification/material/properties/CoolantProperty.java
@@ -2,9 +2,27 @@ package gregtech.api.unification.material.properties;
 
 public class CoolantProperty implements IMaterialProperty<CoolantProperty>{
 
+    private double moderatorFactor = 0.D;
+    private double coolingFactor = 0.D;
+
     @Override
     public void verifyProperty(MaterialProperties properties) {
 
     }
 
+    public void setModeratorFactor(double moderatorFactor) {
+        this.moderatorFactor = moderatorFactor;
+    }
+
+    public double getModeratorFactor() {
+        return this.moderatorFactor;
+    }
+
+    public void setCoolingFactor(double coolingFactor) {
+        this.coolingFactor = coolingFactor;
+    }
+
+    public double getCoolingFactor() {
+        return this.coolingFactor;
+    }
 }
