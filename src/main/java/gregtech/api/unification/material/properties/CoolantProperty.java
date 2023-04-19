9eb55dfa7bc378e0acddfc6ea3e750d0a3d0e653
Zalgo239 <67443118+Zalgo239@users.noreply.github.com>
2023-04-19 17:20:06 +0200

    New material properties

---
+++ src/main/java/gregtech/api/unification/material/properties/CoolantProperty.java
@@ -0,0 +1,10 @@
+package gregtech.api.unification.material.properties;
+
+public class CoolantProperty implements IMaterialProperty<CoolantProperty>{
+
+    @Override
+    public void verifyProperty(MaterialProperties properties) {
+
+    }
+
+}
