f1c0adfd48e4d169c63740e5b1c4417f36689f44
Zalgo239 <67443118+Zalgo239@users.noreply.github.com>
2023-04-19 16:59:26 +0200

    Reorganising packages

--- src/main/java/gregtech/api/nuclear/fission/states/ReactorStateExplosion.java
+++ src/main/java/gregtech/api/nuclear/fission/states/ReactorStateExplosion.java
@@ -1,9 +1,16 @@
-package gregtech.api.nuclear.fission;
+package gregtech.api.nuclear.fission.states;
+
+import gregtech.api.nuclear.fission.FissionReactor;
 
 public class ReactorStateExplosion extends ReactorStateBase {
 
     public ReactorStateExplosion(FissionReactor reactor) {
         super(reactor);
+    }
+
+    @Override
+    public IReactorState getNextState() {
+        return this;
     }
 
     @Override
