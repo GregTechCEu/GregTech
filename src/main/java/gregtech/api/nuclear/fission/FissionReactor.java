58fa6ba6242afd919718ea6efa2eb66db890677d
Zalgo239 <67443118+Zalgo239@users.noreply.github.com>
2023-04-19 16:59:26 +0200

    Reorganising packages

--- src/main/java/gregtech/api/nuclear/fission/FissionReactor.java
+++ src/main/java/gregtech/api/nuclear/fission/FissionReactor.java
@@ -1,41 +1,38 @@
 package gregtech.api.nuclear.fission;
 
-import gregtech.api.nuclear.IReactorState;
+import gregtech.api.nuclear.fission.states.IReactorState;
 
 public class FissionReactor {
 
     private IReactorState state;
 
-
     /**
      * Thresholds important for determining the evolution of the reactor
      */
-    protected final int criticalRodInsertion;
-    //protected final int criticalCoolantFlow;
-    protected final int criticalPressure;
-    protected final double criticalPower;
+    public final int criticalRodInsertion;
+    public final int criticalPressure;
+    public final double criticalPower;
 
     /**
      * Integers used on variables with direct player control for easier adjustments
      */
-    protected int controlRodInsertion;
-    protected int coolantFlowRate;
-    protected int pressure;
+    public int controlRodInsertion;
+    public int coolantFlowRate;
+    public int pressure;
 
-    protected double powerProductionFactor;
-    protected double temperature;
-    protected double fuelDepletion;
-    protected double neutronPoisoning;
+    public double powerProductionFactor;
+    public double temperature;
+    public double fuelDepletion;
+    public double neutronPoisoning;
 
-    protected double envTemperature;
+    public double envTemperature;
 
-    protected double maxTemperature;
-    protected double maxPower;
-    protected double maxCoolantFlow;
+    public double maxTemperature;
+    public double maxPower;
 
-    protected double coolingFactor;
+    public double coolingFactor;
 
-    protected double decayPower;
+    public double decayPower;
 
     public FissionReactor(int criticalRodInsertion, int criticalPressure, double criticalPower) {
         this.criticalRodInsertion = criticalRodInsertion;
