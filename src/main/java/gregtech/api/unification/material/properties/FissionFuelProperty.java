11afc4162ad29d7185e3adb9c329ad39098eabc2
Zalgo239 <67443118+Zalgo239@users.noreply.github.com>
2023-04-19 17:45:23 +0200

    Basic stats for nuclear properties

--- src/main/java/gregtech/api/unification/material/properties/FissionFuelProperty.java
+++ src/main/java/gregtech/api/unification/material/properties/FissionFuelProperty.java
@@ -2,9 +2,29 @@ package gregtech.api.unification.material.properties;
 
 public class FissionFuelProperty implements IMaterialProperty<FissionFuelProperty>{
 
+    private double[] crossSections = new double[]{0.D, 0.D};
+    private double durability = 0.D;
+
     @Override
     public void verifyProperty(MaterialProperties properties) {
 
+    }
+
+    public void setCrossSections(double slowCrossSection, double fastCrossSection) {
+        this.crossSections[0] = slowCrossSection;
+        this.crossSections[1] = fastCrossSection;
+    }
+
+    public double[] getCrossSections() {
+        return this.crossSections;
+    }
+
+    public void setDurability(double durability) {
+        this.durability = durability;
+    }
+
+    public double getDurability() {
+        return this.durability;
     }
 
 }
--- src/main/java/gregtech/api/unification/material/properties/ModeratorProperty.java
+++ src/main/java/gregtech/api/unification/material/properties/ModeratorProperty.java
@@ -2,9 +2,19 @@ package gregtech.api.unification.material.properties;
 
 public class ModeratorProperty implements IMaterialProperty<ModeratorProperty>{
 
+    private double moderatorFactor = 0.D;
+
     @Override
     public void verifyProperty(MaterialProperties properties) {
 
+    }
+
+    public void setModeratorFactor(double moderatorFactor) {
+        this.moderatorFactor = moderatorFactor;
+    }
+
+    public double getModeratorFactor() {
+        return this.moderatorFactor;
     }
 
 }
