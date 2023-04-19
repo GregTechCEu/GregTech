d48137cf3510439a21ca365a0c395afe1fa9c9b5
Zalgo239 <67443118+Zalgo239@users.noreply.github.com>
2023-04-19 17:20:06 +0200

    New material properties

---
+++ src/main/java/gregtech/api/unification/material/properties/ModeratorProperty.java
@@ -0,0 +1,10 @@
+package gregtech.api.unification.material.properties;
+
+public class ModeratorProperty implements IMaterialProperty<ModeratorProperty>{
+
+    @Override
+    public void verifyProperty(MaterialProperties properties) {
+
+    }
+
+}
