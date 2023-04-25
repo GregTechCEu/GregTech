fa146544806556a27e76d87cbbb9bf379b389b30
ALongStringOfNumbers <31759736+ALongStringOfNumbers@users.noreply.github.com>
2023-04-24 19:28:36 -0600

    Fix Chests not opening (#1730)

--- src/main/java/gregtech/common/blocks/BlockGregStairs.java
+++ src/main/java/gregtech/common/blocks/BlockGregStairs.java
@@ -4,6 +4,11 @@ import gregtech.api.GregTechAPI;
 import gregtech.api.items.toolitem.ToolClasses;
 import net.minecraft.block.BlockStairs;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+
+import javax.annotation.Nonnull;
 
 public class BlockGregStairs extends BlockStairs {
 
@@ -12,5 +17,10 @@ public class BlockGregStairs extends BlockStairs {
         setCreativeTab(GregTechAPI.TAB_GREGTECH_DECORATIONS);
         this.useNeighborBrightness = true;
         this.setHarvestLevel(ToolClasses.AXE, 0);
+    }
+
+    @Override
+    public boolean doesSideBlockChestOpening(@Nonnull IBlockState blockState, @Nonnull IBlockAccess world, @Nonnull BlockPos pos, @Nonnull EnumFacing side) {
+        return false;
     }
 }
