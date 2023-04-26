e5d18e3f599fab6f4db56c1f89a0a60307bd3f54
Tech22 <37029404+TechLord22@users.noreply.github.com>
2023-04-25 21:46:23 -0400

    performance improvements

--- src/main/java/gregtech/api/util/PerlinNoise.java
+++ src/main/java/gregtech/api/util/PerlinNoise.java
@@ -2,29 +2,40 @@ package gregtech.api.util;
 
 import java.util.Random;
 
+/**
+ * Modified Perlin Noise algorithm for improved performance
+ */
 public final class PerlinNoise {
 
     private final int[] permutation;
+    private final int[] permutationHashed;
 
     public PerlinNoise(long seed) {
         Random random = new XSTR(seed);
         permutation = new int[512];
+        permutationHashed = new int[512];
+
         for (int i = 0; i < 256; i++) {
             permutation[i] = i;
         }
+
         for (int i = 0; i < 256; i++) {
             int j = random.nextInt(256);
             int k = permutation[i];
             permutation[i] = permutation[j];
             permutation[j] = k;
         }
+
+        for (int i = 0; i < permutation.length; i++) {
+            permutationHashed[i] = permutation[i] & 0xF;
+        }
     }
 
-    public double noise(double x, double y, double z, int octaves, double persistence) {
-        double total = 0;
-        double frequency = 1;
-        double amplitude = 1;
-        double maxValue = 0;
+    public float noise(float x, float y, float z, int octaves, float persistence) {
+        float total = 0;
+        float frequency = 1;
+        float amplitude = 1;
+        float maxValue = 0;
         for (int i = 0; i < octaves; i++) {
             total += noise(x * frequency, y * frequency, z * frequency) * amplitude;
 
@@ -35,45 +46,49 @@ public final class PerlinNoise {
         return total / maxValue;
     }
 
-    public double noise(double x, double y, double z) {
-        int X = (int) Math.floor(x) & 255;
-        int Y = (int) Math.floor(y) & 255;
-        int Z = (int) Math.floor(z) & 255;
-        x -= Math.floor(x);
-        y -= Math.floor(y);
-        z -= Math.floor(z);
-        double u = fade(x);
-        double v = fade(y);
-        double w = fade(z);
+    public float noise(float x, float y, float z) {
+        int X = (int) (x) & 0xFF;
+        int Y = (int) (y) & 0xFF;
+        int Z = (int) (z) & 0xFF;
+        x -= (int) x;
+        y -= (int) y;
+        z -= (int) z;
+        float u = fade(x);
+        float v = fade(y);
+        float w = fade(z);
         int A = permutation[X] + Y;
         int AA = permutation[A] + Z;
         int AB = permutation[A + 1] + Z;
         int B = permutation[X + 1] + Y;
         int BA = permutation[B] + Z;
         int BB = permutation[B + 1] + Z;
-        double res = lerp(w, lerp(v, lerp(u, grad(permutation[AA], x, y, z),
-                                grad(permutation[BA], x - 1, y, z)),
-                        lerp(u, grad(permutation[AB], x, y - 1, z),
-                                grad(permutation[BB], x - 1, y - 1, z))),
-                lerp(v, lerp(u, grad(permutation[AA + 1], x, y, z - 1),
-                                grad(permutation[BA + 1], x - 1, y, z - 1)),
-                        lerp(u, grad(permutation[AB + 1], x, y - 1, z - 1),
-                                grad(permutation[BB + 1], x - 1, y - 1, z - 1))));
-        return (res + 1.0) / 2.0;
+        float res = lerp(w, lerp(v, lerp(u, grad(permutationHashed[AA], x, y, z),
+                                grad(permutationHashed[BA], x - 1, y, z)),
+                        lerp(u, grad(permutationHashed[AB], x, y - 1, z),
+                                grad(permutationHashed[BB], x - 1, y - 1, z))),
+                lerp(v, lerp(u, grad(permutationHashed[AA + 1], x, y, z - 1),
+                                grad(permutationHashed[BA + 1], x - 1, y, z - 1)),
+                        lerp(u, grad(permutationHashed[AB + 1], x, y - 1, z - 1),
+                                grad(permutationHashed[BB + 1], x - 1, y - 1, z - 1))));
+        return (res + 1.0F) / 2.0F;
     }
 
-    private static double fade(double t) {
+    private static float fade(float t) {
         return t * t * t * (t * (t * 6 - 15) + 10);
     }
 
-    private static double lerp(double t, double a, double b) {
+    private static float lerp(float t, float a, float b) {
         return a + t * (b - a);
     }
 
-    private static double grad(int hash, double x, double y, double z) {
-        int h = hash & 0b1111;
-        double u = h < 0b1000 ? x : y;
-        double v = h < 0b0100 ? y : h == 0b1100 || h == 0b1110 ? x : z;
-        return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);
+    private static float grad(int hash, float x, float y, float z) {
+        // not 1:1 with the original behavior, but is close enough, looks nicer in-game, and is much faster
+        return ((hash & 0x1) != 0 ? -x : x) + ((hash & 0x2) != 0 ? -y : y) +
+                ((hash & 0x4) != 0 ? ((hash == 0xC || hash == 0xE) ? -x : -z) : z);
+
+        // original impl
+        // float u = hash < 0x8 ? x : y;
+        // float v = hash < 0x4 ? y : hash == 0xC || hash == 0xE ? x : z;
+        // return ((hash & 1) == 0 ? u : -u) + ((hash & 2) == 0 ? v : -v);
     }
 }
--- src/main/java/gregtech/api/worldgen2/GregTechTerrainGen.java
+++ src/main/java/gregtech/api/worldgen2/GregTechTerrainGen.java
@@ -9,6 +9,8 @@ import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.DimensionType;
 import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.storage.ExtendedBlockStorage;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.terraingen.PopulateChunkEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -17,15 +19,15 @@ import javax.annotation.Nonnull;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GregTechTerrainGen {
+public final class GregTechTerrainGen {
 
-    private static GregTechTerrainGen DEFUALT_INSTANCE;
+    private static List<IBlockState> stoneTypes;
+    private static PerlinNoise noise;
 
-    private final List<IBlockState> stoneTypes;
-    private PerlinNoise noise;
+    private GregTechTerrainGen() {}
 
     public static void init() {
-        List<IBlockState> stoneTypes = new ArrayList<>();
+        stoneTypes = new ArrayList<>();
         for (StoneVariantBlock.StoneType type : StoneVariantBlock.StoneType.values()) {
             stoneTypes.add(MetaBlocks.STONE_BLOCKS.get(StoneVariantBlock.StoneVariant.SMOOTH).getState(type));
         }
@@ -34,12 +36,7 @@ public class GregTechTerrainGen {
         stoneTypes.add(Blocks.STONE.getDefaultState().withProperty(BlockStone.VARIANT, BlockStone.EnumType.GRANITE));
         stoneTypes.add(Blocks.STONE.getDefaultState().withProperty(BlockStone.VARIANT, BlockStone.EnumType.DIORITE));
 
-        DEFUALT_INSTANCE = new GregTechTerrainGen(stoneTypes);
-    }
-
-    public GregTechTerrainGen(@Nonnull List<IBlockState> stoneTypes) {
-        MinecraftForge.EVENT_BUS.register(this);
-        this.stoneTypes = stoneTypes;
+        MinecraftForge.EVENT_BUS.register(GregTechTerrainGen.class);
     }
 
     /**
@@ -48,48 +45,81 @@ public class GregTechTerrainGen {
      * @param event the event containing the required data
      */
     @SubscribeEvent
-    public void generate(@Nonnull PopulateChunkEvent.Pre event) {
+    public static void generate(@Nonnull PopulateChunkEvent.Pre event) {
         final World world = event.getWorld();
         if (world.provider.getDimension() != DimensionType.OVERWORLD.getId()) {
             // only apply this generation to the overworld
             return;
         }
 
-        if (this.noise == null) {
-            this.noise = new PerlinNoise(world.getSeed());
-        }
+        tryInitializeNoise(world);
 
         final int chunkX = event.getChunkX();
         final int chunkZ = event.getChunkZ();
 
-        final int startX = chunkX * 16 + 8;
-        final int startZ = chunkZ * 16 + 8;
+        final int startX = chunkX << 4;
+        final int startZ = chunkZ << 4;
         final int maxY = world.getActualHeight();
+
+        final Chunk chunk = world.getChunk(chunkX, chunkZ);
+        final ExtendedBlockStorage[] storages = chunk.getBlockStorageArray();
 
         BlockPos.MutableBlockPos pos = new BlockPos.MutableBlockPos();
         for (int y = 0; y < maxY; y++) {
+            ExtendedBlockStorage storage = storages[y >> 4];
+            // out of storages, so immediately exit
+            if (storage == null || storage.isEmpty()) return;
+
             for (int xOffset = 0; xOffset < 16; xOffset++) {
                 final int x = startX + xOffset;
                 for (int zOffset = 0; zOffset < 16; zOffset++) {
                     final int z = startZ + zOffset;
-                    final int surface = world.getHeight(x, z);
                     pos.setPos(x, y, z);
 
-                    IBlockState state = world.getBlockState(pos);
+                    final int storageY = y & 0xF;
+
+                    IBlockState state = storage.get(xOffset, storageY, zOffset);
                     if (state.getBlock().isAir(state, world, pos)) continue;
                     if (!(state.getBlock() instanceof BlockStone)) continue;
 
-                    world.setBlockState(pos, getBlockFor(x, y, z, Math.max(1, surface)), 2 | 16);
+                    final int surfaceY = getWorldSurfaceFast(world, chunk, xOffset, zOffset);
+                    final IBlockState toSet = getStateFor(x, y, z, surfaceY);
+                    if (toSet.getBlock().isAir(toSet, world, pos)) continue;
+
+                    storage.set(xOffset, storageY, zOffset, toSet);
                 }
             }
         }
     }
 
+    private static void tryInitializeNoise(@Nonnull World world) {
+        if (noise == null) {
+            noise = new PerlinNoise(world.getSeed());
+        }
+    }
+
+    /**
+     * Faster way to retrieve the surface level of a world. Skips chunk loading checks.
+     *
+     * @param world the world containing the chunk
+     * @param chunk the chunk containing the coordinates
+     * @param x the x block coordinate within the chunk [0, 16)
+     * @param z the z block coordinate within the chunk [0, 16)
+     * @return the world surface height, guaranteed > 0
+     */
+    private static int getWorldSurfaceFast(@Nonnull World world, @Nonnull Chunk chunk, int x, int z) {
+        // code simplified from World#getHeight(int, int)
+        if (x >= -30_000_000 && z >= -30_000_000 && x < 30_000_000 && z < 30_000_000) {
+            return Math.max(1, chunk.getHeightValue(x, z));
+        }
+        return Math.max(1, world.getSeaLevel() + 1);
+    }
+
     @Nonnull
-    private IBlockState getBlockFor(int x, int y, int z, int surfaceY) {
-        assert this.noise != null;
+    private static IBlockState getStateFor(int x, int y, int z, int surfaceY) {
+        assert noise != null;
         // need abs() for x and y, when generating blobs between - and + coords
-        double noiseValue = noise.noise(Math.abs(x * 0.01F), y * 1F / surfaceY, Math.abs(z * 0.01F), 4, 0.1);
+        float noiseValue = noise.noise(Math.abs(x * 0.01F), y * 1.0F / surfaceY, Math.abs(z * 0.01F), 4, 0.1F);
         return stoneTypes.get((int) (stoneTypes.size() * noiseValue));
     }
 }
