4baec8b4e4a5a2f8817c8ddd9ee5b23fb0f62025
bruberu <80226372+bruberu@users.noreply.github.com>
2023-04-29 20:55:37 -0500

    Improve default enchantment API (#1698)
    * Make sure to not modify the tool property's enchantments
    
    * Add capability for "default enchantment level increases"
    
    * Further modification protection and EnchantmentLevel
    
    * Apply ALSON's suggestions
    
    * Expose to CraftTweaker

---
+++ src/main/java/gregtech/api/items/toolitem/EnchantmentLevel.java
@@ -0,0 +1,15 @@
+package gregtech.api.items.toolitem;
+
+public class EnchantmentLevel {
+    private double level;
+    private double levelGrowth;
+
+    public EnchantmentLevel(double level, double levelGrowth) {
+        this.level = level;
+        this.levelGrowth = levelGrowth;
+    }
+
+    public int getLevel(int harvestTier) {
+        return (int) Math.min(Byte.MAX_VALUE, Math.floor(this.level + this.levelGrowth * harvestTier));
+    }
+}
