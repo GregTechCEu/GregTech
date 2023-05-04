d0068cbe021dcbeb0625768a773915afc8ba6d42
Tech22 <37029404+TechLord22@users.noreply.github.com>
2023-05-04 19:30:00 -0400

    use translation keys for tool classes

--- src/main/java/gregtech/api/items/toolitem/IGTTool.java
+++ src/main/java/gregtech/api/items/toolitem/IGTTool.java
@@ -65,7 +65,6 @@ import net.minecraftforge.common.util.Constants;
 import net.minecraftforge.fml.common.Optional;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-import org.apache.commons.lang3.text.WordUtils;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -729,10 +728,12 @@ public interface IGTTool extends ItemUIFactory, IAEWrench, IToolWrench, IToolHam
         tooltip.add("");
 
         // valid tools
-        tooltip.add(I18n.format("item.gt.tool.usable_as", stack.getItem().getToolClasses(stack).stream()
-                .map(GTUtility::convertUnderscoreToSpace)
-                .map(WordUtils::capitalize)
-                .collect(Collectors.joining(", "))));
+        tooltip.add(I18n.format("item.gt.tool.usable_as",
+                stack.getItem().getToolClasses(stack).stream()
+                        .map(s -> "gt.tool.class." + s)
+                        .map(I18n::format)
+                        .collect(Collectors.joining(", "))
+        ));
 
         // repair info
         if (TooltipHelper.isShiftDown()) {
