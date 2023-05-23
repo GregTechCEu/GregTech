f9bba45e00c61277ea921ae859cd95010dfce670
alongstringofnumbers <sjensen793@gmail.com>
2023-05-23 16:36:22 -0700

    Cover not enough energy case

--- src/main/java/gregtech/common/metatileentities/multi/electric/MetaTileEntityFusionReactor.java
+++ src/main/java/gregtech/common/metatileentities/multi/electric/MetaTileEntityFusionReactor.java
@@ -324,7 +324,10 @@ public class MetaTileEntityFusionReactor extends RecipeMapMultiblockController i
         @Override
         public void updateWorkable() {
             super.updateWorkable();
-            if (!(isActive || isWorkingEnabled()) && heat > 0) {
+            // Drain heat when the reactor is not active, is paused via soft mallet, or does not have enough energy and has fully wiped recipe progress
+            // Don't drain heat when there is not enough energy and there is still some recipe progress, as that makes it doubly hard to complete the recipe
+            // (Will have to recover heat and recipe progress)
+            if ((!(isActive || workingEnabled) || (hasNotEnoughEnergy && progressTime == 0)) && heat > 0) {
                 heat = heat <= 10000 ? 0 : (heat - 10000);
             }
         }
