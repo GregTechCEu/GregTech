b7e805e74b698eec70b0e6bc0ef85f0a8e681208
Dane Strandboge <dane@strandboge.com>
2023-05-23 00:31:15 -0500

    OpenComputers module (#1794)

---
+++ src/main/java/gregtech/integration/opencomputers/values/ValueCoverItemFilter.java
@@ -0,0 +1,45 @@
+package gregtech.integration.opencomputers.values;
+
+import gregtech.api.cover.CoverBehavior;
+import gregtech.common.covers.CoverItemFilter;
+import gregtech.common.covers.ItemFilterMode;
+import gregtech.integration.opencomputers.InputValidator;
+import li.cil.oc.api.machine.Arguments;
+import li.cil.oc.api.machine.Callback;
+import li.cil.oc.api.machine.Context;
+import net.minecraft.util.EnumFacing;
+
+public class ValueCoverItemFilter extends ValueCoverBehavior {
+
+    public ValueCoverItemFilter(CoverItemFilter coverBehavior, EnumFacing side) {
+        super(coverBehavior, side, "gt_coverItemFilter");
+    }
+
+    @Override
+    protected CoverItemFilter getCoverBehavior() {
+        CoverBehavior cover = super.getCoverBehavior();
+        return cover instanceof CoverItemFilter ? (CoverItemFilter) cover : null;
+    }
+
+    @Callback(doc = "function(mode:number) --  Sets filter mode. (0:FILTER_INSERT, 1:FILTER_EXTRACT, 2:FILTER_BOTH)")
+    public Object[] setFilterMode(final Context context, final Arguments args) {
+        CoverItemFilter cover = getCoverBehavior();
+        if (cover == null) {
+            return NULL_COVER;
+        }
+
+        ItemFilterMode mode = InputValidator.getEnumArrayIndex(args, 0, ItemFilterMode.values());
+        cover.setFilterMode(mode);
+        return new Object[]{};
+    }
+
+    @Callback(doc = "function():number --  Gets filter mode. (0:FILTER_INSERT, 1:FILTER_EXTRACT, 2:FILTER_BOTH)")
+    public Object[] getFilterMode(final Context context, final Arguments args) {
+        CoverItemFilter cover = getCoverBehavior();
+        if (cover == null) {
+            return NULL_COVER;
+        }
+
+        return new Object[]{cover.getFilterMode().ordinal()};
+    }
+}
--- src/main/java/gregtech/modules/GregTechModules.java
+++ src/main/java/gregtech/modules/GregTechModules.java
@@ -13,6 +13,7 @@ public class GregTechModules implements IModuleContainer {
     public static final String MODULE_TOP = "top_integration";
     public static final String MODULE_CT  = "ct_integration";
     public static final String MODULE_GRS = "grs_integration";
+    public static final String MODULE_OC  = "oc_integration";
 
     @Override
     public String getID() {
