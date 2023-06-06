1837229518ad9298513210a96e8a96d52a273c02
Zalgo239 <67443118+Zalgo239@users.noreply.github.com>
2023-06-06 12:32:18 +0200

    Added fuel rod output hatch

--- src/main/java/gregtech/common/metatileentities/MetaTileEntities.java
+++ src/main/java/gregtech/common/metatileentities/MetaTileEntities.java
@@ -332,6 +332,7 @@ public class MetaTileEntities {
     public static MetaTileEntityActiveTransformer ACTIVE_TRANSFORMER;
     public static MetaTileEntityFissionReactor FISSION_REACTOR;
     public static MetaTileEntityFuelRodHatch FUEL_ROD_INPUT;
+    public static MetaTileEntityFuelRodHatch FUEL_ROD_OUTPUT;
 
     // STORAGE SECTION
     public static MetaTileEntityTankValve WOODEN_TANK_VALVE;
@@ -1183,6 +1184,7 @@ public class MetaTileEntities {
         }
 
         FUEL_ROD_INPUT = registerMetaTileEntity(1730, new MetaTileEntityFuelRodHatch(gregtechId("fuel_rod_input"), false));
+        FUEL_ROD_OUTPUT = registerMetaTileEntity(1731, new MetaTileEntityFuelRodHatch(gregtechId("fuel_rod_output"), true));
 
         /*
          * FOR ADDON DEVELOPERS:
