35fad70282ca26d5cf1013e0ec563a304391f4fd
Tech22 <37029404+TechLord22@users.noreply.github.com>
2023-06-09 12:10:37 -0400

    fix infinite bus collapsing

--- src/main/java/gregtech/common/metatileentities/multi/multiblockpart/MetaTileEntityItemBus.java
+++ src/main/java/gregtech/common/metatileentities/multi/multiblockpart/MetaTileEntityItemBus.java
@@ -16,6 +16,7 @@ import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
 import gregtech.api.metatileentity.multiblock.IMultiblockAbilityPart;
 import gregtech.api.metatileentity.multiblock.MultiblockAbility;
+import gregtech.api.metatileentity.multiblock.MultiblockControllerBase;
 import gregtech.api.util.GTHashMaps;
 import gregtech.client.renderer.texture.Textures;
 import gregtech.client.renderer.texture.cube.SimpleOverlayRenderer;
@@ -67,10 +68,16 @@ public class MetaTileEntityItemBus extends MetaTileEntityMultiblockNotifiablePar
                 }
             }
             // Only attempt to auto collapse the inventory contents once the bus has been notified
-            if (isAutoCollapse()) {
-                IItemHandlerModifiable inventory = (isExportHatch ? this.getExportItems() : this.getImportItems());
-                if (isExportHatch ? this.getNotifiedItemOutputList().contains(inventory) : this.getNotifiedItemInputList().contains(inventory)) {
-                    collapseInventorySlotContents(inventory);
+            if (isAutoCollapse() && this.isAttachedToMultiBlock()) {
+                MultiblockControllerBase controller = getController();
+                if (controller != null) {
+                    List<IItemHandlerModifiable> notified = isExportHatch ? controller.getNotifiedItemOutputList() : controller.getNotifiedItemInputList();
+                    IItemHandlerModifiable inventory = isExportHatch ? getExportItems() : getImportItems();
+                    if (notified.contains(inventory)) {
+                        collapseInventorySlotContents(inventory);
+                        // collapsing re-notifies, so prevent attempts to collapse again
+                        notified.remove(inventory);
+                    }
                 }
             }
         }
@@ -199,9 +206,8 @@ public class MetaTileEntityItemBus extends MetaTileEntityMultiblockNotifiablePar
     }
 
     private static void collapseInventorySlotContents(IItemHandlerModifiable inventory) {
-
         // Gather a snapshot of the provided inventory
-        Object2IntMap<ItemStack> inventoryContents = GTHashMaps.fromItemHandler(inventory);
+        Object2IntMap<ItemStack> inventoryContents = GTHashMaps.fromItemHandler(inventory, true);
 
         List<ItemStack> inventoryItemContents = new ArrayList<>();
 
@@ -235,7 +241,6 @@ public class MetaTileEntityItemBus extends MetaTileEntityMultiblockNotifiablePar
             // Populate the slots
             inventory.setStackInSlot(i, stackToMove);
         }
-
     }
 
     @Override
