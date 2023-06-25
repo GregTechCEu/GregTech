2d2227bc7301b469cccdb13f8502a3575843cb24
bruberu <80226372+bruberu@users.noreply.github.com>
2023-06-24 22:40:35 -0500

    Complete nuclear reactor checks (but apparently locking needs to be communicated to the client :weary:)

--- src/main/java/gregtech/api/nuclear/fission/components/FuelRod.java
+++ src/main/java/gregtech/api/nuclear/fission/components/FuelRod.java
@@ -1,38 +1,38 @@
 package gregtech.api.nuclear.fission.components;
 
-import gregtech.api.nuclear.fission.fuels.NuclearFuel;
+import gregtech.api.unification.material.properties.FissionFuelProperty;
 
 public class FuelRod extends ReactorComponent {
-    private final NuclearFuel fuel;
+    private final FissionFuelProperty fuel;
     private final double neutronSourceIntensity;
 
-    public FuelRod(double maxTemperature, double thermalConductivity, NuclearFuel fuel, double neutronSourceIntensity) {
+    public FuelRod(double maxTemperature, double thermalConductivity, FissionFuelProperty fuel, double neutronSourceIntensity) {
         super(0, maxTemperature, thermalConductivity, true);
         this.fuel = fuel;
         this.neutronSourceIntensity = neutronSourceIntensity;
     }
 
     public double getHEFissionFactor() {
-        return fuel.getCrossSectionVector()[0];
+        return fuel.getFastNeutronFissionCrossSection();
     }
 
     public double getLEFissionFactor() {
-        return fuel.getCrossSectionVector()[1];
+        return fuel.getSlowNeutronFissionCrossSection();
     }
 
     public double getHECaptureFactor() {
-        return fuel.getCrossSectionVector()[2];
+        return fuel.getFastNeutronCaptureCrossSection();
     }
 
     public double getLECaptureFactor() {
-        return fuel.getCrossSectionVector()[3];
+        return fuel.getSlowNeutronCaptureCrossSection();
     }
 
     public double getNeutronSourceIntensity() {
         return neutronSourceIntensity;
     }
 
-    public NuclearFuel getFuel() {
+    public FissionFuelProperty getFuel() {
         return fuel;
     }
 
