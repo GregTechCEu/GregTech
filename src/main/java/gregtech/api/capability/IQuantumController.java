3e7bff4459c43d6ceccfb5ec6e373c63f5cdb436
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2023-07-10 22:10:45 -0700

    add javadoc

--- src/main/java/gregtech/api/capability/IQuantumController.java
+++ src/main/java/gregtech/api/capability/IQuantumController.java
@@ -4,6 +4,9 @@ import net.minecraft.util.math.BlockPos;
 
 public interface IQuantumController {
 
+    /**
+     * Constructs the network upon placement
+     */
     void rebuildNetwork();
 
     /** Return whether this storage block can connect. Can be used to implement a maximum distance from controller for example. */
