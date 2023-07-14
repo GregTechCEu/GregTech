f9dcc7e3668648f38f9c96e92fd2904011ae0ae1
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2023-07-14 14:58:17 -0700

    forgor stackform for hull

--- src/main/java/gregtech/loaders/recipe/MetaTileEntityLoader.java
+++ src/main/java/gregtech/loaders/recipe/MetaTileEntityLoader.java
@@ -1006,7 +1006,7 @@ public class MetaTileEntityLoader {
                 "PCP", "EHS", "CFC",
                 'C', new UnificationEntry(OrePrefix.circuit, Tier.MV),
                 'P', new UnificationEntry(OrePrefix.plate, Materials.Aluminium),
-                'H', MetaTileEntities.HULL[GTValues.MV],
+                'H', MetaTileEntities.HULL[GTValues.MV].getStackForm(),
                 'F', MetaItems.FIELD_GENERATOR_MV.getStackForm(),
                 'E', MetaItems.EMITTER_MV,
                 'S', MetaItems.SENSOR_MV
