d2269c50ce96d37fa152bab7709a6e70763c1553
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2023-07-13 23:27:56 -0700

    use item inventory to insert

--- src/main/java/gregtech/common/metatileentities/storage/MetaTileEntityQuantumChest.java
+++ src/main/java/gregtech/common/metatileentities/storage/MetaTileEntityQuantumChest.java
@@ -565,7 +565,7 @@ public class MetaTileEntityQuantumChest extends MetaTileEntityQuantumStorage<IIt
 
     @Override
     public IItemHandler getTypeValue() {
-        return new ItemHandlerProxy(getImportItems(), getExportItems()) {
+        return new ItemHandlerProxy(getItemInventory(), getExportItems()) {
             @Override
             public boolean isItemValid(int slot, @NotNull ItemStack stack) {
                 return getImportItems().isItemValid(slot, stack);
