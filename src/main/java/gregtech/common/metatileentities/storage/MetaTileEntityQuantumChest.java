e9214f61c9385414170525d6b31fd85917036156
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2023-07-13 21:02:23 -0700

    return combined inventory for qchest

--- src/main/java/gregtech/common/metatileentities/storage/MetaTileEntityQuantumChest.java
+++ src/main/java/gregtech/common/metatileentities/storage/MetaTileEntityQuantumChest.java
@@ -565,7 +565,7 @@ public class MetaTileEntityQuantumChest extends MetaTileEntityQuantumStorage<IIt
 
     @Override
     public IItemHandler getTypeValue() {
-        return itemInventory;
+        return combinedInventory;
     }
 
     @Override
