4412c710d6665dd4a88cf6d229dafef9b9e6e22d
TechLord22 <37029404+TechLord22@users.noreply.github.com>
2023-07-26 14:34:48 -0400

    top addons fluid compat (#1996)

---
+++ src/api/java/io/github/drmanganese/topaddons/reference/Colors.java
@@ -0,0 +1,12 @@
+package io.github.drmanganese.topaddons.reference;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Adapted and minimized from <a href="https://github.com/DrManganese/TOPAddons/blob/1.12/src/main/java/io/github/drmanganese/topaddons/reference/Colors.java">Colors.java</a>
+ */
+public final class Colors {
+
+    public static final Map<String, Integer> FLUID_NAME_COLOR_MAP = new HashMap<>();
+}
