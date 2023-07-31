6fe296a3fad794369b700c5f169113031843b8e7
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2023-07-31 00:58:04 -0700

    rename "Extender" to "Proxy" use proper pos for debugging

--- src/main/java/gregtech/common/metatileentities/MetaTileEntities.java
+++ src/main/java/gregtech/common/metatileentities/MetaTileEntities.java
@@ -223,7 +223,7 @@ public class MetaTileEntities {
     public static final MetaTileEntityMufflerHatch[] MUFFLER_HATCH = new MetaTileEntityMufflerHatch[GTValues.UV + 1]; // LV-UV
     public static final MetaTileEntityFusionReactor[] FUSION_REACTOR = new MetaTileEntityFusionReactor[3];
     public static MetaTileEntityQuantumStorageController QUANTUM_STORAGE_CONTROLLER;
-    public static MetaTileEntityQuantumExtender QUANTUM_STORAGE_EXTENDER;
+    public static MetaTileEntityQuantumProxy QUANTUM_STORAGE_EXTENDER;
     public static final MetaTileEntityQuantumChest[] QUANTUM_CHEST = new MetaTileEntityQuantumChest[11];
     public static final MetaTileEntityQuantumTank[] QUANTUM_TANK = new MetaTileEntityQuantumTank[11];
     public static final MetaTileEntityBuffer[] BUFFER = new MetaTileEntityBuffer[3];
@@ -981,7 +981,7 @@ public class MetaTileEntities {
 
         // Super / Quantum Chests, IDs 1559-1574, 1751
         QUANTUM_STORAGE_CONTROLLER = registerMetaTileEntity(1559, new MetaTileEntityQuantumStorageController(gregtechId("quantum_storage_controller")));
-        QUANTUM_STORAGE_EXTENDER = registerMetaTileEntity(1753, new MetaTileEntityQuantumExtender(gregtechId("quantum_storage_extender")));
+        QUANTUM_STORAGE_EXTENDER = registerMetaTileEntity(1753, new MetaTileEntityQuantumProxy(gregtechId("quantum_storage_extender")));
 
         QUANTUM_CHEST[10] = new MetaTileEntityQuantumChest(gregtechId("super_chest." + GTValues.VN[GTValues.ULV].toLowerCase()), 0, 1000000L);
         registerMetaTileEntity(1751, QUANTUM_CHEST[0]);
