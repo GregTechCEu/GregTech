a658fa3bcaac9df1d400e0c8e2fe20d198e58d81
serenibyss <10861407+serenibyss@users.noreply.github.com>
2023-07-31 23:24:58 -0500

    Shortcut method for CT material registry getter

--- src/main/java/gregtech/integration/crafttweaker/material/CTMaterialRegistry.java
+++ src/main/java/gregtech/integration/crafttweaker/material/CTMaterialRegistry.java
@@ -1,6 +1,7 @@
 package gregtech.integration.crafttweaker.material;
 
 import crafttweaker.annotations.ZenRegister;
+import gregtech.api.GTValues;
 import gregtech.api.GregTechAPI;
 import gregtech.api.unification.material.Material;
 import stanhebben.zenscript.annotations.ZenClass;
@@ -15,6 +16,11 @@ public class CTMaterialRegistry {
     @ZenMethod
     public Material get(String modid, String name) {
         return GregTechAPI.materialManager.getRegistry(modid).getObject(name);
+    }
+
+    @ZenMethod
+    public Material get(String name) {
+        return get(GTValues.MODID, name);
     }
 
     @ZenMethod
