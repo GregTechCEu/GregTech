6a83a95adc9c219e13a230d5e6f4799324724ba9
Zalgo239 <67443118+Zalgo239@users.noreply.github.com>
2023-09-07 14:30:32 +0200

    Blocks cleanup

--- src/main/java/gregtech/common/blocks/BlockNuclearCasing.java
+++ src/main/java/gregtech/common/blocks/BlockNuclearCasing.java
@@ -2,13 +2,10 @@ package gregtech.common.blocks;
 
 import gregtech.api.block.IStateHarvestLevel;
 import gregtech.api.block.VariantActiveBlock;
-import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.IStringSerializable;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockAccess;
 
 import javax.annotation.Nonnull;
 
@@ -20,18 +17,11 @@ public class BlockNuclearCasing extends VariantActiveBlock<BlockNuclearCasing.Nu
         setHardness(5.0f);
         setResistance(10.0f);
         setSoundType(SoundType.METAL);
-        setDefaultState(getState(NuclearCasingType.GAS_CENTRIFUGE_COLUMN));
+        setDefaultState(getState(NuclearCasingType.SPENT_FUEL_CASING));
         setLightLevel(0.5f);
     }
 
-    @Override
-    public boolean isOpaqueCube(IBlockState state) {
-        return state != getState(NuclearCasingType.GAS_CENTRIFUGE_COLUMN);
-    }
-
     public enum NuclearCasingType implements IStringSerializable, IStateHarvestLevel {
-
-        GAS_CENTRIFUGE_COLUMN("gas_centrifuge_column", 2),
         SPENT_FUEL_CASING("spent_fuel_casing", 2);
 
         NuclearCasingType(String name, int harvestLevel) {
