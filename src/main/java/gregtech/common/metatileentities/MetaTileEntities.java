4b8446652c8a75dda6c93a47c0ca6facb41f09f5
Zalgo239 <67443118+Zalgo239@users.noreply.github.com>
2023-09-16 16:38:18 +0200

    Added gas centrifuge multiblock

--- src/main/java/gregtech/common/metatileentities/MetaTileEntities.java
+++ src/main/java/gregtech/common/metatileentities/MetaTileEntities.java
@@ -238,6 +238,7 @@ public class MetaTileEntities {
     public static MetaTileEntityCoolantExportHatch COOLANT_OUTPUT;
     public static MetaTileEntityControlRodPort CONTROL_ROD;
     public static MetaTileEntitySpentFuelPool SPENT_FUEL_POOL;
+    public static MetaTileEntityGasCentrifuge GAS_CENTRIFUGE;
     public static MetaTileEntityDataBank DATA_BANK;
     public static MetaTileEntityResearchStation RESEARCH_STATION;
     public static MetaTileEntityHPCA HIGH_PERFORMANCE_COMPUTING_ARRAY;
@@ -552,6 +553,7 @@ public class MetaTileEntities {
         FISSION_REACTOR = registerMetaTileEntity(1043, new MetaTileEntityFissionReactor(gregtechId("fission_reactor")));
         SPENT_FUEL_POOL = registerMetaTileEntity(1044, new MetaTileEntitySpentFuelPool(gregtechId("spent_fuel_pool")));
         HEAT_EXCHANGER = registerMetaTileEntity(1045, new MetaTileEntityHeatExchanger(gregtechId("heat_exchanger")));
+        GAS_CENTRIFUGE = registerMetaTileEntity(1046, new MetaTileEntityGasCentrifuge(gregtechId("gas_centrifuge")));
 
         // MISC MTE's START: IDs 1150-2000
 
---
+++ src/main/java/gregtech/common/metatileentities/multi/electric/MetaTileEntityGasCentrifuge.java
@@ -0,0 +1,68 @@
+package gregtech.common.metatileentities.multi.electric;
+
+import gregtech.api.capability.impl.MultiblockRecipeLogic;
+import gregtech.api.metatileentity.MetaTileEntity;
+import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
+import gregtech.api.metatileentity.multiblock.IMultiblockPart;
+import gregtech.api.metatileentity.multiblock.RecipeMapMultiblockController;
+import gregtech.api.pattern.BlockPattern;
+import gregtech.api.pattern.FactoryBlockPattern;
+import gregtech.api.recipes.RecipeMaps;
+import gregtech.client.renderer.ICubeRenderer;
+import gregtech.client.renderer.texture.Textures;
+import gregtech.common.blocks.BlockBoilerCasing;
+import gregtech.common.blocks.BlockGasCentrifugeCasing;
+import gregtech.common.blocks.BlockNuclearCasing;
+import gregtech.common.blocks.MetaBlocks;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.ResourceLocation;
+import org.jetbrains.annotations.NotNull;
+
+import static gregtech.api.util.RelativeDirection.*;
+
+public class MetaTileEntityGasCentrifuge extends RecipeMapMultiblockController {
+
+    public MetaTileEntityGasCentrifuge(ResourceLocation metaTileEntityId) {
+        super(metaTileEntityId, RecipeMaps.GAS_CENTRIFUGE_RECIPES);
+        this.recipeMapWorkable = new MultiblockRecipeLogic(this);
+    }
+
+    @NotNull
+    @Override
+    protected BlockPattern createStructurePattern() {
+        return FactoryBlockPattern.start(FRONT, UP, RIGHT)
+                .aisle("SI", "HH", "CC", "CC", "CC", "CC", "CC")
+                .aisle("EE", "HH", "CC", "CC", "CC", "CC", "CC").setRepeatable(1, 14)
+                .aisle("OO", "HH", "CC", "CC", "CC", "CC", "CC")
+                .where('S', selfPredicate())
+                .where('P', states(getPipeState()))
+                .where('H', states(getHeaterState()))
+                .where('C', states(getCentrifugeState()))
+                .where('I', states(getPipeState()).or(autoAbilities(false, false, false, false, true, false, false)))
+                .where('E', states(getPipeState()).or(autoAbilities(true, true, false, false, false, false, false)))
+                .where('O', states(getPipeState()).or(autoAbilities(false, false, false, false, false, true, false)))
+                .build();
+    }
+
+    @Override
+    public ICubeRenderer getBaseTexture(IMultiblockPart sourcePart) {
+        return Textures.INERT_PTFE_CASING;
+    }
+
+    @Override
+    public MetaTileEntity createMetaTileEntity(IGregTechTileEntity tileEntity) {
+        return new MetaTileEntityGasCentrifuge(metaTileEntityId);
+    }
+
+    private IBlockState getPipeState() {
+        return MetaBlocks.BOILER_CASING.getState(BlockBoilerCasing.BoilerCasingType.POLYTETRAFLUOROETHYLENE_PIPE);
+    }
+
+    private IBlockState getHeaterState() {
+        return MetaBlocks.NUCLEAR_CASING.getState(BlockNuclearCasing.NuclearCasingType.GAS_CENTRIFUGE_HEATER);
+    }
+
+    private IBlockState getCentrifugeState() {
+        return MetaBlocks.GAS_CENTRIFUGE_CASING.getState(BlockGasCentrifugeCasing.GasCentrifugeCasingType.GAS_CENTRIFUGE_COLUMN);
+    }
+}
