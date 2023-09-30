d295499512462e5a268e3044194cb40b52a641b3
htmlcsjs <46023024+htmlcsjs@users.noreply.github.com>
2023-09-30 17:57:03 +0100

    add top support

---
+++ src/main/java/gregtech/integration/theoneprobe/provider/LaserContainerInfoProvider.java
@@ -0,0 +1,35 @@
+package gregtech.integration.theoneprobe.provider;
+
+import gregtech.api.GTValues;
+import gregtech.api.capability.GregtechTileCapabilities;
+import gregtech.api.capability.ILaserContainer;
+import mcjty.theoneprobe.api.IProbeHitData;
+import mcjty.theoneprobe.api.IProbeInfo;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.common.capabilities.Capability;
+import org.jetbrains.annotations.NotNull;
+
+public class LaserContainerInfoProvider extends CapabilityInfoProvider<ILaserContainer> {
+    @NotNull
+    @Override
+    protected Capability<ILaserContainer> getCapability() {
+        return GregtechTileCapabilities.CAPABILITY_LASER;
+    }
+
+    @Override
+    protected void addProbeInfo(ILaserContainer capability, IProbeInfo probeInfo, EntityPlayer player, TileEntity tileEntity, IProbeHitData data) {
+        long maxStorage = capability.getEnergyCapacity();
+        if (maxStorage == 0) return; // do not add empty max storage progress bar
+        probeInfo.progress(capability.getEnergyStored(), maxStorage, probeInfo.defaultProgressStyle()
+                .suffix(" / " + maxStorage + " EU")
+                .filledColor(0xFFEEE600)
+                .alternateFilledColor(0xFFEEE600)
+                .borderColor(0xFF555555));
+    }
+
+    @Override
+    public String getID() {
+        return GTValues.MODID + ":laser_container_provider";
+    }
+}
