24126c88eb638fe46a97a2a7e8a0584f2a706a71
serenibyss <10861407+serenibyss@users.noreply.github.com>
2023-10-11 06:01:03 -0500

    more tooltip cleanup

--- src/main/java/gregtech/common/pipelike/optical/ItemBlockOpticalPipe.java
+++ src/main/java/gregtech/common/pipelike/optical/ItemBlockOpticalPipe.java
@@ -1,13 +1,14 @@
 package gregtech.common.pipelike.optical;
 
 import gregtech.api.pipenet.block.ItemBlockPipe;
+import gregtech.client.utils.TooltipHelper;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import javax.annotation.Nullable;
 import java.util.List;
 
 public class ItemBlockOpticalPipe extends ItemBlockPipe<OpticalPipeType, OpticalPipeProperties> {
@@ -20,5 +21,11 @@ public class ItemBlockOpticalPipe extends ItemBlockPipe<OpticalPipeType, Optical
     public void addInformation(@NotNull ItemStack stack, @Nullable World worldIn, @NotNull List<String> tooltip, @NotNull ITooltipFlag flagIn) {
         super.addInformation(stack, worldIn, tooltip, flagIn);
         tooltip.add(I18n.format("tile.optical_pipe_normal.tooltip1"));
+
+        if (TooltipHelper.isShiftDown()) {
+            tooltip.add(I18n.format("gregtech.tool_action.wire_cutter.connect"));
+        } else {
+            tooltip.add(I18n.format("gregtech.tool_action.show_tooltips"));
+        }
     }
 }
