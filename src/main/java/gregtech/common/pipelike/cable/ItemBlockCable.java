a5b9f64caffef3a933e31d852608065f6e569011
Serenibyss <10861407+serenibyss@users.noreply.github.com>
2023-10-14 19:09:52 -0500

    More tooltip cleanup (#2106)

--- src/main/java/gregtech/common/pipelike/cable/ItemBlockCable.java
+++ src/main/java/gregtech/common/pipelike/cable/ItemBlockCable.java
@@ -5,6 +5,7 @@ import gregtech.api.pipenet.block.material.BlockMaterialPipe;
 import gregtech.api.pipenet.block.material.ItemBlockMaterialPipe;
 import gregtech.api.unification.material.properties.WireProperties;
 import gregtech.api.util.GTUtility;
+import gregtech.client.utils.TooltipHelper;
 import gregtech.common.ConfigHolder;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
@@ -12,9 +13,9 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import java.util.List;
 
 public class ItemBlockCable extends ItemBlockMaterialPipe<Insulation, WireProperties> {
@@ -25,13 +26,22 @@ public class ItemBlockCable extends ItemBlockMaterialPipe<Insulation, WireProper
 
     @Override
     @SideOnly(Side.CLIENT)
-    public void addInformation(@Nonnull ItemStack stack, @Nullable World worldIn, @Nonnull List<String> tooltip, @Nonnull ITooltipFlag flagIn) {
+    public void addInformation(@NotNull ItemStack stack, @Nullable World worldIn, @NotNull List<String> tooltip, @NotNull ITooltipFlag flagIn) {
+        super.addInformation(stack, worldIn, tooltip, flagIn);
         WireProperties wireProperties = blockPipe.createItemProperties(stack);
         int tier = GTUtility.getTierByVoltage(wireProperties.getVoltage());
         if (wireProperties.isSuperconductor()) tooltip.add(I18n.format("gregtech.cable.superconductor", GTValues.VN[tier]));
         tooltip.add(I18n.format("gregtech.cable.voltage", wireProperties.getVoltage(), GTValues.VNF[tier]));
         tooltip.add(I18n.format("gregtech.cable.amperage", wireProperties.getAmperage()));
         tooltip.add(I18n.format("gregtech.cable.loss_per_block", wireProperties.getLossPerBlock()));
+
+        if (TooltipHelper.isShiftDown()) {
+            tooltip.add(I18n.format("gregtech.tool_action.wire_cutter.connect"));
+            tooltip.add(I18n.format("gregtech.tool_action.screwdriver.access_covers"));
+            tooltip.add(I18n.format("gregtech.tool_action.crowbar"));
+        } else {
+            tooltip.add(I18n.format("gregtech.tool_action.show_tooltips"));
+        }
 
         if (ConfigHolder.misc.debug) {
             BlockMaterialPipe<?, ?, ?> blockMaterialPipe = (BlockMaterialPipe<?, ?, ?>) blockPipe;
