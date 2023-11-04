95e5d39df9e8c78e46b7cac916e05681dc01c125
kumquat-ir <66188216+kumquat-ir@users.noreply.github.com>
2023-11-04 16:31:08 -0400

    javadocs

--- src/main/java/gregtech/api/modules/ModuleContainer.java
+++ src/main/java/gregtech/api/modules/ModuleContainer.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotate your {@link IModuleContainer} with this for it to be automatically registered.
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ModuleContainer {
