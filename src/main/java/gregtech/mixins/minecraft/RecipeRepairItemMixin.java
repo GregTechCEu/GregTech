d55241e61d5d62e941d98c8357a5ea083cb14434
alongstringofnumbers <sjensen793@gmail.com>
2023-11-24 16:53:49 -0700

    Some Fixes

--- src/main/java/gregtech/mixins/minecraft/RecipeRepairItemMixin.java
+++ src/main/java/gregtech/mixins/minecraft/RecipeRepairItemMixin.java
@@ -22,9 +22,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(RecipeRepairItem.class)
 public class RecipeRepairItemMixin {
 
-    @Inject(method = "Lnet/minecraft/item/crafting/RecipeRepairItem;matches(Lnet/minecraft/inventory/InventoryCrafting;Lnet/minecraft/world/World;)Z",
-            at = @At(value = "INVOKE_ASSIGN", target = "Ljava/util/List;get(I)Ljava/lang/Object;"))
-    public static void gregtechCEu$matches(InventoryCrafting inv, World worldIn, CallbackInfoReturnable<Boolean> cir,
+    @Inject(method = "matches(Lnet/minecraft/inventory/InventoryCrafting;Lnet/minecraft/world/World;)Z",
+            at = @At(value = "INVOKE_ASSIGN", target = "Ljava/util/List;get(I)Ljava/lang/Object;"), cancellable = true)
+    public void gregtechCEu$matches(InventoryCrafting inv, World worldIn, CallbackInfoReturnable<Boolean> cir,
                                            @Local LocalRef<ItemStack> itemstack, @Local LocalRef<ItemStack> itemstack1) {
         if (itemstack.get().getItem() instanceof IGTTool first && itemstack1.get().getItem() instanceof IGTTool second) {
             if (first.isElectric() || second.isElectric()) {
@@ -35,9 +35,9 @@ public class RecipeRepairItemMixin {
         }
     }
 
-    @Inject(method = "Lnet/minecraft/item/crafting/RecipeRepairItem;getCraftingResult(Lnet/minecraft/inventory/InventoryCrafting;)Lnet/minecraft/item/ItemStack;",
-            at = @At(value = "INVOKE_ASSIGN", target = "Ljava/util/List;get(I)Ljava/lang/Object;", ordinal = 0))
-    public static void gregtechCEu$getCraftingResultFirst(InventoryCrafting inv, CallbackInfoReturnable<ItemStack> cir,
+    @Inject(method = "getCraftingResult(Lnet/minecraft/inventory/InventoryCrafting;)Lnet/minecraft/item/ItemStack;",
+            at = @At(value = "INVOKE_ASSIGN", target = "Ljava/util/List;get(I)Ljava/lang/Object;", ordinal = 0), cancellable = true)
+    public void gregtechCEu$getCraftingResultFirst(InventoryCrafting inv, CallbackInfoReturnable<ItemStack> cir,
                                                           @Local(ordinal = 0) LocalRef<ItemStack> itemstack, @Local(ordinal = 1) LocalRef<ItemStack> itemstack1) {
         if (itemstack.get().getItem() instanceof IGTTool tool && tool.isElectric()) {
             cir.setReturnValue(ItemStack.EMPTY);
@@ -46,9 +46,9 @@ public class RecipeRepairItemMixin {
         }
     }
 
-    @Inject(method = "Lnet/minecraft/item/crafting/RecipeRepairItem;getCraftingResult(Lnet/minecraft/inventory/InventoryCrafting;)Lnet/minecraft/item/ItemStack;",
-            at = @At(value = "RETURN", ordinal = 1))
-    public static void gregtechCEu$getCraftingResultSecond(InventoryCrafting inv, CallbackInfoReturnable<ItemStack> cir,
+    @Inject(method = "getCraftingResult(Lnet/minecraft/inventory/InventoryCrafting;)Lnet/minecraft/item/ItemStack;",
+            at = @At(value = "RETURN", ordinal = 1), cancellable = true)
+    public void gregtechCEu$getCraftingResultSecond(InventoryCrafting inv, CallbackInfoReturnable<ItemStack> cir,
                                                            @Local(ordinal = 4) LocalIntRef i1, @Local(ordinal = 2) LocalRef<ItemStack> itemstack2,
                                                            @Local(ordinal = 3) LocalRef<ItemStack> itemstack3) {
         if (itemstack2.get().getItem() instanceof IGTTool first && itemstack3.get().getItem() instanceof IGTTool) {
@@ -64,8 +64,14 @@ public class RecipeRepairItemMixin {
         }
     }
 
+    // TODO, see if we can change this from an overwrite
+
+    /**
+     * @author GregTech CEu
+     * @reason Properly account for GT Tools in Crafting Recipes
+     */
     @Overwrite
-    public static NonNullList<ItemStack> getRemainingItems(InventoryCrafting inv) {
+    public NonNullList<ItemStack> getRemainingItems(InventoryCrafting inv) {
         NonNullList<ItemStack> list = NonNullList.withSize(inv.getSizeInventory(), ItemStack.EMPTY);
         for (int i = 0; i < list.size(); i++) {
             ItemStack stack = inv.getStackInSlot(i);
