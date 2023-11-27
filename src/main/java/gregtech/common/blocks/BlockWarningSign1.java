1d29504edd7bdcc047fdade4a57042b2e21afbe4
Serenibyss <10861407+serenibyss@users.noreply.github.com>
2023-11-26 23:28:38 -0600

    Fix mobs being able to spawn on warning signs (#2206)

--- src/main/java/gregtech/common/blocks/BlockWarningSign1.java
+++ src/main/java/gregtech/common/blocks/BlockWarningSign1.java
@@ -6,7 +6,11 @@ import gregtech.api.items.toolitem.ToolClasses;
 
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.util.IStringSerializable;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -21,6 +25,12 @@ public class BlockWarningSign1 extends VariantBlock<BlockWarningSign1.SignType> 
         setHarvestLevel(ToolClasses.WRENCH, 1);
         setDefaultState(getState(SignType.MOB_SPAWNER_HAZARD));
         setCreativeTab(GregTechAPI.TAB_GREGTECH_DECORATIONS);
+    }
+
+    @Override
+    public boolean canCreatureSpawn(@NotNull IBlockState state, @NotNull IBlockAccess world, @NotNull BlockPos pos,
+                                    @NotNull EntityLiving.SpawnPlacementType type) {
+        return false;
     }
 
     public enum SignType implements IStringSerializable {
