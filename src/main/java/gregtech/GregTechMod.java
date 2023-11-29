ab59f5131958e1f0db5c99ba4df09b5a0dcdaa9d
Serenibyss <10861407+serenibyss@users.noreply.github.com>
2023-11-28 22:18:14 -0600

    Groovyscript 0.7.0 compat (#2217)

--- src/main/java/gregtech/GregTechMod.java
+++ src/main/java/gregtech/GregTechMod.java
@@ -24,7 +24,7 @@ import net.minecraftforge.fml.common.event.*;
      dependencies = "required:forge@[14.23.5.2847,);" + "required-after:codechickenlib@[3.2.3,);" +
              "after:appliedenergistics2;" + "after:forestry;" + "after:extrabees;" + "after:extratrees;" +
              "after:genetics;" + "after:magicbees;" + "after:jei@[4.15.0,);" + "after:crafttweaker@[4.1.20,);" +
-             "after:groovyscript@[0.6.0,);" + "after:theoneprobe;" + "after:hwyla;")
+             "after:groovyscript@[0.7.0,);" + "after:theoneprobe;" + "after:hwyla;")
 public class GregTechMod {
 
     // Hold this so that we can reference non-interface methods without
