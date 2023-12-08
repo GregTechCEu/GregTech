e8be52a0ae8d57cdb875d992663ada6464c30ddd
serenibyss <10861407+serenibyss@users.noreply.github.com>
2023-12-08 01:17:12 -0600

    "API" programming

---
+++ src/main/java/gregtech/api/mui/synchandler/GhostCircuitSyncHandler.java
@@ -0,0 +1,82 @@
+package gregtech.api.mui.synchandler;
+
+import com.cleanroommc.modularui.utils.MouseData;
+import com.cleanroommc.modularui.value.sync.ItemSlotSH;
+import com.cleanroommc.modularui.widgets.slot.ModularSlot;
+
+import gregtech.api.capability.impl.GhostCircuitItemStackHandler;
+import gregtech.api.recipes.ingredients.IntCircuitIngredient;
+
+import net.minecraftforge.items.IItemHandler;
+
+public class GhostCircuitSyncHandler extends ItemSlotSH {
+
+    public GhostCircuitSyncHandler(ModularSlot slot) {
+        super(slot);
+    }
+
+    @Override
+    protected void phantomClick(MouseData mouseData) {
+        if (mouseData.mouseButton == 0) {
+            // increment on left-click
+            setCircuitValue(getNextCircuitValue(true));
+        } else if (mouseData.mouseButton == 1 && mouseData.shift) {
+            // clear on shift-right-click
+            setCircuitValue(GhostCircuitItemStackHandler.NO_CONFIG);
+        } else if (mouseData.mouseButton == 1) {
+            // decrement on right-click
+            setCircuitValue(getNextCircuitValue(false));
+        }
+    }
+
+    @Override
+    protected void phantomScroll(MouseData mouseData) {
+        setCircuitValue(getNextCircuitValue(mouseData.mouseButton == 1));
+    }
+
+    private void setCircuitValue(int value) {
+        GhostCircuitItemStackHandler handler = getGhostCircuitHandler();
+        handler.setCircuitValue(value);
+        syncToClient(1, buf -> {
+            buf.writeBoolean(false);
+            buf.writeItemStack(handler.getStackInSlot(0));
+            buf.writeBoolean(false);
+        });
+    }
+
+    private int getNextCircuitValue(boolean increment) {
+        GhostCircuitItemStackHandler handler = getGhostCircuitHandler();
+        if (increment) {
+            // if at max, loop around to no circuit
+            if (handler.getCircuitValue() == IntCircuitIngredient.CIRCUIT_MAX) {
+                return GhostCircuitItemStackHandler.NO_CONFIG;
+            }
+            // if at no circuit, skip 0 and return 1
+            if (!handler.hasCircuitValue()) {
+                return 1;
+            }
+            // normal case: increment by 1
+            return handler.getCircuitValue() + 1;
+        } else {
+            // if at no circuit, loop around to max
+            if (!handler.hasCircuitValue()) {
+                return IntCircuitIngredient.CIRCUIT_MAX;
+            }
+            // if at 1, skip 0 and return no circuit
+            if (handler.getCircuitValue() == 1) {
+                return GhostCircuitItemStackHandler.NO_CONFIG;
+            }
+            // normal case: decrement by 1
+            return handler.getCircuitValue() - 1;
+        }
+    }
+
+    private GhostCircuitItemStackHandler getGhostCircuitHandler() {
+        IItemHandler handler = getSlot().getItemHandler();
+        if (!(handler instanceof GhostCircuitItemStackHandler ghostHandler)) {
+            throw new IllegalStateException(
+                    "GhostCircuitSyncHandler has IItemHandler that is not GhostCircuitItemStackHandler");
+        }
+        return ghostHandler;
+    }
+}
--- src/main/java/gregtech/api/mui/widget/GhostCircuitSlotWidget.java
+++ src/main/java/gregtech/api/mui/widget/GhostCircuitSlotWidget.java
@@ -1,10 +1,11 @@
 package gregtech.api.mui.widget;
 
-import gregtech.api.capability.impl.GhostCircuitItemStackHandler;
-import gregtech.api.recipes.ingredients.IntCircuitIngredient;
-import gregtech.client.utils.TooltipHelper;
+import com.cleanroommc.modularui.utils.MouseData;
+import com.cleanroommc.modularui.value.sync.ItemSlotSH;
+import com.cleanroommc.modularui.widgets.slot.ModularSlot;
 
-import net.minecraftforge.items.IItemHandler;
+import gregtech.api.mui.synchandler.GhostCircuitSyncHandler;
+import gregtech.client.utils.TooltipHelper;
 
 import com.cleanroommc.modularui.screen.ModularScreen;
 import com.cleanroommc.modularui.widgets.ItemSlot;
@@ -15,67 +16,28 @@ public class GhostCircuitSlotWidget extends ItemSlot {
     @Override
     public @NotNull Result onMousePressed(int mouseButton) {
         if (mouseButton == 0 && TooltipHelper.isShiftDown()) {
-            // open popup on shift-left-click
-            // todo
-        } else if (mouseButton == 0) {
-            // increment on left-click
-            setCircuitValue(getNextCircuitValue(true));
-        } else if (mouseButton == 1 && TooltipHelper.isShiftDown()) {
-            // clear on shift-right-click
-            setCircuitValue(GhostCircuitItemStackHandler.NO_CONFIG);
-        } else if (mouseButton == 1) {
-            // decrement on right-click
-            setCircuitValue(getNextCircuitValue(false));
+            // todo open popup on shift-left-click
+
+        } else {
+            MouseData mouseData = MouseData.create(mouseButton);
+            getSyncHandler().syncToServer(2, mouseData::writeToPacket);
         }
         return Result.SUCCESS;
     }
 
     @Override
     public boolean onMouseScroll(ModularScreen.UpOrDown scrollDirection, int amount) {
-        setCircuitValue(getNextCircuitValue(scrollDirection.isUp()));
+        MouseData mouseData = MouseData.create(scrollDirection.modifier);
+        getSyncHandler().syncToServer(3, mouseData::writeToPacket);
         return false;
     }
 
-    private void setCircuitValue(int value) {
-        GhostCircuitItemStackHandler handler = getGhostCircuitHandler();
-        handler.setCircuitValue(value);
-        getSyncHandler().updateFromClient(handler.getStackInSlot(0));
-    }
-
-    private int getNextCircuitValue(boolean increment) {
-        GhostCircuitItemStackHandler handler = getGhostCircuitHandler();
-        if (increment) {
-            // if at max, loop around to no circuit
-            if (handler.getCircuitValue() == IntCircuitIngredient.CIRCUIT_MAX) {
-                return GhostCircuitItemStackHandler.NO_CONFIG;
-            }
-            // if at no circuit, skip 0 and return 1
-            if (!handler.hasCircuitValue()) {
-                return 1;
-            }
-            // normal case: increment by 1
-            return handler.getCircuitValue() + 1;
-        } else {
-            // if at no circuit, loop around to max
-            if (!handler.hasCircuitValue()) {
-                return IntCircuitIngredient.CIRCUIT_MAX;
-            }
-            // if at 1, skip 0 and return no circuit
-            if (handler.getCircuitValue() == 1) {
-                return GhostCircuitItemStackHandler.NO_CONFIG;
-            }
-            // normal case: decrement by 1
-            return handler.getCircuitValue() - 1;
-        }
-    }
-
-    private GhostCircuitItemStackHandler getGhostCircuitHandler() {
-        IItemHandler handler = getSyncHandler().getSlot().getItemHandler();
-        if (!(handler instanceof GhostCircuitItemStackHandler ghostHandler)) {
-            throw new IllegalStateException(
-                    "GhostCircuitSlotWidget has IItemHandler that is not GhostCircuitItemStackHandler");
-        }
-        return ghostHandler;
+    @Override
+    public ItemSlot slot(ModularSlot slot) {
+        ItemSlotSH sh = new GhostCircuitSyncHandler(slot);
+        isValidSyncHandler(sh);
+        setSyncHandler(sh);
+        return this;
     }
 
     @Override
