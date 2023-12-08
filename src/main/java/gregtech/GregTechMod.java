787f00e9d679d3affb48905c9687ef8f4526fb5b
serenibyss <10861407+serenibyss@users.noreply.github.com>
2023-12-07 20:33:40 -0600

    dep stuff

--- src/main/java/gregtech/GregTechMod.java
+++ src/main/java/gregtech/GregTechMod.java
@@ -22,9 +22,10 @@ import net.minecraftforge.fml.common.event.*;
      acceptedMinecraftVersions = "[1.12.2,1.13)",
      version = GTInternalTags.VERSION,
      dependencies = "required:forge@[14.23.5.2847,);" + "required-after:codechickenlib@[3.2.3,);" +
-             "required-after:modularui@[2,);" + "after:appliedenergistics2;" + "after:forestry;" + "after:extrabees;" +
-             "after:extratrees;" + "after:genetics;" + "after:magicbees;" + "after:jei@[4.15.0,);" +
-             "after:crafttweaker@[4.1.20,);" + "after:groovyscript@[0.7.0,);" + "after:theoneprobe;" + "after:hwyla;")
+             "required-after:modularui@[2.3,);" + "required-after:mixinbooter@[8.0,);" + "after:appliedenergistics2;" +
+             "after:forestry;" + "after:extrabees;" + "after:extratrees;" + "after:genetics;" + "after:magicbees;" +
+             "after:jei@[4.15.0,);" + "after:crafttweaker@[4.1.20,);" + "after:groovyscript@[0.7.0,);" +
+             "after:theoneprobe;" + "after:hwyla;")
 public class GregTechMod {
 
     // Hold this so that we can reference non-interface methods without
