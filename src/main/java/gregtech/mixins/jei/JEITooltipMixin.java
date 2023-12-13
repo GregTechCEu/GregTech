ef5454a60c734801cbdec795cc08ed668ce7865b
alongstringofnumbers <sjensen793@gmail.com>
2023-12-13 12:18:14 -0700

    CTM mixins

--- src/main/java/gregtech/mixins/jei/JEITooltipMixin.java
+++ src/main/java/gregtech/mixins/jei/JEITooltipMixin.java
@@ -1,25 +1,34 @@
 package gregtech.mixins.jei;
 
+import gregtech.api.util.FluidTooltipUtil;
+
+import net.minecraftforge.fluids.FluidStack;
+
+import mezz.jei.api.ingredients.IIngredientHelper;
 import mezz.jei.startup.ForgeModIdHelper;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
 
 // TODO, Needs to apply to the fluid items in JEI
 @Mixin(ForgeModIdHelper.class)
 public class JEITooltipMixin {
 
-    /*
-     * @Inject(method = "addModNameToIngredientTooltip", at = @At("HEAD"), remap = false)
-     * public void addTooltip(List<String> tooltip, Object ingredient, IIngredientHelper<Object> ingredientHelper,
-     * CallbackInfoReturnable<List<String>> cir) {
-     * if (ingredient instanceof FluidStack) {
-     * List<String> formula = FluidTooltipUtil.getFluidTooltip((FluidStack) ingredient);
-     * if (formula != null) {
-     * for (String s : formula) {
-     * if (s.isEmpty()) continue;
-     * tooltip.add(s);
-     * }
-     * }
-     * }
-     * }
-     */
+    @Inject(method = "addModNameToIngredientTooltip", at = @At("HEAD"), remap = false)
+    public void addTooltip(List<String> tooltip, Object ingredient, IIngredientHelper<Object> ingredientHelper,
+                           CallbackInfoReturnable<List<String>> cir) {
+        if (ingredient instanceof FluidStack) {
+            List<String> formula = FluidTooltipUtil.getFluidTooltip((FluidStack) ingredient);
+            if (formula != null) {
+                for (String s : formula) {
+                    if (s.isEmpty()) continue;
+                    tooltip.add(s);
+                }
+            }
+        }
+    }
+
 }
