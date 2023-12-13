204b46525871805754bf6ef6974efbeafc818aaa
Serenibyss <10861407+serenibyss@users.noreply.github.com>
2023-12-13 10:38:32 -0600

    More tool compat (#2292)

---
+++ src/api/java/mrtjp/projectred/api/IScrewdriver.java
@@ -0,0 +1,11 @@
+package mrtjp.projectred.api;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.player.EntityPlayer;
+
+public interface IScrewdriver {
+
+    boolean canUse(EntityPlayer player, ItemStack stack);
+
+    void damageScrewdriver(EntityPlayer player, ItemStack stack); // Damage the item on usage
+}
