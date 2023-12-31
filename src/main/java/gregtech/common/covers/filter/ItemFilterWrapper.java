bf2e34d6d10e58c5388f81892597d8f97a75ddb5
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2023-12-31 12:59:26 -0700

    remap more methods to container

--- src/main/java/gregtech/common/covers/filter/ItemFilterWrapper.java
+++ src/main/java/gregtech/common/covers/filter/ItemFilterWrapper.java
@@ -1,31 +1,23 @@
 package gregtech.common.covers.filter;
 
-import gregtech.api.gui.GuiTextures;
-import gregtech.api.gui.widgets.ServerWidgetGroup;
-import gregtech.api.gui.widgets.ToggleButtonWidget;
-
 import net.minecraft.item.ItemStack;
 
 import java.util.function.BooleanSupplier;
 import java.util.function.Consumer;
 
 public class ItemFilterWrapper {
-    private ItemFilterContainer container;
+    private final ItemFilterContainer container;
 
     public ItemFilterWrapper(ItemFilterContainer container) {
         this.container = container;
     }
 
     public void initUI(int y, Consumer<gregtech.api.gui.Widget> widgetGroup) {
-        widgetGroup.accept(new WidgetGroupItemFilter(y, this::getItemFilter));
+        container.initFilterUI(y, widgetGroup);
     }
 
     public void blacklistUI(int y, Consumer<gregtech.api.gui.Widget> widgetGroup, BooleanSupplier showBlacklistButton) {
-        ServerWidgetGroup blacklistButton = new ServerWidgetGroup(() -> getItemFilter() != null);
-        blacklistButton.addWidget(new ToggleButtonWidget(144, y, 20, 20, GuiTextures.BUTTON_BLACKLIST,
-                this::isBlacklistFilter, this::setBlacklistFilter).setPredicate(showBlacklistButton)
-                        .setTooltipText("cover.filter.blacklist"));
-        widgetGroup.accept(blacklistButton);
+        container.blacklistUI(y, widgetGroup, showBlacklistButton);
     }
 
     public void setItemFilter(ItemFilter itemFilter) {
