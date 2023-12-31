1f27c5767d4d25de2b596646ce5d695d7b789a97
M-W-K <mwk001@icloud.com>
2023-12-31 01:51:30 -0700

    Sound

--- src/main/java/gregtech/core/sound/GTSoundEvents.java
+++ src/main/java/gregtech/core/sound/GTSoundEvents.java
@@ -29,6 +29,7 @@ public class GTSoundEvents {
     public static SoundEvent COOLING;
     public static SoundEvent MINER;
     public static SoundEvent COMPUTATION;
+    public static SoundEvent FORGING;
 
     // Tool Sounds
     public static SoundEvent DRILL_TOOL;
@@ -81,6 +82,7 @@ public class GTSoundEvents {
         MINER = soundManager.registerSound("tick.miner");
         SCIENCE = soundManager.registerSound("tick.science");
         COMPUTATION = soundManager.registerSound("tick.computation");
+        FORGING = soundManager.registerSound("tick.forging");
 
         WRENCH_TOOL = soundManager.registerSound("use.wrench");
         SOFT_MALLET_TOOL = soundManager.registerSound("use.soft_hammer");
