c6e27cf67e9d9f09d627bcb7238405e9bc2bb5a8
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2024-01-03 16:34:54 -0700

    set keep exact max stack size to int max

--- src/main/java/gregtech/common/covers/TransferMode.java
+++ src/main/java/gregtech/common/covers/TransferMode.java
@@ -8,7 +8,7 @@ public enum TransferMode implements IStringSerializable {
 
     TRANSFER_ANY("cover.robotic_arm.transfer_mode.transfer_any", 1),
     TRANSFER_EXACT("cover.robotic_arm.transfer_mode.transfer_exact", 1024),
-    KEEP_EXACT("cover.robotic_arm.transfer_mode.keep_exact", 1024);
+    KEEP_EXACT("cover.robotic_arm.transfer_mode.keep_exact", Integer.MAX_VALUE);
 
     public final String localeName;
     public final int maxStackSize;
