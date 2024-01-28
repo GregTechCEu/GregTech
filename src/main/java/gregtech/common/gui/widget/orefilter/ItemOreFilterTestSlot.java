21468c2f7b78b03a129dc5c3e18c79669f3e144e
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2024-01-28 10:09:58 -0700

    remove commented code

--- src/main/java/gregtech/common/gui/widget/orefilter/ItemOreFilterTestSlot.java
+++ src/main/java/gregtech/common/gui/widget/orefilter/ItemOreFilterTestSlot.java
@@ -27,17 +27,6 @@ public class ItemOreFilterTestSlot extends ModularSlot {
         this.glob = glob;
     }
 
-    // @Override
-    // @SideOnly(Side.CLIENT)
-    // public boolean mouseClicked(int mouseX, int mouseY, int button) {
-    // if (isMouseOverElement(mouseX, mouseY)) {
-    // EntityPlayer player = Minecraft.getMinecraft().player;
-    // putItem(player.inventory.getItemStack());
-    // return true;
-    // }
-    // return false;
-    // }
-
     @Override
     public void putStack(ItemStack stack) {
         ItemStack testStack = getStack();
@@ -49,27 +38,4 @@ public class ItemOreFilterTestSlot extends ModularSlot {
             this.parent.updatePreview();
         }
     }
-
-    // @Override
-    // public List<IGhostIngredientHandler.Target<?>> getPhantomTargets(Object ingredient) {
-    // if (!(ingredient instanceof ItemStack)) {
-    // return Collections.emptyList();
-    // }
-    // Rectangle rectangle = toRectangleBox();
-    // return Lists.newArrayList(new IGhostIngredientHandler.Target<Object>() {
-    //
-    // @NotNull
-    // @Override
-    // public Rectangle getArea() {
-    // return rectangle;
-    // }
-    //
-    // @Override
-    // public void accept(@NotNull Object ingredient) {
-    // if (ingredient instanceof ItemStack) {
-    // putItem((ItemStack) ingredient);
-    // }
-    // }
-    // });
-    // }
 }
