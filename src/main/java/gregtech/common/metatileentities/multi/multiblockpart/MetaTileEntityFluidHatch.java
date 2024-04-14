e8208be63c88f5011e1adaeffda2f7a9b74ac74a
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2024-04-14 08:38:23 -0700

    try fix tests spotless

--- src/main/java/gregtech/common/metatileentities/multi/multiblockpart/MetaTileEntityFluidHatch.java
+++ src/main/java/gregtech/common/metatileentities/multi/multiblockpart/MetaTileEntityFluidHatch.java
@@ -56,7 +56,6 @@ import com.cleanroommc.modularui.widgets.slot.ModularSlot;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -267,7 +266,7 @@ public class MetaTileEntityFluidHatch extends MetaTileEntityMultiblockNotifiable
         if (key.equals(MultiblockAbility.EXPORT_FLUIDS)) {
             return Collections.singletonList(fluidTank);
         } else if (key.equals(MultiblockAbility.IMPORT_DUAL)) {
-            return Collections.singletonList(new DualHandler(this.circuitInventory, this.importFluids,false));
+            return Collections.singletonList(new DualHandler(this.circuitInventory, this.importFluids, false));
         }
         return Collections.emptyList();
     }
