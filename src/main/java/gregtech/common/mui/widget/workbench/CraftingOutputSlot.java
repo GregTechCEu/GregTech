2d6f150c8c9a829692d7d0d0cbcc449d67f86ce1
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2024-04-24 13:06:47 -0700

    initial work on recipe memory spotless

--- src/main/java/gregtech/common/mui/widget/workbench/CraftingOutputSlot.java
+++ src/main/java/gregtech/common/mui/widget/workbench/CraftingOutputSlot.java
@@ -1,17 +1,8 @@
 package gregtech.common.mui.widget.workbench;
 
-import com.cleanroommc.modularui.utils.MouseData;
-import com.cleanroommc.modularui.value.sync.IntSyncValue;
-import com.cleanroommc.modularui.value.sync.ItemSlotSH;
-import com.cleanroommc.modularui.widgets.ItemSlot;
-import com.cleanroommc.modularui.widgets.slot.ModularSlot;
-import com.google.common.collect.Lists;
-
 import gregtech.api.util.GTLog;
 import gregtech.common.metatileentities.storage.CraftingRecipeLogic;
-
 import gregtech.common.metatileentities.storage.CraftingRecipeMemory;
-
 import gregtech.common.metatileentities.storage.MetaTileEntityWorkbench;
 
 import net.minecraft.entity.player.EntityPlayer;
@@ -20,9 +11,14 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketBuffer;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.items.IItemHandler;
-
 import net.minecraftforge.items.IItemHandlerModifiable;
 
+import com.cleanroommc.modularui.utils.MouseData;
+import com.cleanroommc.modularui.value.sync.IntSyncValue;
+import com.cleanroommc.modularui.value.sync.ItemSlotSH;
+import com.cleanroommc.modularui.widgets.ItemSlot;
+import com.cleanroommc.modularui.widgets.slot.ModularSlot;
+import com.google.common.collect.Lists;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
@@ -44,7 +40,8 @@ public class CraftingOutputSlot extends ItemSlot {
     }
 
     public static ModularSlot modular(IntSyncValue syncValue, MetaTileEntityWorkbench workbench) {
-        return new CraftingOutputModularSlot(workbench.getCraftingRecipeLogic().getCraftingResultInventory(), syncValue, workbench);
+        return new CraftingOutputModularSlot(workbench.getCraftingRecipeLogic().getCraftingResultInventory(), syncValue,
+                workbench);
     }
 
     @SuppressWarnings("UnstableApiUsage")
@@ -155,7 +152,8 @@ public class CraftingOutputSlot extends ItemSlot {
         private final CraftingRecipeMemory recipeMemory;
         private final IItemHandler craftingGrid;
 
-        public CraftingOutputModularSlot(IInventory craftingInventory, IntSyncValue syncValue, MetaTileEntityWorkbench workbench) {
+        public CraftingOutputModularSlot(IInventory craftingInventory, IntSyncValue syncValue,
+                                         MetaTileEntityWorkbench workbench) {
             super(new InventoryWrapper(craftingInventory, workbench.getCraftingRecipeLogic()), 0, true);
             this.syncValue = syncValue;
             this.recipeLogic = workbench.getCraftingRecipeLogic();
