424aaf0731f3f77582c52049db91b792e1f5778a
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2024-06-18 16:56:36 -0700

    make fields mutable and modify in static init remove GTEnumHelper

--- src/main/java/gregtech/mixins/vintagium/BlockRenderPassMixin.java
+++ src/main/java/gregtech/mixins/vintagium/BlockRenderPassMixin.java
@@ -1,43 +1,47 @@
 package gregtech.mixins.vintagium;
 
-import gregtech.api.util.GTEnumHelper;
 import gregtech.client.utils.BloomEffectUtil;
 import gregtech.client.utils.BloomEffectVintagiumUtil;
+
+import me.jellysquid.mods.sodium.client.util.BufferSizeUtil;
+import me.jellysquid.mods.sodium.client.util.EnumUtil;
 
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraftforge.common.util.EnumHelper;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import me.jellysquid.mods.sodium.client.render.chunk.passes.BlockRenderPass;
-import me.jellysquid.mods.sodium.client.util.BufferSizeUtil;
-import me.jellysquid.mods.sodium.client.util.EnumUtil;
-import org.apache.commons.lang3.ArrayUtils;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BlockRenderPass.class)
 public abstract class BlockRenderPassMixin {
 
-    @ModifyExpressionValue(method = "<clinit>",
-                           at = @At(value = "INVOKE",
-                                    target = "Lme/jellysquid/mods/sodium/client/render/chunk/passes/BlockRenderPass;values()[Lme/jellysquid/mods/sodium/client/render/chunk/passes/BlockRenderPass;"),
-                           remap = false)
-    private static BlockRenderPass[] addEnum(BlockRenderPass[] original) {
+    @Final
+    @Mutable
+    @Shadow(remap = false)
+    public static BlockRenderPass[] VALUES;
+
+    @Final
+    @Mutable
+    @Shadow(remap = false)
+    public static int COUNT;
+
+    @Inject(method = "<clinit>",
+            at = @At(value = "TAIL"),
+            remap = false)
+    private static void init(CallbackInfo ci) {
         EnumUtil.LAYERS = BlockRenderLayer.values();
         BufferSizeUtil.BUFFER_SIZES.put(BloomEffectUtil.getBloomLayer(), 131072);
 
         var params = new Class[] { BlockRenderLayer.class, boolean.class };
         var values = new Object[] { BloomEffectUtil.getBloomLayer(), true };
-        var field = GTEnumHelper.getValuesField(BlockRenderPass.class);
-        try {
-            BloomEffectVintagiumUtil.bloom = GTEnumHelper.makeEnum(BlockRenderPass.class, "BLOOM",
-                    original.length, params, values);
-            var array = ArrayUtils.add(original, BloomEffectVintagiumUtil.getBloomPass());
-            EnumHelper.setFailsafeFieldValue(field, null, array);
-            GTEnumHelper.cleanEnumCache(BlockRenderPass.class);
-            return array;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        BloomEffectVintagiumUtil.bloom = EnumHelper.addEnum(BlockRenderPass.class, "BLOOM", params, values);
+        VALUES = BlockRenderPass.values();
+        COUNT = VALUES.length;
     }
 }
