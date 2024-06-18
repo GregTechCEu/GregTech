31d39c81c5734369e0dffb6da866de04ad5b4b8e
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2024-06-18 16:57:32 -0700

    spotless weee

--- src/main/java/gregtech/mixins/vintagium/BlockRenderPassMixin.java
+++ src/main/java/gregtech/mixins/vintagium/BlockRenderPassMixin.java
@@ -3,13 +3,12 @@ package gregtech.mixins.vintagium;
 import gregtech.client.utils.BloomEffectUtil;
 import gregtech.client.utils.BloomEffectVintagiumUtil;
 
-import me.jellysquid.mods.sodium.client.util.BufferSizeUtil;
-import me.jellysquid.mods.sodium.client.util.EnumUtil;
-
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraftforge.common.util.EnumHelper;
 
 import me.jellysquid.mods.sodium.client.render.chunk.passes.BlockRenderPass;
+import me.jellysquid.mods.sodium.client.util.BufferSizeUtil;
+import me.jellysquid.mods.sodium.client.util.EnumUtil;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
