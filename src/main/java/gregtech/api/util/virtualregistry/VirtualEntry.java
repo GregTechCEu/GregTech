2db1d29236dc762efba946a29c5d220082b653c7
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2024-06-20 16:43:29 -0700

    a lot of work

--- src/main/java/gregtech/api/util/virtualregistry/VirtualEntry.java
+++ src/main/java/gregtech/api/util/virtualregistry/VirtualEntry.java
@@ -10,40 +10,33 @@ public abstract class VirtualEntry implements INBTSerializable<NBTTagCompound> {
     protected static final String DEFAULT_COLOR = "FFFFFFFF";
     protected static final String NAME_KEY = "entry_name";
     protected static final String COLOR_KEY = "color";
-    protected static final String ENTRY_KEY = "entry_tag";
-    private @NotNull NBTTagCompound entry = new NBTTagCompound();
+    private final @NotNull NBTTagCompound data = new NBTTagCompound();
 
-    public abstract EntryType getType();
+    public abstract <T extends VirtualEntry> EntryTypes<T> getType();
 
     public String getColor() {
-        return this.entry.getString(COLOR_KEY);
+        return this.data.getString(COLOR_KEY);
     }
 
     public void setColor(String color) {
-        this.entry.setString(COLOR_KEY, color == null ? DEFAULT_COLOR : color);
+        this.data.setString(COLOR_KEY, color == null ? DEFAULT_COLOR : color);
     }
 
     public String getName() {
-        return this.entry.getString(NAME_KEY);
+        return this.data.getString(NAME_KEY);
     }
 
     protected void setName(String name) {
-        this.entry.setString(NAME_KEY, name);
+        this.data.setString(NAME_KEY, name == null ? "null" : name);
     }
 
-    public NBTTagCompound getData() {
-        if (!this.entry.hasKey(ENTRY_KEY))
-            setData(new NBTTagCompound());
-
-        return this.entry.getCompoundTag(ENTRY_KEY);
-    }
-
-    public void setData(NBTTagCompound entry) {
-        this.entry.setTag(ENTRY_KEY, entry != null ? entry : new NBTTagCompound());
+    @NotNull
+    public final NBTTagCompound getData() {
+        return this.data;
     }
 
     @Override
-    public boolean equals(Object o) {
+    public final boolean equals(Object o) {
         if (!(o instanceof VirtualEntry other)) return false;
         return this.getType() == other.getType() &&
                 this.getName().equals(other.getName());
@@ -51,12 +44,14 @@ public abstract class VirtualEntry implements INBTSerializable<NBTTagCompound> {
     }
 
     @Override
-    public NBTTagCompound serializeNBT() {
-        return this.entry;
+    public final NBTTagCompound serializeNBT() {
+        return this.data;
     }
 
     @Override
-    public void deserializeNBT(NBTTagCompound nbt) {
-        this.entry = nbt;
+    public final void deserializeNBT(NBTTagCompound nbt) {
+        for (var key : nbt.getKeySet()) {
+            this.data.setTag(key, nbt.getTag(key));
+        }
     }
 }
--- src/main/java/gregtech/api/util/virtualregistry/VirtualRegistryMap.java
+++ src/main/java/gregtech/api/util/virtualregistry/VirtualRegistryMap.java
@@ -7,15 +7,13 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
-import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-public class VirtualRegistryMap implements INBTSerializable<NBTTagCompound>, Map<EntryType, Map<String, VirtualEntry>> {
+public class VirtualRegistryMap implements INBTSerializable<NBTTagCompound> {
 
-    private static final Map<EntryType, Map<String, VirtualEntry>> registryMap = new EnumMap<>(
-            EntryType.class);
+    private final Map<EntryTypes<?>, Map<String, VirtualEntry>> registryMap = new HashMap<>();
 
     public VirtualRegistryMap(NBTTagCompound tag) {
         deserializeNBT(tag);
@@ -23,98 +21,41 @@ public class VirtualRegistryMap implements INBTSerializable<NBTTagCompound>, Map
 
     public VirtualRegistryMap() {}
 
-    public VirtualEntry getEntry(EntryType type, String name) {
-        return get(type).get(name);
+    @SuppressWarnings("unchecked")
+    public <T extends VirtualEntry> T getEntry(EntryTypes<T> type, String name) {
+        return (T) registryMap.get(type).get(name);
     }
 
-    public void addEntry(VirtualEntry entry) {
-        computeIfAbsent(entry.getType(), k -> new HashMap<>())
+    public <T extends VirtualEntry> void addEntry(T entry) {
+        registryMap.computeIfAbsent(entry.getType(), k -> new HashMap<>())
                 .put(entry.getName(), entry);
     }
 
-    public boolean contains(EntryType type, String name) {
-        if (!containsKey(type))
+    public <T extends VirtualEntry> boolean contains(EntryTypes<T> type, String name) {
+        if (!registryMap.containsKey(type))
             return false;
 
-        return get(type).containsKey(name);
+        return registryMap.get(type).containsKey(name);
     }
 
-    public void deleteEntry(EntryType type, String name) {
-        get(type).remove(name);
-    }
-
-    @Override
-    public int size() {
-        return registryMap.size();
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return size() == 0;
-    }
-
-    @Override
-    public boolean containsKey(Object key) {
-        return registryMap.containsKey(key);
-    }
-
-    @Override
-    public boolean containsValue(Object value) {
-        return registryMap.containsValue(value);
-    }
-
-    @Override
-    public Map<String, VirtualEntry> get(Object key) {
-        return registryMap.get(key);
-    }
-
-    @Nullable
-    @Override
-    public Map<String, VirtualEntry> put(EntryType key, Map<String, VirtualEntry> value) {
-        return registryMap.put(key, value);
-    }
-
-    @Override
-    public Map<String, VirtualEntry> remove(Object key) {
-        return registryMap.remove(key);
-    }
-
-    @Override
-    public void putAll(@NotNull Map<? extends EntryType, ? extends Map<String, VirtualEntry>> m) {
-        registryMap.putAll(m);
+    public void deleteEntry(EntryTypes<?> type, String name) {
+        registryMap.get(type).remove(name);
     }
 
     public void clear() {
         registryMap.clear();
     }
 
-    @NotNull
-    @Override
-    public Set<EntryType> keySet() {
-        return registryMap.keySet();
-    }
-
-    @Override
-    public @NotNull Collection<Map<String, VirtualEntry>> values() {
-        return registryMap.values();
-    }
-
-    @Override
-    public Set<Entry<EntryType, Map<String, VirtualEntry>>> entrySet() {
-        return registryMap.entrySet();
-    }
-
     @Override
     public @NotNull NBTTagCompound serializeNBT() {
         var tag = new NBTTagCompound();
         for (var type : registryMap.keySet()) {
-            var typeTag = new NBTTagCompound();
+            var entriesTag = new NBTTagCompound();
             var entries = registryMap.get(type);
             for (var name : entries.keySet()) {
-                typeTag.setTag(name, entries.get(name).serializeNBT());
+                entriesTag.setTag(name, entries.get(name).serializeNBT());
             }
-            String key = type.getName() + ":" + type.ordinal();
-            tag.setTag(key, typeTag);
+            tag.setTag(type.toString(), entriesTag);
         }
         return tag;
     }
@@ -122,19 +63,13 @@ public class VirtualRegistryMap implements INBTSerializable<NBTTagCompound>, Map
     @Override
     public void deserializeNBT(NBTTagCompound nbt) {
         for (var entryType : nbt.getKeySet()) {
-            String[] split = entryType.split(":");
-            if (split.length != 2) continue;
-
-            EntryType type = EntryType.VALUES[Integer.parseInt(split[1])];
+            var type = EntryTypes.fromString(entryType);
+            if (type == null) continue;
 
             var virtualEntries = nbt.getCompoundTag(entryType);
             for (var name : virtualEntries.getKeySet()) {
-                var entry = VirtualRegistryBase.createEntry(type);
-                if (entry == null) continue;
-
-                entry.deserializeNBT(virtualEntries.getCompoundTag(name));
-                registryMap.computeIfAbsent(type, key -> new HashMap<>())
-                        .put(name, entry);
+                var entry = virtualEntries.getCompoundTag(name);
+                addEntry(type.createInstance(entry));
             }
         }
     }
