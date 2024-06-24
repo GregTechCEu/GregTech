ea1cb8b4759ef74ee7e3761406cf251c6bdc561d
bruberu <80226372+bruberu@users.noreply.github.com>
2024-06-23 23:33:39 -0500

    feat: describe EU/t for water a little better

--- src/main/java/gregtech/api/unification/material/materials/MaterialFlagAddition.java
+++ src/main/java/gregtech/api/unification/material/materials/MaterialFlagAddition.java
@@ -413,7 +413,7 @@ public class MaterialFlagAddition {
          */
         DistilledWater.setProperty(PropertyKey.COOLANT,
                 new CoolantProperty(Steam, HighPressureSteam, 1., 1000,
-                        373, 10., FissionReactor.standardPressure, 2260000, 4168).setSpecialCoolantAbsorption(10)
+                        373, 10., FissionReactor.standardPressure, 2260000, 4168).setSpecialCoolantAbsorption(250)
                                 .setAccumulatesHydrogen(true));
     }
 }
