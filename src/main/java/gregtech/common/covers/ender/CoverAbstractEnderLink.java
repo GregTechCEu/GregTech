3f2affe534bda2a4d484e99b67d48b51362ae4fc
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2024-06-26 11:28:57 -0700

    move ender fluid cover to abstract ender cover

--- src/main/java/gregtech/common/covers/ender/CoverAbstractEnderLink.java
+++ src/main/java/gregtech/common/covers/ender/CoverAbstractEnderLink.java
@@ -1,27 +1,38 @@
 package gregtech.common.covers.ender;
 
+import gregtech.api.capability.GregtechDataCodes;
 import gregtech.api.capability.IControllable;
 import gregtech.api.cover.CoverBase;
 import gregtech.api.cover.CoverDefinition;
 import gregtech.api.cover.CoverWithUI;
 import gregtech.api.cover.CoverableView;
+import gregtech.api.mui.GTGuiTextures;
 import gregtech.api.mui.GTGuis;
 import gregtech.api.util.virtualregistry.VirtualEntry;
-import gregtech.common.covers.CoverPump;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.ITickable;
 
 import codechicken.lib.raytracer.CuboidRayTraceResult;
+import com.cleanroommc.modularui.api.drawable.IKey;
+import com.cleanroommc.modularui.api.widget.IWidget;
+import com.cleanroommc.modularui.drawable.DynamicDrawable;
+import com.cleanroommc.modularui.drawable.Rectangle;
 import com.cleanroommc.modularui.factory.SidedPosGuiData;
 import com.cleanroommc.modularui.screen.ModularPanel;
+import com.cleanroommc.modularui.utils.Color;
+import com.cleanroommc.modularui.value.sync.BooleanSyncValue;
 import com.cleanroommc.modularui.value.sync.GuiSyncManager;
+import com.cleanroommc.modularui.widgets.ToggleButton;
 import com.cleanroommc.modularui.widgets.layout.Column;
+import com.cleanroommc.modularui.widgets.layout.Row;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -31,9 +42,10 @@ import java.util.regex.Pattern;
 public abstract class CoverAbstractEnderLink<T extends VirtualEntry> extends CoverBase
                                             implements CoverWithUI, ITickable, IControllable {
 
-    private static final Pattern COLOR_INPUT_PATTERN = Pattern.compile("[0-9a-fA-F]*");
+    protected static final Pattern COLOR_INPUT_PATTERN = Pattern.compile("[0-9a-fA-F]*");
+    public static final int UPDATE_PRIVATE = GregtechDataCodes.assignId();
 
-    protected CoverPump.PumpMode pumpMode = CoverPump.PumpMode.IMPORT;
+    protected T activeEntry = null;
 
     private UUID playerUUID = null;
     private boolean isPrivate = false;
@@ -43,22 +55,35 @@ public abstract class CoverAbstractEnderLink<T extends VirtualEntry> extends Cov
     public CoverAbstractEnderLink(@NotNull CoverDefinition definition, @NotNull CoverableView coverableView,
                                   @NotNull EnumFacing attachedSide) {
         super(definition, coverableView, attachedSide);
+        this.activeEntry = createEntry(createName(), this.playerUUID);
     }
 
-    protected abstract T getEntry();
+    protected abstract T createEntry(String name, UUID owner);
 
-    protected abstract void setEntry(T entry);
-
-    protected abstract void updateLink();
+    protected void updateLink() {
+        // todo use string variable for name checking instead of the old way
+        this.activeEntry = createEntry(createName(), getOwner());
+        markDirty();
+    }
 
     protected final String createName() {
-        return identifier() + getEntry().getColor();
+        String s = this.activeEntry == null ? VirtualEntry.DEFAULT_COLOR : this.activeEntry.getColor();
+        return identifier() + s;
     }
 
     protected abstract String identifier();
 
     protected final UUID getOwner() {
         return isPrivate ? playerUUID : null;
+    }
+
+    @Override
+    public void readCustomData(int discriminator, @NotNull PacketBuffer buf) {
+        super.readCustomData(discriminator, buf);
+        if (discriminator == UPDATE_PRIVATE) {
+            setPrivate(buf.readBoolean());
+            updateLink();
+        }
     }
 
     @Override
@@ -81,11 +106,18 @@ public abstract class CoverAbstractEnderLink<T extends VirtualEntry> extends Cov
 
     public void updateColor(String str) {
         if (str.length() == 8) {
-            getEntry().setColor(str);
-        } else {
-            getEntry().setColor(null);
+            this.activeEntry.setColor(str);
+            updateLink();
         }
-        updateLink();
+    }
+
+    public int parseColor(String s) {
+        // stupid java not having actual unsigned ints
+        long tmp = Long.parseLong(s, 16);
+        if (tmp > 0x7FFFFFFF) {
+            tmp -= 0x100000000L;
+        }
+        return (int) tmp;
     }
 
     @Override
@@ -104,6 +136,49 @@ public abstract class CoverAbstractEnderLink<T extends VirtualEntry> extends Cov
 
     protected abstract Column createWidgets(ModularPanel panel, GuiSyncManager guiSyncManager);
 
+    protected IWidget createColorIcon() {
+        // todo color selector popup panel
+        return new DynamicDrawable(() -> new Rectangle()
+                .setColor(parseColor(this.activeEntry.getColor()))
+                .asIcon().size(16))
+                        .asWidget()
+                        .background(GTGuiTextures.SLOT)
+                        .size(18)
+                        .marginRight(2);
+    }
+
+    protected IWidget createPrivateButton() {
+        var isPrivate = new BooleanSyncValue(this::isPrivate, this::setPrivate);
+        isPrivate.updateCacheFromSource(true);
+
+        return new ToggleButton()
+                .tooltip(tooltip -> tooltip.setAutoUpdate(true))
+                .background(GTGuiTextures.PRIVATE_MODE_BUTTON[0])
+                .hoverBackground(GTGuiTextures.PRIVATE_MODE_BUTTON[0])
+                .selectedBackground(GTGuiTextures.PRIVATE_MODE_BUTTON[1])
+                .selectedHoverBackground(GTGuiTextures.PRIVATE_MODE_BUTTON[1])
+                .tooltipBuilder(tooltip -> tooltip.addLine(IKey.lang(this.isPrivate ?
+                        "cover.ender_fluid_link.private.tooltip.enabled" :
+                        "cover.ender_fluid_link.private.tooltip.disabled")))
+                .marginRight(2)
+                .value(isPrivate);
+    }
+
+    protected IWidget createIoRow() {
+        var ioEnabled = new BooleanSyncValue(this::isIoEnabled, this::setIoEnabled);
+
+        return new Row().marginBottom(2)
+                .coverChildrenHeight()
+                .child(new ToggleButton()
+                        .value(ioEnabled)
+                        .overlay(IKey.dynamic(() -> IKey.lang(this.ioEnabled ?
+                                "behaviour.soft_hammer.enabled" :
+                                "behaviour.soft_hammer.disabled").get())
+                                .color(Color.WHITE.darker(1)))
+                        .widthRel(0.6f)
+                        .left(0));
+    }
+
     @Override
     public boolean isWorkingEnabled() {
         return workingEnabled;
@@ -114,11 +189,11 @@ public abstract class CoverAbstractEnderLink<T extends VirtualEntry> extends Cov
         this.workingEnabled = isActivationAllowed;
     }
 
-    private boolean isIoEnabled() {
+    public boolean isIoEnabled() {
         return ioEnabled;
     }
 
-    private void setIoEnabled(boolean ioEnabled) {
+    protected void setIoEnabled(boolean ioEnabled) {
         this.ioEnabled = ioEnabled;
     }
 
@@ -129,5 +204,39 @@ public abstract class CoverAbstractEnderLink<T extends VirtualEntry> extends Cov
     private void setPrivate(boolean isPrivate) {
         this.isPrivate = isPrivate;
         updateLink();
+        writeCustomData(UPDATE_PRIVATE, buffer -> buffer.writeBoolean(this.isPrivate));
+    }
+
+    @Override
+    public void writeInitialSyncData(PacketBuffer packetBuffer) {
+        // packetBuffer.writeInt(this.color);
+        packetBuffer.writeString(this.playerUUID == null ? "null" : this.playerUUID.toString());
+    }
+
+    @Override
+    public void readInitialSyncData(PacketBuffer packetBuffer) {
+        // this.color = packetBuffer.readInt();
+        // does client even need uuid info? just in case
+        String uuidStr = packetBuffer.readString(36);
+        this.playerUUID = uuidStr.equals("null") ? null : UUID.fromString(uuidStr);
+        // client does not need the actual tank reference, the default one will do just fine
+    }
+
+    @Override
+    public void readFromNBT(@NotNull NBTTagCompound nbt) {
+        super.readFromNBT(nbt);
+        this.ioEnabled = nbt.getBoolean("IOAllowed");
+        this.isPrivate = nbt.getBoolean("Private");
+        this.workingEnabled = nbt.getBoolean("WorkingAllowed");
+        this.playerUUID = UUID.fromString(nbt.getString("PlacedUUID"));
+    }
+
+    @Override
+    public void writeToNBT(@NotNull NBTTagCompound nbt) {
+        super.writeToNBT(nbt);
+        nbt.setBoolean("IOAllowed", ioEnabled);
+        nbt.setBoolean("Private", isPrivate);
+        nbt.setBoolean("WorkingAllowed", workingEnabled);
+        nbt.setString("PlacedUUID", playerUUID.toString());
     }
 }
