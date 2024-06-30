2af2cf8e9ab2f66a414794d0587da1bcb113785a
M-W-K <mwk001@icloud.com>
2024-06-30 08:52:20 -0600

    Alternate mixin

--- src/main/java/gregtech/mixins/jei/BasicRecipeTransferHandlerServerMixin.java
+++ src/main/java/gregtech/mixins/jei/BasicRecipeTransferHandlerServerMixin.java
@@ -2,136 +2,78 @@ package gregtech.mixins.jei;
 
 import gregtech.api.items.toolitem.ToolHelper;
 
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.Container;
-import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 
-import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
+import it.unimi.dsi.fastutil.ints.IntArrayList;
 import mezz.jei.transfer.BasicRecipeTransferHandlerServer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.HashMap;
-import java.util.List;
+import java.util.Iterator;
 import java.util.Map;
 
-@Mixin(BasicRecipeTransferHandlerServer.class)
+@Mixin(value = BasicRecipeTransferHandlerServer.class, remap = false)
 public class BasicRecipeTransferHandlerServerMixin {
 
     /**
-     * @author M_W_K
-     * @reason The modification required to make tools only be needed once on a max-transfer event is best done through
-     *         an overwrite, due to being spread out throughout the method.
+     * @reason create the list used to indicate what slots contain tools, and thus should be ignored on the 2nd+ checks
      */
-    @Overwrite(remap = false)
-    private static Map<Integer, ItemStack> removeItemsFromInventory(
-                                                                    EntityPlayer player,
-                                                                    Container container,
-                                                                    Map<Integer, ItemStack> required,
-                                                                    List<Integer> craftingSlots,
-                                                                    List<Integer> inventorySlots,
-                                                                    boolean transferAsCompleteSets,
-                                                                    boolean maxTransfer) {
-        List<Map.Entry<Integer, ItemStack>> orderedRequired = new ObjectArrayList<>(required.entrySet());
-
-        // This map becomes populated with the resulting items to transfer and is returned by this method.
-        final Map<Integer, ItemStack> result = new HashMap<>(orderedRequired.size());
-
-        loopSets:
-        while (true) { // for each set
-
-            // This map holds the original contents of a slot we have removed items from. This is used if we don't
-            // have enough items to complete a whole set, we can roll back the items that were removed.
-            Map<Slot, ItemStack> originalSlotContents = null;
-
-            if (transferAsCompleteSets) {
-                // We only need to create a new map for each set iteration if we're transferring as complete sets.
-                originalSlotContents = new HashMap<>();
-            }
-
-            // This map holds items found for each set iteration. Its contents are added to the result map
-            // after each complete set iteration. If we are transferring as complete sets, this allows
-            // us to simply ignore the map's contents when a complete set isn't found.
-            final Map<Integer, ItemStack> foundItemsInSet = new HashMap<>(orderedRequired.size());
-
-            // This flag is set to false if at least one item is found during the set iteration. It is used
-            // to determine if iteration should continue and prevents an infinite loop if not transferring
-            // as complete sets.
-            boolean noItemsFound = true;
-
-            for (int i = 0; i < orderedRequired.size(); i++) {
-                Map.Entry<Integer, ItemStack> entry = orderedRequired.get(i);
-                if (entry == null) continue; // null entries are set for tool stacks that have been satisfied
-
-                final ItemStack requiredStack = entry.getValue().copy();
-
-                // Locate a slot that has what we need.
-                final Slot slot = getSlotWithStack(container, requiredStack, craftingSlots, inventorySlots);
-
-                boolean itemFound = (slot != null) && !slot.getStack().isEmpty() && slot.canTakeStack(player);
-                ItemStack resultItemStack = result.get(entry.getKey());
-                boolean resultItemStackLimitReached = (resultItemStack != null) &&
-                        (resultItemStack.getCount() == resultItemStack.getMaxStackSize());
-
-                if (!itemFound || resultItemStackLimitReached) {
-                    // We can't find any more items to fulfill the requirements or the maximum stack size for this item
-                    // has been reached.
-
-                    if (transferAsCompleteSets) {
-                        // Since the full set requirement wasn't satisfied, we need to roll back any
-                        // slot changes we've made during this set iteration.
-                        for (Map.Entry<Slot, ItemStack> slotEntry : originalSlotContents.entrySet()) {
-                            ItemStack stack = slotEntry.getValue();
-                            slotEntry.getKey().putStack(stack);
-                        }
-                        break loopSets;
-                    }
-
-                } else { // the item was found and the stack limit has not been reached
-
-                    // Keep a copy of the slot's original contents in case we need to roll back.
-                    if (originalSlotContents != null && !originalSlotContents.containsKey(slot)) {
-                        originalSlotContents.put(slot, slot.getStack().copy());
-                    }
-
-                    // Reduce the size of the found slot.
-                    ItemStack removedItemStack = slot.decrStackSize(1);
-                    foundItemsInSet.put(entry.getKey(), removedItemStack);
-
-                    // mark tool stacks as 'permanently' satisfied
-                    if (ToolHelper.isTool(removedItemStack)) orderedRequired.set(i, null);
-
-                    noItemsFound = false;
-                }
-            }
-
-            // Merge the contents of the temporary map with the result map.
-            for (Map.Entry<Integer, ItemStack> entry : foundItemsInSet.entrySet()) {
-                ItemStack resultItemStack = result.get(entry.getKey());
-
-                if (resultItemStack == null) {
-                    result.put(entry.getKey(), entry.getValue());
-
-                } else {
-                    resultItemStack.grow(1);
-                }
-            }
-
-            if (!maxTransfer || noItemsFound) {
-                // If max transfer is not requested by the player this will exit the loop after trying one set.
-                // If no items were found during this iteration, we're done.
-                break;
-            }
-        }
-
-        return result;
+    @Inject(method = "removeItemsFromInventory", at = @At("HEAD"))
+    private static void gregtechCEu$initCompletedList(CallbackInfoReturnable<Map<Integer, ItemStack>> cir,
+                                                      @Share("completed") LocalRef<IntArrayList> completed) {
+        completed.set(new IntArrayList());
     }
 
-    @Shadow(remap = false)
-    private static Slot getSlotWithStack(Container container, ItemStack requiredStack, List<Integer> craftingSlots,
-                                         List<Integer> inventorySlots) {
-        return null;
+    /**
+     * @reason add the index of the entry to the list of skipped locations on the 2nd+ checks
+     */
+    @Inject(method = "removeItemsFromInventory",
+            at = @At(value = "INVOKE",
+                     target = "Ljava/util/Map;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;",
+                     ordinal = 1))
+    private static void gregtechCEu$markItemStackAsCompleted(CallbackInfoReturnable<Map<Integer, ItemStack>> cir,
+                                                             @Local(ordinal = 0) Map.Entry<Integer, ItemStack> entry,
+                                                             @Local(ordinal = 2) ItemStack removedItemStack,
+                                                             @Share("completed") LocalRef<IntArrayList> completed) {
+        // mark tool stacks as 'permanently' satisfied
+        if (removedItemStack != null && ToolHelper.isTool(removedItemStack)) completed.get().add(entry.getKey());
+    }
+
+    /**
+     * @reason if the given location is marked as completed, skip processing this slot and check the next slot
+     */
+    @ModifyExpressionValue(method = "removeItemsFromInventory",
+                           at = @At(value = "INVOKE", target = "Ljava/util/Iterator;hasNext()Z", ordinal = 0))
+    private static boolean gregtechCEu$skipCompletedStacks(boolean original,
+                                                           @Local(ordinal = 0) Iterator<Map.Entry<Integer, ItemStack>> iterator,
+                                                           @Share("completed") LocalRef<IntArrayList> completed,
+                                                           @Share("iterator_next") LocalRef<Map.Entry<Integer, ItemStack>> next) {
+        if (!original) return false;
+        next.set(iterator.next());
+        while (completed.get().contains(next.get().getKey())) {
+            if (!iterator.hasNext()) return false;
+            next.set(iterator.next());
+        }
+        return true;
+    }
+
+    /**
+     * @reason use the next variable that respects the completed status
+     */
+    @WrapOperation(method = "removeItemsFromInventory",
+                   at = @At(value = "INVOKE", target = "Ljava/util/Iterator;next()Ljava/lang/Object;", ordinal = 0))
+    private static Object gregtechCEu$useCorrectIteratorNext(Iterator<Map.Entry<Integer, ItemStack>> instance,
+                                                             Operation<Map.Entry<Integer, ItemStack>> original,
+                                                             @Share("completed") LocalRef<IntArrayList> completed,
+                                                             @Share("iterator_next") LocalRef<Map.Entry<Integer, ItemStack>> next) {
+        return next.get() == null ? original.call(instance) : next.get();
     }
 }
