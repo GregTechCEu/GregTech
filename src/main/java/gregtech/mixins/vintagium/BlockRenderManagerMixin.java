21c13357acd38c88b0a99f9165965716865aa8b3
TechLord22 <37029404+techlord22@users.noreply.github.com>
2024-07-05 17:56:31 -0400

    spotless

--- src/main/java/gregtech/mixins/vintagium/BlockRenderManagerMixin.java
+++ src/main/java/gregtech/mixins/vintagium/BlockRenderManagerMixin.java
@@ -10,7 +10,6 @@ import me.jellysquid.mods.sodium.client.render.chunk.passes.BlockRenderPass;
 import me.jellysquid.mods.sodium.client.render.chunk.passes.BlockRenderPassManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
