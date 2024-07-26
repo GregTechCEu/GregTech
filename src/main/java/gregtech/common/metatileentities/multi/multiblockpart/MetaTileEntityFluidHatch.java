573f8c32479e1f17d8ca93d0ebb3c09b29c29027
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2024-07-26 14:45:03 -0700

    no need to use this method here

--- src/main/java/gregtech/common/metatileentities/multi/multiblockpart/MetaTileEntityFluidHatch.java
+++ src/main/java/gregtech/common/metatileentities/multi/multiblockpart/MetaTileEntityFluidHatch.java
@@ -56,7 +56,6 @@ import com.cleanroommc.modularui.widgets.slot.ModularSlot;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Collections;
 import java.util.List;
 
 public class MetaTileEntityFluidHatch extends MetaTileEntityMultiblockNotifiablePart
@@ -249,13 +248,6 @@ public class MetaTileEntityFluidHatch extends MetaTileEntityMultiblockNotifiable
     @Override
     public MultiblockAbility<IFluidTank> getAbility() {
         return isExportHatch ? MultiblockAbility.EXPORT_FLUIDS : MultiblockAbility.IMPORT_FLUIDS;
-    }
-
-    @Override
-    public @NotNull List<MultiblockAbility<?>> getAbilities() {
-        return isExportHatch ?
-                Collections.singletonList(MultiblockAbility.EXPORT_FLUIDS) :
-                Collections.singletonList(MultiblockAbility.IMPORT_FLUIDS);
     }
 
     @Override
