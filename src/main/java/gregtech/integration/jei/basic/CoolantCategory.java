d75dbd66c9e17a7c8354101aef93ddf5d3682f74
bruberu <80226372+bruberu@users.noreply.github.com>
2024-07-26 23:27:29 -0500

    fix: JEI pages + GUI scaling

--- src/main/java/gregtech/integration/jei/basic/CoolantCategory.java
+++ src/main/java/gregtech/integration/jei/basic/CoolantCategory.java
@@ -21,11 +21,6 @@ public class CoolantCategory extends BasicRecipeCategory<CoolantInfo, CoolantInf
     private final IDrawable icon;
     protected final IDrawable slot;
     private final IDrawable arrow;
-    private String temps;
-    private String heatCapacity;
-    private String heatTransfer;
-    private String moderation;
-    private String hydrogen;
 
     public CoolantCategory(IGuiHelper guiHelper) {
         super("coolant", "fission.coolant.name", guiHelper.createBlankDrawable(176, 90), guiHelper);
@@ -51,23 +46,6 @@ public class CoolantCategory extends BasicRecipeCategory<CoolantInfo, CoolantInf
         fluidStackGroup.set(0, recipeWrapper.coolant);
         fluidStackGroup.init(1, true, 105, 9);
         fluidStackGroup.set(1, recipeWrapper.hotCoolant);
-
-        ICoolantStats coolant = CoolantRegistry.getCoolant(recipeWrapper.coolant.getFluid());
-
-        temps = I18n.format("gregtech.coolant.exit_temp",
-                coolant.getHotCoolant().getTemperature());
-        heatCapacity = I18n.format("gregtech.coolant.heat_capacity",
-                coolant.getSpecificHeatCapacity());
-        heatTransfer = I18n.format(I18n.format("gregtech.coolant.cooling_factor",
-                coolant.getCoolingFactor()));
-        moderation = I18n.format("gregtech.coolant.moderation_factor",
-                coolant.getModeratorFactor());
-
-        if (coolant.accumulatesHydrogen()) {
-            hydrogen = I18n.format("gregtech.coolant.accumulates_hydrogen");
-        } else {
-            hydrogen = "";
-        }
     }
 
     @Override
@@ -75,21 +53,9 @@ public class CoolantCategory extends BasicRecipeCategory<CoolantInfo, CoolantInf
         slot.draw(minecraft, 54, 8);
         slot.draw(minecraft, 104, 8);
         arrow.draw(minecraft, 77, 6);
-
-        int start = 40;
-        int linesDrawn = 0;
-        minecraft.fontRenderer.drawString(temps, 0, FONT_HEIGHT * linesDrawn + start, 0x111111);
-        linesDrawn++;
-        minecraft.fontRenderer.drawString(heatCapacity, 0, FONT_HEIGHT * linesDrawn + start, 0x111111);
-        linesDrawn++;
-        minecraft.fontRenderer.drawString(heatTransfer, 0, FONT_HEIGHT * linesDrawn + start, 0x111111);
-        linesDrawn++;
-        minecraft.fontRenderer.drawString(moderation, 0, FONT_HEIGHT * linesDrawn + start, 0x111111);
-        linesDrawn++;
-        if (hydrogen.length() > 0) {
-            minecraft.fontRenderer.drawString(hydrogen, 0, FONT_HEIGHT * linesDrawn + start, 0x111111);
-        }
     }
+
+
 
     @Override
     public IRecipeWrapper getRecipeWrapper(CoolantInfo recipe) {
--- src/main/java/gregtech/integration/jei/basic/CoolantInfo.java
+++ src/main/java/gregtech/integration/jei/basic/CoolantInfo.java
@@ -1,5 +1,10 @@
 package gregtech.integration.jei.basic;
 
+import gregtech.api.nuclear.fission.CoolantRegistry;
+import gregtech.api.nuclear.fission.ICoolantStats;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.I18n;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidStack;
 
@@ -12,14 +17,54 @@ public class CoolantInfo implements IRecipeWrapper {
     public FluidStack coolant;
     public FluidStack hotCoolant;
 
+    private String temps;
+    private String heatCapacity;
+    private String heatTransfer;
+    private String moderation;
+    private String hydrogen;
+
     public CoolantInfo(Fluid coolant, Fluid hotCoolant) {
         this.coolant = new FluidStack(coolant, 1000);
         this.hotCoolant = new FluidStack(hotCoolant, 1000);
+
+        ICoolantStats stats = CoolantRegistry.getCoolant(this.coolant.getFluid());
+
+        temps = I18n.format("gregtech.coolant.exit_temp",
+                stats.getHotCoolant().getTemperature());
+        heatCapacity = I18n.format("gregtech.coolant.heat_capacity",
+                stats.getSpecificHeatCapacity());
+        heatTransfer = I18n.format(I18n.format("gregtech.coolant.cooling_factor",
+                stats.getCoolingFactor()));
+        moderation = I18n.format("gregtech.coolant.moderation_factor",
+                stats.getModeratorFactor());
+        if (stats.accumulatesHydrogen()) {
+            hydrogen = I18n.format("gregtech.coolant.accumulates_hydrogen");
+        }
     }
 
     @Override
     public void getIngredients(IIngredients ingredients) {
         ingredients.setInput(VanillaTypes.FLUID, coolant);
         ingredients.setOutput(VanillaTypes.FLUID, hotCoolant);
+    }
+
+    @Override
+    public void drawInfo(Minecraft minecraft, int recipeWidth, int recipeHeight, int mouseX, int mouseY) {
+        int fontHeight = Minecraft.getMinecraft().fontRenderer.FONT_HEIGHT;
+
+        int start = 40;
+        int linesDrawn = 0;
+        minecraft.fontRenderer.drawString(temps, 0, fontHeight * linesDrawn + start, 0x111111);
+        linesDrawn++;
+        minecraft.fontRenderer.drawString(heatCapacity, 0, fontHeight * linesDrawn + start, 0x111111);
+        linesDrawn++;
+        minecraft.fontRenderer.drawString(heatTransfer, 0, fontHeight * linesDrawn + start, 0x111111);
+        linesDrawn++;
+        minecraft.fontRenderer.drawString(moderation, 0, fontHeight * linesDrawn + start, 0x111111);
+        linesDrawn++;
+
+        if (hydrogen != null) {
+            minecraft.fontRenderer.drawString(hydrogen, 0, fontHeight * linesDrawn + start, 0x111111);
+        }
     }
 }
