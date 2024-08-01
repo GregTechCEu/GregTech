bcb52ceecff13342e9131a48b0a4d14721c8611b
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2024-08-01 12:47:46 -0700

    initial rework

--- src/main/java/gregtech/api/recipes/chance/BaseChanceEntry.java
+++ src/main/java/gregtech/api/recipes/chance/BaseChanceEntry.java
@@ -11,10 +11,21 @@ public abstract class BaseChanceEntry<T> implements ChanceEntry<T> {
 
     private final T ingredient;
     private final int chance;
+    private final int numerator;
+    private final int denominator;
 
     public BaseChanceEntry(@NotNull T ingredient, int chance) {
         this.ingredient = ingredient;
         this.chance = chance;
+        this.numerator = 1;
+        this.denominator = (int) (1 / chance);
+    }
+
+    public BaseChanceEntry(@NotNull T ingredient, int numerator, int denominator) {
+        this.ingredient = ingredient;
+        this.chance = Math.floorDiv(numerator, denominator);
+        this.numerator = numerator;
+        this.denominator = denominator;
     }
 
     @Override
