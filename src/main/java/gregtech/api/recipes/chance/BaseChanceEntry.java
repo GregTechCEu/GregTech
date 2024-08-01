783e5f3e195ecc01f3fb942ec6ddb2ab7737cd0e
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2024-08-01 12:50:13 -0700

    a bit more work

--- src/main/java/gregtech/api/recipes/chance/BaseChanceEntry.java
+++ src/main/java/gregtech/api/recipes/chance/BaseChanceEntry.java
@@ -1,5 +1,7 @@
 package gregtech.api.recipes.chance;
 
+import gregtech.api.recipes.chance.output.ChancedOutputLogic;
+
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -17,8 +19,10 @@ public abstract class BaseChanceEntry<T> implements ChanceEntry<T> {
     public BaseChanceEntry(@NotNull T ingredient, int chance) {
         this.ingredient = ingredient;
         this.chance = chance;
-        this.numerator = 1;
-        this.denominator = (int) (1 / chance);
+        // chance of 500 = 5%
+        // 5% = 5/100 or 500 / 10000
+        this.numerator = chance;
+        this.denominator = ChancedOutputLogic.getMaxChancedValue();
     }
 
     public BaseChanceEntry(@NotNull T ingredient, int numerator, int denominator) {
@@ -36,5 +40,15 @@ public abstract class BaseChanceEntry<T> implements ChanceEntry<T> {
     @Override
     public int getChance() {
         return chance;
+    }
+
+    @Override
+    public int getNumerator() {
+        return numerator;
+    }
+
+    @Override
+    public int getDenominator() {
+        return denominator;
     }
 }
