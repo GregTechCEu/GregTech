2dbd5cbdfac15722b7efd4f786e5e2a1eb32b347
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2024-08-11 16:02:18 -0700

    store class for type checking add special list for instances

--- src/main/java/gregtech/api/metatileentity/multiblock/MultiblockAbility.java
+++ src/main/java/gregtech/api/metatileentity/multiblock/MultiblockAbility.java
@@ -8,6 +8,7 @@ import net.minecraftforge.fluids.capability.IFluidHandler;
 import net.minecraftforge.items.IItemHandlerModifiable;
 
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,61 +21,73 @@ public class MultiblockAbility<T> {
     public static final Map<String, MultiblockAbility<?>> NAME_REGISTRY = new HashMap<>();
     public static final Map<MultiblockAbility<?>, List<MetaTileEntity>> REGISTRY = new Object2ObjectOpenHashMap<>();
 
-    public static final MultiblockAbility<IItemHandlerModifiable> EXPORT_ITEMS = new MultiblockAbility<>(
-            "export_items");
-    public static final MultiblockAbility<IItemHandlerModifiable> IMPORT_ITEMS = new MultiblockAbility<>(
-            "import_items");
+    public static final MultiblockAbility<IItemHandlerModifiable> EXPORT_ITEMS = ability(
+            "export_items", IItemHandlerModifiable.class);
+    public static final MultiblockAbility<IItemHandlerModifiable> IMPORT_ITEMS = ability(
+            "import_items", IItemHandlerModifiable.class);
 
-    public static final MultiblockAbility<IFluidTank> EXPORT_FLUIDS = new MultiblockAbility<>("export_fluids");
-    public static final MultiblockAbility<IFluidTank> IMPORT_FLUIDS = new MultiblockAbility<>("import_fluids");
+    public static final MultiblockAbility<IFluidTank> EXPORT_FLUIDS = ability(
+            "export_fluids", IFluidTank.class);
+    public static final MultiblockAbility<IFluidTank> IMPORT_FLUIDS = ability(
+            "import_fluids", IFluidTank.class);
 
-    public static final MultiblockAbility<IEnergyContainer> INPUT_ENERGY = new MultiblockAbility<>("input_energy");
-    public static final MultiblockAbility<IEnergyContainer> OUTPUT_ENERGY = new MultiblockAbility<>("output_energy");
+    public static final MultiblockAbility<IEnergyContainer> INPUT_ENERGY = ability(
+            "input_energy", IEnergyContainer.class);
+    public static final MultiblockAbility<IEnergyContainer> OUTPUT_ENERGY = ability(
+            "output_energy", IEnergyContainer.class);
 
-    public static final MultiblockAbility<IEnergyContainer> SUBSTATION_INPUT_ENERGY = new MultiblockAbility<>(
-            "substation_input_energy");
-    public static final MultiblockAbility<IEnergyContainer> SUBSTATION_OUTPUT_ENERGY = new MultiblockAbility<>(
-            "substation_output_energy");
+    public static final MultiblockAbility<IEnergyContainer> SUBSTATION_INPUT_ENERGY = ability(
+            "substation_input_energy", IEnergyContainer.class);
+    public static final MultiblockAbility<IEnergyContainer> SUBSTATION_OUTPUT_ENERGY = ability(
+            "substation_output_energy", IEnergyContainer.class);
 
-    public static final MultiblockAbility<IRotorHolder> ROTOR_HOLDER = new MultiblockAbility<>("rotor_holder");
+    public static final MultiblockAbility<IRotorHolder> ROTOR_HOLDER = ability(
+            "rotor_holder", IRotorHolder.class);
 
-    public static final MultiblockAbility<IFluidTank> PUMP_FLUID_HATCH = new MultiblockAbility<>("pump_fluid_hatch");
+    public static final MultiblockAbility<IFluidTank> PUMP_FLUID_HATCH = ability(
+            "pump_fluid_hatch", IFluidTank.class);
 
-    public static final MultiblockAbility<IFluidTank> STEAM = new MultiblockAbility<>("steam");
-    public static final MultiblockAbility<IItemHandlerModifiable> STEAM_IMPORT_ITEMS = new MultiblockAbility<>(
-            "steam_import_items");
-    public static final MultiblockAbility<IItemHandlerModifiable> STEAM_EXPORT_ITEMS = new MultiblockAbility<>(
-            "steam_export_items");
+    public static final MultiblockAbility<IFluidTank> STEAM = new MultiblockAbility<>(
+            "steam", IFluidTank.class);
+    public static final MultiblockAbility<IItemHandlerModifiable> STEAM_IMPORT_ITEMS = ability(
+            "steam_import_items", IItemHandlerModifiable.class);
+    public static final MultiblockAbility<IItemHandlerModifiable> STEAM_EXPORT_ITEMS = ability(
+            "steam_export_items", IItemHandlerModifiable.class);
 
-    public static final MultiblockAbility<IMaintenanceHatch> MAINTENANCE_HATCH = new MultiblockAbility<>(
-            "maintenance_hatch");
-    public static final MultiblockAbility<IMufflerHatch> MUFFLER_HATCH = new MultiblockAbility<>("muffler_hatch");
+    public static final MultiblockAbility<IMaintenanceHatch> MAINTENANCE_HATCH = ability(
+            "maintenance_hatch", IMaintenanceHatch.class);
+    public static final MultiblockAbility<IMufflerHatch> MUFFLER_HATCH = ability(
+            "muffler_hatch", IMufflerHatch.class);
 
-    public static final MultiblockAbility<IItemHandlerModifiable> MACHINE_HATCH = new MultiblockAbility<>(
-            "machine_hatch");
+    public static final MultiblockAbility<IItemHandlerModifiable> MACHINE_HATCH = ability(
+            "machine_hatch", IItemHandlerModifiable.class);
 
-    public static final MultiblockAbility<IFluidHandler> TANK_VALVE = new MultiblockAbility<>("tank_valve");
+    public static final MultiblockAbility<IFluidHandler> TANK_VALVE = ability(
+            "tank_valve", IFluidHandler.class);
 
-    public static final MultiblockAbility<IPassthroughHatch> PASSTHROUGH_HATCH = new MultiblockAbility<>(
-            "passthrough_hatch");
+    public static final MultiblockAbility<IPassthroughHatch> PASSTHROUGH_HATCH = ability(
+            "passthrough_hatch", IPassthroughHatch.class);
 
-    public static final MultiblockAbility<IDataAccessHatch> DATA_ACCESS_HATCH = new MultiblockAbility<>(
-            "data_access_hatch");
-    public static final MultiblockAbility<IOpticalDataAccessHatch> OPTICAL_DATA_RECEPTION = new MultiblockAbility<>(
-            "optical_data_reception");
-    public static final MultiblockAbility<IOpticalDataAccessHatch> OPTICAL_DATA_TRANSMISSION = new MultiblockAbility<>(
-            "optical_data_transmission");
-    public static final MultiblockAbility<ILaserContainer> INPUT_LASER = new MultiblockAbility<>("input_laser");
-    public static final MultiblockAbility<ILaserContainer> OUTPUT_LASER = new MultiblockAbility<>("output_laser");
+    public static final MultiblockAbility<IDataAccessHatch> DATA_ACCESS_HATCH = ability(
+            "data_access_hatch", IDataAccessHatch.class);
+    public static final MultiblockAbility<IOpticalDataAccessHatch> OPTICAL_DATA_RECEPTION = ability(
+            "optical_data_reception", IOpticalDataAccessHatch.class);
+    public static final MultiblockAbility<IOpticalDataAccessHatch> OPTICAL_DATA_TRANSMISSION = ability(
+            "optical_data_transmission", IOpticalDataAccessHatch.class);
+    public static final MultiblockAbility<ILaserContainer> INPUT_LASER = ability(
+            "input_laser", ILaserContainer.class);
+    public static final MultiblockAbility<ILaserContainer> OUTPUT_LASER = ability(
+            "output_laser", ILaserContainer.class);
 
-    public static final MultiblockAbility<IOpticalComputationHatch> COMPUTATION_DATA_RECEPTION = new MultiblockAbility<>(
-            "computation_data_reception");
-    public static final MultiblockAbility<IOpticalComputationHatch> COMPUTATION_DATA_TRANSMISSION = new MultiblockAbility<>(
-            "computation_data_transmission");
+    public static final MultiblockAbility<IOpticalComputationHatch> COMPUTATION_DATA_RECEPTION = ability(
+            "computation_data_reception", IOpticalComputationHatch.class);
+    public static final MultiblockAbility<IOpticalComputationHatch> COMPUTATION_DATA_TRANSMISSION = ability(
+            "computation_data_transmission", IOpticalComputationHatch.class);
 
-    public static final MultiblockAbility<IHPCAComponentHatch> HPCA_COMPONENT = new MultiblockAbility<>(
-            "hpca_component");
-    public static final MultiblockAbility<IObjectHolder> OBJECT_HOLDER = new MultiblockAbility<>("object_holder");
+    public static final MultiblockAbility<IHPCAComponentHatch> HPCA_COMPONENT = ability(
+            "hpca_component", IHPCAComponentHatch.class);
+    public static final MultiblockAbility<IObjectHolder> OBJECT_HOLDER = ability(
+            "object_holder", IObjectHolder.class);
 
     public static void registerMultiblockAbility(MultiblockAbility<?> ability, MetaTileEntity part) {
         if (!REGISTRY.containsKey(ability)) {
@@ -83,10 +96,16 @@ public class MultiblockAbility<T> {
         REGISTRY.get(ability).add(part);
     }
 
-    private final String name;
+    public static <R> MultiblockAbility<R> ability(String name, Class<R> clazz) {
+        return new MultiblockAbility<>(name, clazz);
+    }
 
-    public MultiblockAbility(String name) {
+    private final String name;
+    private final Class<T> clazz;
+
+    public MultiblockAbility(String name, Class<T> clazz) {
         this.name = name.toLowerCase();
+        this.clazz = clazz;
         NAME_REGISTRY.put(this.name, this);
     }
 
@@ -99,5 +118,15 @@ public class MultiblockAbility<T> {
     public boolean equals(Object obj) {
         return obj instanceof MultiblockAbility<?>other &&
                 this.name.equals(other.name);
+    }
+
+    public boolean checkType(Object o) {
+        return clazz.isAssignableFrom(o.getClass());
+    }
+
+    public @Nullable T checkAndCast(Object o) {
+        if (checkType(o))
+            return clazz.cast(o);
+        else return null;
     }
 }
