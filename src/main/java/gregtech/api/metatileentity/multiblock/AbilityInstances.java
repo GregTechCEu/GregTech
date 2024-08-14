af173ab2b3cde0982e89311c48ee618ee9d9e176
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2024-08-13 20:40:21 -0700

    better javadocs

--- src/main/java/gregtech/api/metatileentity/multiblock/AbilityInstances.java
+++ src/main/java/gregtech/api/metatileentity/multiblock/AbilityInstances.java
@@ -7,6 +7,13 @@ import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Stores a list of {@link MultiblockAbility} instances.
+ * <br />
+ * <br />
+ * Make sure to use {@link AbilityInstances#isKey(MultiblockAbility)} to check what kind of
+ * instances to add to this list.
+ */
 public class AbilityInstances extends AbstractList<Object> {
 
     public static final AbilityInstances EMPTY = new AbilityInstances(null) {
@@ -57,6 +64,14 @@ public class AbilityInstances extends AbstractList<Object> {
         return (List<R>) this;
     }
 
+    /**
+     * Adds an instance of a MultiblockAbility to this list. If the object
+     * passed in is a list of instances that match the key, they will all be added.
+     * if the object is not what the key expects, an exception will be thrown.
+     * 
+     * @param o element whose class matches the type from the key
+     * @return true if the list was modified
+     */
     @Override
     public boolean add(Object o) {
         int s = size();
@@ -76,15 +91,24 @@ public class AbilityInstances extends AbstractList<Object> {
     public void add(int index, Object element) {
         if (!key.checkType(element))
             throw new IllegalArgumentException(
-                    String.format("element's class \"%s\" is not of type \"%s\"", element.getClass(), this.key.getType()));
+                    String.format("element's class \"%s\" is not of type \"%s\"", element.getClass(),
+                            this.key.getType()));
         instances.add(index, element);
     }
 
+    /**
+     * Replaces the element at the index. Will throw and exception if the element is not what the key expects.
+     * 
+     * @param index   index of the element to replace
+     * @param element element to be stored at the specified position
+     * @return the element previously stored at the index
+     */
     @Override
     public Object set(int index, Object element) {
         if (!key.checkType(element))
             throw new IllegalArgumentException(
-                    String.format("element's class \"%s\" is not of type \"%s\"", element.getClass(), this.key.getType()));
+                    String.format("element's class \"%s\" is not of type \"%s\"", element.getClass(),
+                            this.key.getType()));
         return instances.set(index, element);
     }
 
