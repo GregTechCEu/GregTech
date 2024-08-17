c5b398cd2379085f0bfda9dd80ccd64da177c497
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2024-08-17 16:02:00 -0700

    add overloaded method fix distinct fluids

--- src/main/java/gregtech/api/capability/impl/MultiblockRecipeLogic.java
+++ src/main/java/gregtech/api/capability/impl/MultiblockRecipeLogic.java
@@ -1,6 +1,7 @@
 package gregtech.api.capability.impl;
 
 import gregtech.api.GTValues;
+import gregtech.api.capability.DualHandler;
 import gregtech.api.capability.IEnergyContainer;
 import gregtech.api.capability.IMultiblockController;
 import gregtech.api.capability.IMultipleRecipeMaps;
@@ -97,6 +98,21 @@ public class MultiblockRecipeLogic extends AbstractRecipeLogic {
     protected IMultipleTankHandler getInputTank() {
         RecipeMapMultiblockController controller = (RecipeMapMultiblockController) metaTileEntity;
         return controller.getInputFluidInventory();
+    }
+
+    /**
+     * Overload of {@link #getInputTank()} to gather extra fluid tanks
+     * that could exist in a distinct item handler (such as a {@link DualHandler})
+     * 
+     * @param items Handler to gather fluid tanks from
+     * @return a new FluidTankList with extra fluid tanks on top of the existing fluid tanks
+     */
+    protected IMultipleTankHandler getInputTank(IItemHandler items) {
+        var tanks = new ArrayList<>(getInputTank().getFluidTanks());
+        if (items instanceof IMultipleTankHandler tankHandler) {
+            tanks.addAll(tankHandler.getFluidTanks());
+        }
+        return new FluidTankList(getInputTank().allowSameFluidFill(), tanks);
     }
 
     @Override
@@ -222,7 +238,7 @@ public class MultiblockRecipeLogic extends AbstractRecipeLogic {
                 continue;
             }
             // Look for a new recipe after a cache miss
-            currentRecipe = findRecipe(maxVoltage, bus, getInputTank());
+            currentRecipe = findRecipe(maxVoltage, bus, getInputTank(bus));
             // Cache the current recipe, if one is found
             if (currentRecipe != null && checkRecipe(currentRecipe)) {
                 this.previousRecipe = currentRecipe;
@@ -252,7 +268,7 @@ public class MultiblockRecipeLogic extends AbstractRecipeLogic {
     }
 
     protected boolean checkPreviousRecipeDistinct(IItemHandlerModifiable previousBus) {
-        return previousRecipe != null && previousRecipe.matches(false, previousBus, getInputTank());
+        return previousRecipe != null && previousRecipe.matches(false, previousBus, getInputTank(previousBus));
     }
 
     protected boolean prepareRecipeDistinct(Recipe recipe) {
@@ -262,14 +278,15 @@ public class MultiblockRecipeLogic extends AbstractRecipeLogic {
         recipe = findParallelRecipe(
                 recipe,
                 currentDistinctInputBus,
-                getInputTank(),
+                getInputTank(currentDistinctInputBus),
                 getOutputInventory(),
                 getOutputTank(),
                 getMaxParallelVoltage(),
                 getParallelLimit());
 
         if (recipe != null) {
-            recipe = setupAndConsumeRecipeInputs(recipe, currentDistinctInputBus, getInputTank());
+            recipe = setupAndConsumeRecipeInputs(recipe, currentDistinctInputBus,
+                    getInputTank(currentDistinctInputBus));
             if (recipe != null) {
                 setupRecipe(recipe);
                 return true;
