518a896382a41a9c80f3fbfc0b947fb794980ffd
TechLord22 <37029404+TechLord22@users.noreply.github.com>
2024-08-22 18:46:58 -0400

    fix duplicate assembly line research entries (#2584)

--- src/main/java/gregtech/api/recipes/machines/RecipeMapAssemblyLine.java
+++ src/main/java/gregtech/api/recipes/machines/RecipeMapAssemblyLine.java
@@ -62,6 +62,12 @@ public class RecipeMapAssemblyLine<R extends RecipeBuilder<R>> extends RecipeMap
     }
 
     @Override
+    protected void removeAllRecipes() {
+        super.removeAllRecipes();
+        researchEntries.clear();
+    }
+
+    @Override
     public void addDataStickEntry(@NotNull String researchId, @NotNull Recipe recipe) {
         if (researchId.contains("xmetaitem.")) {
             // save compatibility with an issue in 2.8.6, causing research IDs to change
