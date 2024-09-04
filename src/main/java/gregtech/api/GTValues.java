6dacbb8f53f96ea8e4f0472766c5197820d0244f
IncandescenceSy <76633436+IncandescenceSy@users.noreply.github.com>
2024-09-04 03:19:31 -0500

    apply spotless formatting

--- src/main/java/gregtech/api/GTValues.java
+++ src/main/java/gregtech/api/GTValues.java
@@ -78,9 +78,10 @@ public class GTValues {
      * The Voltage Tiers extended all the way to max Long value for overclocking
      */
     public static final long[] VOC = { 8, 32, 128, 512, 2048, 8192, 32768, 131072, 524288, 2097152, 8388608, 33554432,
-            134217728, 536870912, Integer.MAX_VALUE, 8589934592L, 34359738368L, 137438953472L, 549755813888L, 2199023255552L,
+            134217728, 536870912, Integer.MAX_VALUE, 8589934592L, 34359738368L, 137438953472L, 549755813888L,
+            2199023255552L,
             8796093022208L, 35184372088832L, 140737488355328L, 562949953421312L, 2251799813685248L, 9007199254740992L,
-            36028797018963968L, 144115188075855872L, 576460752303423488L, 2305843009213693952L, Long.MAX_VALUE};
+            36028797018963968L, 144115188075855872L, 576460752303423488L, 2305843009213693952L, Long.MAX_VALUE };
 
     public static final int ULV = 0;
     public static final int LV = 1;
@@ -109,7 +110,8 @@ public class GTValues {
     /**
      * The short names for the voltages, formatted for text
      */
-    public static final String MAX_PLUS = RED.toString() + BOLD + "M" + YELLOW + BOLD + "A" + GREEN + BOLD + "X" + AQUA + BOLD + "+" + LIGHT_PURPLE + BOLD;
+    public static final String MAX_PLUS = RED.toString() + BOLD + "M" + YELLOW + BOLD + "A" + GREEN + BOLD + "X" +
+            AQUA + BOLD + "+" + LIGHT_PURPLE + BOLD;
     public static final String[] VNF = new String[] {
             DARK_GRAY + "ULV", GRAY + "LV", AQUA + "MV",
             GOLD + "HV", DARK_PURPLE + "EV", DARK_BLUE + "IV",
@@ -119,7 +121,7 @@ public class GTValues {
             MAX_PLUS + "1", MAX_PLUS + "2", MAX_PLUS + "3", MAX_PLUS + "4",
             MAX_PLUS + "5", MAX_PLUS + "6", MAX_PLUS + "7", MAX_PLUS + "8",
             MAX_PLUS + "9", MAX_PLUS + "10", MAX_PLUS + "11", MAX_PLUS + "12",
-            MAX_PLUS + "13", MAX_PLUS + "14", MAX_PLUS + "15", MAX_PLUS + "16",};
+            MAX_PLUS + "13", MAX_PLUS + "14", MAX_PLUS + "15", MAX_PLUS + "16", };
 
     /**
      * Color values for the voltages
