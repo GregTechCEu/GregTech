093e4d79224653698e4e784707853d06b08fe4e3
TechLord22 <37029404+techlord22@users.noreply.github.com>
2024-09-13 17:52:48 -0400

    tierByVoltage optimizations
    Co-authored-by: M-W-K <31022105+M-W-K@users.noreply.github.com>
    Co-authored-by: kross <135918757+krossgg@users.noreply.github.com>

--- src/main/java/gregtech/api/util/GTUtility.java
+++ src/main/java/gregtech/api/util/GTUtility.java
@@ -257,7 +257,10 @@ public class GTUtility {
      *         tier that can handle it, {@code MAX} is returned.
      */
     public static byte getTierByVoltage(long voltage) {
-        return (byte) Math.min(GTValues.MAX, nearestLesser(V, voltage) + 1);
+        if (voltage >= Integer.MAX_VALUE) {
+            return GTValues.MAX;
+        }
+        return getOCTierByVoltage(voltage);
     }
 
     /**
@@ -266,7 +269,10 @@ public class GTUtility {
      *         tier that can handle it, {@code MAX_TRUE} is returned.
      */
     public static byte getOCTierByVoltage(long voltage) {
-        return (byte) Math.min(GTValues.MAX_TRUE, nearestLesser(VOC, voltage) + 1);
+        if (voltage <= GTValues.V[GTValues.ULV]) {
+            return GTValues.ULV;
+        }
+        return (byte) ((62 - Long.numberOfLeadingZeros(voltage - 1)) >> 1);
     }
 
     /**
@@ -276,7 +282,14 @@ public class GTUtility {
      *         {@code ULV} if there's no tier below
      */
     public static byte getFloorTierByVoltage(long voltage) {
-        return (byte) Math.max(GTValues.ULV, nearestLesserOrEqual(VOC, voltage));
+        if (voltage < GTValues.V[GTValues.LV]) {
+            return GTValues.ULV;
+        }
+        if (voltage == Long.MAX_VALUE) {
+            return GTValues.MAX_TRUE;
+        }
+
+        return (byte) ((60 - Long.numberOfLeadingZeros(voltage)) >> 1);
     }
 
     @SuppressWarnings("deprecation")
