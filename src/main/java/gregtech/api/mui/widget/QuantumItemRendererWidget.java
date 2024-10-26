1affdba8f5f8b70102664e1cae6976cade27b2b2
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2024-10-26 13:42:58 -0700

    start work on qchest item renderer

--- src/main/java/gregtech/api/mui/widget/QuantumItemRendererWidget.java
+++ src/main/java/gregtech/api/mui/widget/QuantumItemRendererWidget.java
@@ -1,11 +1,13 @@
 package gregtech.api.mui.widget;
 
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.items.IItemHandler;
 
 import com.cleanroommc.modularui.api.widget.Interactable;
 import com.cleanroommc.modularui.integration.jei.JeiGhostIngredientSlot;
 import com.cleanroommc.modularui.integration.jei.JeiIngredientProvider;
+import com.cleanroommc.modularui.screen.GuiScreenWrapper;
 import com.cleanroommc.modularui.screen.viewport.GuiContext;
 import com.cleanroommc.modularui.theme.WidgetTheme;
 import com.cleanroommc.modularui.widget.Widget;
@@ -37,6 +39,18 @@ public class QuantumItemRendererWidget extends Widget<QuantumItemRendererWidget>
     @Override
     public void draw(GuiContext context, WidgetTheme widgetTheme) {
         // draw stuff
+        ItemStack stack = itemHandler.getStackInSlot(0);
+        if (stack.isEmpty()) return;
+
+        GuiScreenWrapper screenWrapper = getScreen().getScreenWrapper();
+        var renderer = screenWrapper.getItemRenderer();
+        screenWrapper.setZ(100);
+        renderer.zLevel = 100;
+        GlStateManager.disableDepth();
+        renderer.renderItemAndEffectIntoGUI(stack, 1, 1);
+        GlStateManager.enableDepth();
+        screenWrapper.setZ(0);
+        renderer.zLevel = 0;
     }
 
     @Override
