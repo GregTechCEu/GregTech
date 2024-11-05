c085a4f181e5cca7df91613277007e7d4e9d8550
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2024-11-05 14:25:24 -0700

    add title row back ensure only one cover panel can be open at a time

--- src/main/java/gregtech/common/metatileentities/storage/MetaTileEntityCrate.java
+++ src/main/java/gregtech/common/metatileentities/storage/MetaTileEntityCrate.java
@@ -42,6 +42,7 @@ import com.cleanroommc.modularui.factory.PosGuiData;
 import com.cleanroommc.modularui.factory.SidedPosGuiData;
 import com.cleanroommc.modularui.screen.ModularPanel;
 import com.cleanroommc.modularui.utils.Alignment;
+import com.cleanroommc.modularui.value.sync.PanelSyncHandler;
 import com.cleanroommc.modularui.value.sync.PanelSyncManager;
 import com.cleanroommc.modularui.value.sync.SyncHandlers;
 import com.cleanroommc.modularui.widgets.ButtonWidget;
@@ -205,6 +206,7 @@ public class MetaTileEntityCrate extends MetaTileEntity {
                 .height(24 * 3);
 
         int numCovers = 0;
+        List<PanelSyncHandler> coverPanels = new ArrayList<>();
         for (EnumFacing side : EnumFacing.VALUES) {
             if (getCoverAtSide(side) instanceof CoverWithUI cover) {
                 if (!cover.shouldShowSmallUI()) continue;
@@ -215,9 +217,13 @@ public class MetaTileEntityCrate extends MetaTileEntity {
                 // todo better key for this?
                 var panel = manager.panel("cover at side: " + side.getName(), mainPanel,
                         (syncManager, syncHandler) -> cover.getSmallGUI(sideData, syncManager));
+                coverPanels.add(panel);
 
                 IGuiAction.MousePressed handlePanel = i -> {
                     if (!panel.isPanelOpen()) {
+                        coverPanels.forEach(h -> {
+                            if (h.isPanelOpen()) h.closePanel();
+                        });
                         panel.openPanel();
                     } else {
                         panel.closePanel();
