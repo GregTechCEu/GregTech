bcd15a8bf3066a49089840941668449c437c1b68
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2024-11-06 19:33:19 -0700

    fix dragging in certain conditions

--- src/main/java/gregtech/mixins/mui2/TextWidgetMixin.java
+++ src/main/java/gregtech/mixins/mui2/TextWidgetMixin.java
@@ -1,15 +1,21 @@
 package gregtech.mixins.mui2;
 
+import com.cleanroommc.modularui.widget.Widget;
 import com.cleanroommc.modularui.widgets.TextWidget;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(value = TextWidget.class, remap = false)
-public class TextWidgetMixin {
+public abstract class TextWidgetMixin extends Widget<TextWidget> {
 
     @ModifyReturnValue(method = { "getDefaultHeight", "getDefaultWidth" }, at = @At("TAIL"))
     public int clamp(int r) {
         return Math.max(1, r);
+    }
+
+    @Override
+    public boolean canHover() {
+        return hasTooltip();
     }
 }
