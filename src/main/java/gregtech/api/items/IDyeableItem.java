a23249b1528f4b14bd3e52528dfb41defff4b568
M-W-K <31022105+M-W-K@users.noreply.github.com>
2024-11-08 19:11:27 -0700

    spotless

--- src/main/java/gregtech/api/items/IDyeableItem.java
+++ src/main/java/gregtech/api/items/IDyeableItem.java
@@ -10,6 +10,7 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.common.util.Constants;
 
 import net.minecraftforge.common.util.Constants;
 
