c4b2c1681b9c8d93abe6b3f04893f6ca11699b41
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2024-11-10 19:32:32 -0700

    improve StateOverlay + mixin

--- src/main/java/gregtech/api/mui/StateOverlay.java
+++ src/main/java/gregtech/api/mui/StateOverlay.java
@@ -3,13 +3,20 @@ package gregtech.api.mui;
 import com.cleanroommc.modularui.api.drawable.IDrawable;
 import com.cleanroommc.modularui.widgets.ToggleButton;
 
+import java.util.function.Consumer;
+
 public interface StateOverlay {
 
-    ToggleButton overlay(boolean selected, IDrawable... overlay);
+    StateOverlay overlay(boolean selected, IDrawable... overlay);
 
-    ToggleButton hoverOverlay(boolean selected, IDrawable... overlay);
+    StateOverlay hoverOverlay(boolean selected, IDrawable... overlay);
 
-    static StateOverlay cast(ToggleButton button) {
-        return (StateOverlay) button;
+    static ToggleButton cast(ToggleButton button, Consumer<StateOverlay> function) {
+        function.accept((StateOverlay) button);
+        return button;
+    }
+
+    static ToggleButton create(Consumer<StateOverlay> function) {
+        return cast(new ToggleButton(), function);
     }
 }
--- src/main/java/gregtech/mixins/mui2/ToggleButtonMixin.java
+++ src/main/java/gregtech/mixins/mui2/ToggleButtonMixin.java
@@ -10,13 +10,13 @@ import org.spongepowered.asm.mixin.Mixin;
 @Mixin(value = ToggleButton.class, remap = false)
 public class ToggleButtonMixin extends AbstractCycleButtonWidget<ToggleButton> implements StateOverlay {
 
-    public ToggleButton overlay(boolean selected, IDrawable... overlay) {
+    public StateOverlay overlay(boolean selected, IDrawable... overlay) {
         this.overlay = addToArray(this.overlay, overlay, selected ? 1 : 0);
-        return getThis();
+        return (StateOverlay) getThis();
     }
 
-    public ToggleButton hoverOverlay(boolean selected, IDrawable... overlay) {
+    public StateOverlay hoverOverlay(boolean selected, IDrawable... overlay) {
         this.hoverOverlay = addToArray(this.hoverOverlay, overlay, selected ? 1 : 0);
-        return getThis();
+        return (StateOverlay) getThis();
     }
 }
