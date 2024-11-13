a9a9789205777c09da7ff735b1a3e4fe18396cdd
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2024-11-13 16:07:35 -0700

    simplify raytrace method

--- src/main/java/gregtech/api/items/metaitem/stats/ItemFluidContainer.java
+++ src/main/java/gregtech/api/items/metaitem/stats/ItemFluidContainer.java
@@ -13,7 +13,6 @@ import net.minecraft.util.EnumHand;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
@@ -92,24 +91,17 @@ public class ItemFluidContainer implements IItemContainerItemProvider, IItemBeha
         return pass(stack);
     }
 
-    // copied from Item.java
+    // copied and adapted from Item.java
     @Nullable
-    private static RayTraceResult rayTrace(World worldIn, EntityPlayer playerIn) {
-        float f = playerIn.rotationPitch;
-        float f1 = playerIn.rotationYaw;
-        double d0 = playerIn.posX;
-        double d1 = playerIn.posY + (double) playerIn.getEyeHeight();
-        double d2 = playerIn.posZ;
-        Vec3d vec3d = new Vec3d(d0, d1, d2);
-        float f2 = MathHelper.cos(-f1 * 0.017453292F - (float) Math.PI);
-        float f3 = MathHelper.sin(-f1 * 0.017453292F - (float) Math.PI);
-        float f4 = -MathHelper.cos(-f * 0.017453292F);
-        float f5 = MathHelper.sin(-f * 0.017453292F);
-        float f6 = f3 * f4;
-        float f7 = f2 * f4;
-        double d3 = playerIn.getEntityAttribute(EntityPlayer.REACH_DISTANCE).getAttributeValue();
-        Vec3d vec3d1 = vec3d.add((double) f6 * d3, (double) f5 * d3, (double) f7 * d3);
-        return worldIn.rayTraceBlocks(vec3d, vec3d1, true, false, false);
+    private static RayTraceResult rayTrace(World worldIn, EntityPlayer player) {
+        Vec3d lookPos = player.getPositionVector()
+                .add(0, player.getEyeHeight(), 0);
+
+        Vec3d lookOffset = player.getLookVec()
+                .scale(player.getEntityAttribute(EntityPlayer.REACH_DISTANCE).getAttributeValue());
+
+        return worldIn.rayTraceBlocks(lookPos, lookPos.add(lookOffset),
+                true, false, false);
     }
 
     @NotNull
