5aafea09d37183389198bc7811f1c3fd142f2f3a
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2024-12-01 20:06:17 -0700

    sync harder fix small issue with rebuilding display

--- src/main/java/gregtech/api/metatileentity/multiblock/MultiblockWithDisplayBase.java
+++ src/main/java/gregtech/api/metatileentity/multiblock/MultiblockWithDisplayBase.java
@@ -49,6 +49,7 @@ public abstract class MultiblockWithDisplayBase extends MultiblockControllerBase
     private static final String NBT_VOIDING_MODE = "VoidingMode";
     private static final String NBT_VOIDING_ITEMS = "VoidingItems";
     private static final String NBT_VOIDING_FLUIDS = "VoidingFluids";
+    private static final int UI_SYNC = GregtechDataCodes.assignId();
     private MultiblockUIFactory uiFactory;
 
     private boolean voidingItems = false;
@@ -205,6 +206,8 @@ public abstract class MultiblockWithDisplayBase extends MultiblockControllerBase
         }
         this.variantActiveBlocks = context.getOrDefault("VABlock", new LinkedList<>());
         replaceVariantBlocksActive(false);
+        if (uiFactory != null)
+            writeCustomData(UI_SYNC, uiFactory::writeInitialSync);;
     }
 
     @Override
@@ -556,6 +559,7 @@ public abstract class MultiblockWithDisplayBase extends MultiblockControllerBase
     @Override
     public final ModularPanel buildUI(PosGuiData guiData, PanelSyncManager panelSyncManager) {
         if (uiFactory == null) return null;
+        writeCustomData(UI_SYNC, uiFactory::writeInitialSync); // is this too early to sync?
         return this.uiFactory.buildUI(guiData, panelSyncManager);
     }
 
@@ -656,6 +660,10 @@ public abstract class MultiblockWithDisplayBase extends MultiblockControllerBase
         }
         if (dataId == IS_WORKING) {
             lastActive = buf.readBoolean();
+        }
+        if (dataId == UI_SYNC) {
+            uiFactory.readInitialSync(buf);
+            uiFactory.markDirty();
         }
     }
 
