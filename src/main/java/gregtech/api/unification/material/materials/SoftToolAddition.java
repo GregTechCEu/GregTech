c38866d9d242c1b22cb72dfc3a4a82a9527acad7
LittleCube <froakie20161113@gmail.com>
2025-01-06 21:46:11 +0800

    Change soft tool property registration to already existing method (ensureSet)

--- src/main/java/gregtech/api/unification/material/materials/SoftToolAddition.java
+++ src/main/java/gregtech/api/unification/material/materials/SoftToolAddition.java
@@ -14,22 +14,21 @@ public class SoftToolAddition {
             Materials.Polytetrafluoroethylene, Materials.Polybenzimidazole
     };
 
-    private static ExtraToolProperty ensureExtraToolProperty(Material material) {
-        if (material.getProperty(PropertyKey.EXTRATOOL) == null)
-            material.setProperty(PropertyKey.EXTRATOOL, new ExtraToolProperty());
-        return material.getProperty(PropertyKey.EXTRATOOL);
-    }
-
     public static void register() {
         for (int i = 0; i < softMaterials.length; i++) {
 
             Material material = softMaterials[i];
 
             if (ModHandler.isMaterialWood(material)) {
-                ensureExtraToolProperty(material).setOverrideProperty(ToolClasses.SOFT_MALLET,
+                material.getProperties().ensureSet(PropertyKey.EXTRATOOL);
+                ExtraToolProperty extraToolProperty = material.getProperty(PropertyKey.EXTRATOOL);
+
+                extraToolProperty.setOverrideProperty(ToolClasses.SOFT_MALLET,
                         ExtraToolProperty.Builder.of(4F, 1F, 48, 1).build());
             } else {
-                ExtraToolProperty extraToolProperty = ensureExtraToolProperty(material);
+                material.getProperties().ensureSet(PropertyKey.EXTRATOOL);
+                ExtraToolProperty extraToolProperty = material.getProperty(PropertyKey.EXTRATOOL);
+
                 extraToolProperty.setOverrideProperty(ToolClasses.SOFT_MALLET,
                         ExtraToolProperty.Builder.of(4F, 1F, 128 * (1 << i), 1).build());
                 extraToolProperty.setOverrideProperty(ToolClasses.PLUNGER,
