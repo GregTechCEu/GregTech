69b6f106836a114c7549579070638e03a0ce427a
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2025-01-09 18:25:20 -0700

    port pyrolyse and multismelter add hover method to KeyUtil

--- src/main/java/gregtech/common/metatileentities/multi/electric/MetaTileEntityMultiSmelter.java
+++ src/main/java/gregtech/common/metatileentities/multi/electric/MetaTileEntityMultiSmelter.java
@@ -9,6 +9,7 @@ import gregtech.api.metatileentity.multiblock.MultiblockAbility;
 import gregtech.api.metatileentity.multiblock.MultiblockDisplayText;
 import gregtech.api.metatileentity.multiblock.ParallelLogicType;
 import gregtech.api.metatileentity.multiblock.RecipeMapMultiblockController;
+import gregtech.api.metatileentity.multiblock.ui.MultiblockUIFactory;
 import gregtech.api.pattern.BlockPattern;
 import gregtech.api.pattern.FactoryBlockPattern;
 import gregtech.api.pattern.PatternMatchContext;
@@ -19,6 +20,7 @@ import gregtech.api.recipes.logic.OCResult;
 import gregtech.api.recipes.machines.RecipeMapFurnace;
 import gregtech.api.recipes.properties.RecipePropertyStorage;
 import gregtech.api.util.GTUtility;
+import gregtech.api.util.KeyUtil;
 import gregtech.api.util.TextComponentUtil;
 import gregtech.api.util.TextFormattingUtil;
 import gregtech.client.renderer.ICubeRenderer;
@@ -36,6 +38,9 @@ import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+import com.cleanroommc.modularui.api.drawable.IKey;
+import com.cleanroommc.modularui.value.sync.DoubleSyncValue;
+import com.cleanroommc.modularui.value.sync.IntSyncValue;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -105,16 +110,62 @@ public class MetaTileEntityMultiSmelter extends RecipeMapMultiblockController {
     }
 
     @Override
+    protected MultiblockUIFactory createUIFactory() {
+        DoubleSyncValue progress = new DoubleSyncValue(recipeMapWorkable::getProgressPercent, null);
+        IntSyncValue tier = new IntSyncValue(() -> GTUtility.getTierByVoltage(recipeMapWorkable.getMaxVoltage()), null);
+        tier.updateCacheFromSource(true);
+        IntSyncValue discount = new IntSyncValue(() -> heatingCoilDiscount, null);
+        IntSyncValue parallel = new IntSyncValue(() -> getRecipeMapWorkable().getParallelLimit(), null);
+        return new MultiblockUIFactory(this)
+                .syncValue("progress", progress)
+                .syncValue("tier", tier)
+                .syncValue("discount", discount)
+                .syncValue("para", parallel)
+                .configureDisplayText(builder -> builder
+                        .setWorkingStatus(recipeMapWorkable::isWorkingEnabled, recipeMapWorkable::isActive)
+                        .addEnergyUsageLine(this::getEnergyContainer)
+                        .addEnergyTierLine(tier.getIntValue())
+                        .addCustom(richText -> {
+                            if (!isStructureFormed()) return;
+
+                            if (discount.getIntValue() > 1) {
+                                IKey coilDiscount = KeyUtil.number(TextFormatting.AQUA,
+                                        (long) (100.0 / discount.getIntValue()), "%");
+
+                                IKey base = KeyUtil.lang(TextFormatting.GRAY,
+                                        "gregtech.multiblock.multi_furnace.heating_coil_discount",
+                                        coilDiscount);
+
+                                IKey hoverText = KeyUtil.lang(TextFormatting.GRAY,
+                                        "gregtech.multiblock.multi_furnace.heating_coil_discount_hover");
+
+                                richText.addLine(KeyUtil.setHover(base, hoverText));
+                            }
+
+                            if (parallel.getIntValue() > 0) {
+                                IKey parallels = KeyUtil.number(TextFormatting.DARK_PURPLE,
+                                        parallel.getIntValue());
+
+                                IKey bodyText = KeyUtil.lang(TextFormatting.GRAY,
+                                        "gregtech.multiblock.parallel",
+                                        parallels);
+
+                                IKey hoverText = KeyUtil.lang(TextFormatting.GRAY,
+                                        "gregtech.multiblock.multi_furnace.parallel_hover");
+
+                                richText.addLine(KeyUtil.setHover(bodyText, hoverText));
+                            }
+                        })
+                        .addWorkingStatusLine()
+                        .addProgressLine(progress::getDoubleValue));
+    }
+
+    @Override
     protected void formStructure(PatternMatchContext context) {
         super.formStructure(context);
-        Object coilType = context.get("CoilType");
-        if (coilType instanceof IHeatingCoilBlockStats) {
-            this.heatingCoilLevel = ((IHeatingCoilBlockStats) coilType).getLevel();
-            this.heatingCoilDiscount = ((IHeatingCoilBlockStats) coilType).getEnergyDiscount();
-        } else {
-            this.heatingCoilLevel = CoilType.CUPRONICKEL.getLevel();
-            this.heatingCoilDiscount = CoilType.CUPRONICKEL.getEnergyDiscount();
-        }
+        IHeatingCoilBlockStats coilType = context.getOrDefault("CoilType", CoilType.CUPRONICKEL);
+        this.heatingCoilLevel = coilType.getLevel();
+        this.heatingCoilDiscount = coilType.getEnergyDiscount();
     }
 
     @Override
