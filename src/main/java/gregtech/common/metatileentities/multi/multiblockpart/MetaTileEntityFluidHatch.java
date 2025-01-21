0bd9b2a31d2cfe24c5a3afa44c2ea34099b6b38f
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2025-01-21 14:11:37 -0700

    use better method

--- src/main/java/gregtech/common/metatileentities/multi/multiblockpart/MetaTileEntityFluidHatch.java
+++ src/main/java/gregtech/common/metatileentities/multi/multiblockpart/MetaTileEntityFluidHatch.java
@@ -205,7 +205,7 @@ public class MetaTileEntityFluidHatch extends MetaTileEntityMultiblockNotifiable
         if (isExportHatch) {
             this.locked = buf.readBoolean();
         } else {
-            this.circuitInventory.setCircuitValue(buf.readVarInt());
+            setGhostCircuitConfig(buf.readVarInt());
         }
     }
 
