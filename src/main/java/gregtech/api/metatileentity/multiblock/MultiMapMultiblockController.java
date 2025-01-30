25e4e0a704e721a8ea8b5d489bce76435a7f4105
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2025-01-30 15:50:31 -0700

    move trifunction back to bifunction and remove panel parameter cleanup/add todos

--- src/main/java/gregtech/api/metatileentity/multiblock/MultiMapMultiblockController.java
+++ src/main/java/gregtech/api/metatileentity/multiblock/MultiMapMultiblockController.java
@@ -136,7 +136,7 @@ public abstract class MultiMapMultiblockController extends RecipeMapMultiblockCo
     protected MultiblockUIFactory createUIFactory() {
         IntSyncValue recipeMapValue = new IntSyncValue(this::getRecipeMapIndex, this::setRecipeMapIndex);
         return super.createUIFactory()
-                .createFlexButton((panel, syncManager, guiData) -> {
+                .createFlexButton((guiData, syncManager) -> {
                     if (getAvailableRecipeMaps() == null || getAvailableRecipeMaps().length <= 1)
                         return null;
 
