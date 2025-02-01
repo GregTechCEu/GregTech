e275aceea8806d41b874aa01e862069b61ca6a67
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2025-01-31 19:43:17 -0700

    also replace `/n` with `\n`

--- src/main/java/gregtech/mixins/mui2/LangKeyMixin.java
+++ src/main/java/gregtech/mixins/mui2/LangKeyMixin.java
@@ -4,6 +4,7 @@ import gregtech.api.mui.LocaleAccessor;
 
 import com.cleanroommc.modularui.drawable.text.BaseKey;
 import com.cleanroommc.modularui.drawable.text.LangKey;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -16,6 +17,15 @@ public abstract class LangKeyMixin extends BaseKey {
               at = @At(value = "INVOKE",
                        target = "Lnet/minecraft/client/resources/I18n;format(Ljava/lang/String;[Ljava/lang/Object;)Ljava/lang/String;"))
     public String getTranslateKey(String translateKey, Object[] parameters) {
-        return LocaleAccessor.getRawKey(translateKey).replace("\\n", "\n");
+        return LocaleAccessor.getRawKey(translateKey)
+                .replace("\\n", "\n")
+                .replace("/n", "\n");
+    }
+
+    @ModifyExpressionValue(method = "get",
+                           at = @At(value = "INVOKE",
+                                    target = "Lnet/minecraft/client/resources/I18n;format(Ljava/lang/String;[Ljava/lang/Object;)Ljava/lang/String;"))
+    public String switchNewLines(String original) {
+        return original.replace("/n", "\n");
     }
 }
