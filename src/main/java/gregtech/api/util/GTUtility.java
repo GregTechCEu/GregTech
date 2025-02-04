eaee0de996e4041c436d17b55366eee6c70f3efe
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2025-02-04 12:51:56 -0700

    move `getFluidIKey` to KeyUtil and rename deprecate methods and schedule removal remove SECTION add notes

--- src/main/java/gregtech/api/util/GTUtility.java
+++ src/main/java/gregtech/api/util/GTUtility.java
@@ -54,11 +54,11 @@ import net.minecraftforge.fml.relauncher.ReflectionHelper;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.IItemHandlerModifiable;
 
-import com.cleanroommc.modularui.api.drawable.IKey;
 import com.google.common.util.concurrent.AtomicDouble;
 import it.unimi.dsi.fastutil.objects.ObjectOpenCustomHashSet;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.tuple.Pair;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -877,6 +877,8 @@ public class GTUtility {
     }
 
     @Contract("null -> null")
+    @Deprecated
+    @ApiStatus.ScheduledForRemoval(inVersion = "2.10")
     public static TextComponentTranslation getFluidTranslation(@Nullable FluidStack stack) {
         if (stack == null) return null;
         if (stack.getFluid() instanceof GTFluid.GTMaterialFluid materialFluid) {
@@ -887,6 +889,8 @@ public class GTUtility {
     }
 
     @Contract("null -> null")
+    @Deprecated
+    @ApiStatus.ScheduledForRemoval(inVersion = "2.10")
     public static TextComponentTranslation getFluidTranslation(@Nullable Fluid fluid) {
         if (fluid == null) return null;
         if (fluid instanceof GTFluid.GTMaterialFluid materialFluid) {
@@ -895,15 +899,7 @@ public class GTUtility {
         return new TextComponentTranslation(fluid.getUnlocalizedName());
     }
 
-    @Contract("null -> null")
-    public static IKey getFluidIKey(@Nullable Fluid fluid) {
-        if (fluid == null) return null;
-        if (fluid instanceof GTFluid.GTMaterialFluid materialFluid) {
-            return materialFluid.toIKey();
-        }
-        return IKey.lang(fluid.getUnlocalizedName());
-    }
-
+    @Deprecated
     public static @NotNull Pair<DoubleSupplier, DoubleSupplier> createPairedSupplier(int ticksPerCycle, int width,
                                                                                      double splitPoint) {
         AtomicDouble tracker = new AtomicDouble(0.0);
