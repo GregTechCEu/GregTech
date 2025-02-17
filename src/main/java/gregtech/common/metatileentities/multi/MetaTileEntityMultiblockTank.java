d25dce50531b391775c3928ba829b1c4da062149
Zorbatron <46525467+Zorbatron@users.noreply.github.com>
2025-02-17 02:18:34 -0500

    Convert the multiblock tank to MUI2

--- src/main/java/gregtech/common/metatileentities/multi/MetaTileEntityMultiblockTank.java
+++ src/main/java/gregtech/common/metatileentities/multi/MetaTileEntityMultiblockTank.java
@@ -1,5 +1,7 @@
 package gregtech.common.metatileentities.multi;
 
+import com.cleanroommc.modularui.widgets.FluidSlot;
+
 import gregtech.api.capability.impl.FilteredFluidHandler;
 import gregtech.api.capability.impl.FluidTankList;
 import gregtech.api.capability.impl.PropertyFluidFilter;
@@ -11,6 +13,10 @@ import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
 import gregtech.api.metatileentity.multiblock.IMultiblockPart;
 import gregtech.api.metatileentity.multiblock.MultiblockWithDisplayBase;
+import gregtech.api.metatileentity.multiblock.ui.MultiblockUIFactory;
+import gregtech.api.mui.GTGuiTextures;
+import gregtech.api.mui.GTGuis;
+import gregtech.api.mui.sync.GTFluidSyncHandler;
 import gregtech.api.pattern.BlockPattern;
 import gregtech.api.pattern.FactoryBlockPattern;
 import gregtech.client.renderer.ICubeRenderer;
@@ -19,6 +25,7 @@ import gregtech.common.blocks.BlockMetalCasing;
 import gregtech.common.blocks.BlockSteamCasing;
 import gregtech.common.blocks.MetaBlocks;
 import gregtech.common.metatileentities.MetaTileEntities;
+import gregtech.common.mui.widget.GTFluidSlot;
 
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.resources.I18n;
@@ -37,6 +44,11 @@ import codechicken.lib.raytracer.CuboidRayTraceResult;
 import codechicken.lib.render.CCRenderState;
 import codechicken.lib.render.pipeline.IVertexOperation;
 import codechicken.lib.vec.Matrix4;
+import com.cleanroommc.modularui.api.drawable.IKey;
+import com.cleanroommc.modularui.factory.PosGuiData;
+import com.cleanroommc.modularui.screen.ModularPanel;
+import com.cleanroommc.modularui.value.sync.PanelSyncManager;
+import com.cleanroommc.modularui.widgets.TextWidget;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -129,13 +141,27 @@ public class MetaTileEntityMultiblockTank extends MultiblockWithDisplayBase {
     }
 
     @Override
-    protected ModularUI.Builder createUITemplate(@NotNull EntityPlayer entityPlayer) {
-        return ModularUI.defaultBuilder()
-                .widget(new LabelWidget(6, 6, getMetaFullName()))
-                .widget(new TankWidget(importFluids.getTankAt(0), 52, 18, 72, 61)
-                        .setBackgroundTexture(GuiTextures.SLOT)
-                        .setContainerClicking(true, true))
-                .bindPlayerInventory(entityPlayer.inventory, GuiTextures.SLOT, 0);
+    protected MultiblockUIFactory createUIFactory() {
+        return new MultiblockUIFactory(this) {
+
+            @Override
+            public @NotNull ModularPanel buildUI(PosGuiData guiData, PanelSyncManager panelSyncManager) {
+                var panel = GTGuis.createPanel(MetaTileEntityMultiblockTank.this, 176, 166);
+
+                panel.child(new TextWidget(IKey.lang(getMetaFullName()))
+                        .pos(5, 5));
+                panel.child(new GTFluidSlot()
+                        .pos(52, 18)
+                        .size(72, 61)
+                        .background(GTGuiTextures.SLOT)
+                        .syncHandler(GTFluidSlot.sync(importFluids.getTankAt(0))
+                                .showAmountOnSlot(false)
+                                .drawAlwaysFull(false)));
+                panel.bindPlayerInventory();
+
+                return panel;
+            }
+        };
     }
 
     @Override
