abaaeeba12217ddc0e4a813573ab433a70605341
TechLord22 <37029404+techlord22@users.noreply.github.com>
2025-02-25 17:54:52 -0500

    spotless

--- src/main/java/gregtech/modules/ModuleManager.java
+++ src/main/java/gregtech/modules/ModuleManager.java
@@ -15,7 +15,6 @@ import com.google.common.base.Preconditions;
 import it.unimi.dsi.fastutil.objects.Object2ReferenceLinkedOpenHashMap;
 import it.unimi.dsi.fastutil.objects.ObjectLinkedOpenHashSet;
 import it.unimi.dsi.fastutil.objects.ReferenceLinkedOpenHashSet;
-import it.unimi.dsi.fastutil.objects.ReferenceOpenHashSet;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
