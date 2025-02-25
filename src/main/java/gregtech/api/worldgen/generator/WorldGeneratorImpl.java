012de01858e7fe29fe114841cd986022f62d8421
TechLord22 <37029404+TechLord22@users.noreply.github.com>
2025-02-25 18:31:42 -0500

    create the worldgen module (#2737)

--- src/main/java/gregtech/api/worldgen/generator/WorldGeneratorImpl.java
+++ src/main/java/gregtech/api/worldgen/generator/WorldGeneratorImpl.java
@@ -11,38 +11,18 @@ import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.IChunkGenerator;
 import net.minecraftforge.common.BiomeDictionary;
-import net.minecraftforge.event.terraingen.OreGenEvent;
-import net.minecraftforge.event.terraingen.OreGenEvent.GenerateMinable.EventType;
 import net.minecraftforge.fml.common.IWorldGenerator;
-import net.minecraftforge.fml.common.eventhandler.Event.Result;
-import net.minecraftforge.fml.common.eventhandler.EventPriority;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-
-import com.google.common.collect.ImmutableSet;
 
 import java.util.Random;
-import java.util.Set;
-
-import static net.minecraftforge.event.terraingen.OreGenEvent.GenerateMinable.EventType.*;
 
 public class WorldGeneratorImpl implements IWorldGenerator {
 
     public static final WorldGeneratorImpl INSTANCE = new WorldGeneratorImpl();
 
-    private static final Set<EventType> ORE_EVENT_TYPES = ImmutableSet.of(COAL, DIAMOND, GOLD, IRON, LAPIS, REDSTONE,
-            QUARTZ, EMERALD);
     public static final int GRID_SIZE_X = 3;
     public static final int GRID_SIZE_Z = 3;
 
     private WorldGeneratorImpl() {}
-
-    @SubscribeEvent(priority = EventPriority.HIGH)
-    public static void onOreGenerate(OreGenEvent.GenerateMinable event) {
-        EventType eventType = event.getType();
-        if (ConfigHolder.worldgen.disableVanillaOres && ORE_EVENT_TYPES.contains(eventType)) {
-            event.setResult(Result.DENY);
-        }
-    }
 
     @Override
     public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator,
