df36bf743e06b1b8e3075aaf8a26ed9e087501b5
TechLord22 <37029404+techlord22@users.noreply.github.com>
2025-02-25 15:31:53 -0500

    rewrite rubber trees

--- src/main/java/gregtech/api/worldgen/generator/WorldGeneratorImpl.java
+++ src/main/java/gregtech/api/worldgen/generator/WorldGeneratorImpl.java
@@ -1,16 +1,8 @@
 package gregtech.api.worldgen.generator;
 
-import gregtech.common.ConfigHolder;
-import gregtech.common.worldgen.WorldGenRubberTree;
-
-import net.minecraft.init.Biomes;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraft.world.biome.Biome;
-import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.IChunkGenerator;
-import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.fml.common.IWorldGenerator;
 
 import java.util.Random;
@@ -30,12 +22,6 @@ public class WorldGeneratorImpl implements IWorldGenerator {
         int selfGridX = Math.floorDiv(chunkX, GRID_SIZE_X);
         int selfGridZ = Math.floorDiv(chunkZ, GRID_SIZE_Z);
         generateInternal(world, selfGridX, selfGridZ, chunkX, chunkZ, random);
-
-        long rubberTreeSeed = random.nextLong();
-        if (!ConfigHolder.worldgen.disableRubberTreeGeneration) {
-            generateRubberTree(random, rubberTreeSeed, chunkProvider.provideChunk(chunkX, chunkZ),
-                    ConfigHolder.worldgen.rubberTreeRateIncrease);
-        }
     }
 
     private static void generateInternal(World world, int selfGridX, int selfGridZ, int chunkX, int chunkZ,
@@ -47,38 +33,6 @@ public class WorldGeneratorImpl implements IWorldGenerator {
                 CachedGridEntry cachedGridEntry = CachedGridEntry.getOrCreateEntry(world, selfGridX + gridX,
                         selfGridZ + gridZ, chunkX, chunkZ);
                 cachedGridEntry.populateChunk(world, chunkX, chunkZ, random);
-            }
-        }
-    }
-
-    private static void generateRubberTree(Random random, long seed, Chunk chunk, double baseScale) {
-        random.setSeed(seed);
-        Biome[] biomes = new Biome[4];
-        BlockPos.MutableBlockPos pos = new BlockPos.MutableBlockPos();
-        int seaLevel = chunk.getWorld().getSeaLevel();
-        for (int i = 0; i < 4; i++) {
-            int x = chunk.x * 16 + 8 + (i & 0x1) * 15;
-            int z = chunk.z * 16 + 8 + ((i & 0x2) >>> 1) * 15;
-            biomes[i] = chunk.getWorld().getBiomeProvider().getBiome(pos.setPos(x, seaLevel, z), Biomes.PLAINS);
-        }
-        int rubberTrees = 0;
-        for (Biome biome : biomes) {
-            if (biome != null) {
-                if (BiomeDictionary.hasType(biome, BiomeDictionary.Type.SWAMP))
-                    rubberTrees += random.nextInt(10) + 5;
-                if (BiomeDictionary.hasType(biome, BiomeDictionary.Type.FOREST) ||
-                        BiomeDictionary.hasType(biome, BiomeDictionary.Type.JUNGLE))
-                    rubberTrees += random.nextInt(5) + 1;
-            }
-        }
-        rubberTrees = (int) Math.round(rubberTrees * baseScale);
-        rubberTrees /= 2;
-        if (rubberTrees > 0 && random.nextInt(100) < rubberTrees) {
-            for (int j = 0; j < rubberTrees; j++) {
-                pos.setPos(chunk.x * 16 + random.nextInt(16), seaLevel, chunk.z * 16 + random.nextInt(16));
-                if (!WorldGenRubberTree.WORLD_GEN_INSTANCE.generateImpl(chunk.getWorld(), random, pos)) {
-                    rubberTrees -= 3;
-                }
             }
         }
     }
--- src/main/java/gregtech/worldgen/WorldgenModule.java
+++ src/main/java/gregtech/worldgen/WorldgenModule.java
@@ -5,7 +5,14 @@ import gregtech.api.modules.GregTechModule;
 import gregtech.common.ConfigHolder;
 import gregtech.modules.BaseGregTechModule;
 import gregtech.modules.GregTechModules;
+import gregtech.worldgen.impl.WorldGenRubberTree;
 
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+import net.minecraftforge.common.BiomeDictionary;
+import net.minecraftforge.event.terraingen.DecorateBiomeEvent;
 import net.minecraftforge.event.terraingen.OreGenEvent;
 import net.minecraftforge.fml.common.eventhandler.Event;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
@@ -18,6 +25,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Collections;
 import java.util.EnumSet;
 import java.util.List;
+import java.util.Random;
 import java.util.Set;
 
 import static net.minecraftforge.event.terraingen.OreGenEvent.GenerateMinable.EventType.*;
@@ -40,6 +48,11 @@ public class WorldgenModule extends BaseGregTechModule {
     }
 
     @Override
+    public @NotNull List<Class<?>> getTerrainGenBusSubscribers() {
+        return Collections.singletonList(WorldgenModule.class);
+    }
+
+    @Override
     public @NotNull List<Class<?>> getOreGenBusSubscribers() {
         return Collections.singletonList(WorldgenModule.class);
     }
@@ -49,5 +62,67 @@ public class WorldgenModule extends BaseGregTechModule {
         if (ConfigHolder.worldgen.disableVanillaOres && VANILLA_ORE_GEN_EVENT_TYPES.contains(event.getType())) {
             event.setResult(Event.Result.DENY);
         }
+    }
+
+    @SubscribeEvent
+    public static void onBiomeDecorate(@NotNull DecorateBiomeEvent.Decorate event) {
+        if (event.getType() == DecorateBiomeEvent.Decorate.EventType.TREE) {
+            if (ConfigHolder.worldgen.disableRubberTreeGeneration) {
+                return;
+            }
+
+            // replaces regular tree generation with occasional rubber trees
+            if (generateRubberTrees(event.getWorld(), event.getChunkPos(), event.getRand())) {
+                event.setResult(Event.Result.DENY);
+            }
+        }
+    }
+
+    /**
+     * Generates a rubber trees
+     *
+     * @param world    the world in which the tree should be placed
+     * @param chunkPos the position of the chunk the tree should be generated at
+     * @param random   the random number generator to use
+     * @return if trees were generated
+     */
+    private static boolean generateRubberTrees(@NotNull World world, @NotNull ChunkPos chunkPos,
+                                               @NotNull Random random) {
+        BlockPos.MutableBlockPos pos = new BlockPos.MutableBlockPos();
+
+        final int chunkX = chunkPos.x * 16;
+        final int chunkZ = chunkPos.z * 16;
+        pos.setPos(chunkX + 16, 0, chunkZ + 16);
+        Biome biome = world.getBiome(pos);
+
+        int amount = 0;
+        if (BiomeDictionary.hasType(biome, BiomeDictionary.Type.SWAMP)) {
+            amount += random.nextInt(10) + 5;
+        }
+        if (BiomeDictionary.hasType(biome, BiomeDictionary.Type.FOREST) ||
+                BiomeDictionary.hasType(biome, BiomeDictionary.Type.JUNGLE)) {
+            amount += random.nextInt(5) + 1;
+        }
+
+        amount = (int) (amount * ConfigHolder.worldgen.rubberTreeRateIncrease / 2);
+        if (amount > 0 && random.nextInt(100) < amount) {
+            boolean generated = false;
+            for (int i = 0; i < amount; i++) {
+                int x = chunkX + random.nextInt(16) + 8;
+                int z = chunkZ + random.nextInt(16) + 8;
+                int y = world.getHeight(x, z);
+                pos.setPos(x, y, z);
+
+                WorldGenRubberTree.INSTANCE.setDecorationDefaults();
+                if (WorldGenRubberTree.INSTANCE.generate(world, random, pos)) {
+                    WorldGenRubberTree.INSTANCE.generateSaplings(world, random, pos);
+                    generated = true;
+                }
+            }
+
+            return generated;
+        }
+
+        return false;
     }
 }
