f9a12bfd933ad0f0df704120e903a30fa5d4957a
TechLord22 <37029404+techlord22@users.noreply.github.com>
2025-02-25 15:39:57 -0500

    add API for GT Modules to subscribe to the Ore and Terrain Gen buses

--- src/main/java/gregtech/api/modules/IGregTechModule.java
+++ src/main/java/gregtech/api/modules/IGregTechModule.java
@@ -55,11 +55,29 @@ public interface IGregTechModule {
     default void registerPackets() {}
 
     /**
-     * @return A list of classes to subscribe to the Forge event bus.
-     *         As the class gets subscribed, not any specific instance, event handlers must be static!
+     * As the class gets subscribed, not any specific instance, event handlers must be static!
+     *
+     * @return A list of classes to subscribe to the Forge Event Bus, {@link net.minecraftforge.common.MinecraftForge#EVENT_BUS}.
      */
-    @NotNull
-    default List<Class<?>> getEventBusSubscribers() {
+    default @NotNull List<Class<?>> getEventBusSubscribers() {
+        return Collections.emptyList();
+    }
+
+    /**
+     * As the class gets subscribed, not any specific instance, event handlers must be static!
+     *
+     * @return A list of classes to subscribe to the Forge Terrain Gen Bus, {@link net.minecraftforge.common.MinecraftForge#TERRAIN_GEN_BUS}.
+     */
+    default @NotNull List<Class<?>> getTerrainGenBusSubscribers() {
+        return Collections.emptyList();
+    }
+
+    /**
+     * As the class gets subscribed, not any specific instance, event handlers must be static!
+     *
+     * @return A list of classes to subscribe to the Forge Ore Gen Bus, {@link net.minecraftforge.common.MinecraftForge#ORE_GEN_BUS}.
+     */
+    default @NotNull List<Class<?>> getOreGenBusSubscribers() {
         return Collections.emptyList();
     }
 
