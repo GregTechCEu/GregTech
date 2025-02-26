db335646d0b96775dc80e5f1cd9c404d413eb3a2
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2025-02-25 20:21:43 -0700

    add overload for syncObject simplify UISyncer methods

--- src/main/java/gregtech/api/metatileentity/multiblock/ui/UISyncer.java
+++ src/main/java/gregtech/api/metatileentity/multiblock/ui/UISyncer.java
@@ -1,12 +1,21 @@
 package gregtech.api.metatileentity.multiblock.ui;
 
+import com.cleanroommc.modularui.utils.serialization.ByteBufAdapters;
+import com.cleanroommc.modularui.utils.serialization.IByteBufAdapter;
 import com.cleanroommc.modularui.utils.serialization.IByteBufDeserializer;
 import com.cleanroommc.modularui.utils.serialization.IByteBufSerializer;
+import com.cleanroommc.modularui.utils.serialization.IEquals;
 import io.netty.buffer.ByteBuf;
+import org.jetbrains.annotations.NotNull;
 
 import java.math.BigInteger;
 
 public interface UISyncer {
+
+    IByteBufAdapter<BigInteger> BIG_INT = ByteBufAdapters.makeAdapter(
+            buffer -> new BigInteger(buffer.readByteArray()),
+            (buffer, value) -> buffer.writeByteArray(value.toByteArray()),
+            IEquals.defaultTester());
 
     boolean syncBoolean(boolean initial);
 
@@ -14,7 +23,9 @@ public interface UISyncer {
 
     long syncLong(long initial);
 
-    String syncString(String initial);
+    default @NotNull String syncString(@NotNull String initial) {
+        return syncObject(initial, ByteBufAdapters.STRING);
+    }
 
     byte syncByte(byte initial);
 
@@ -22,15 +33,19 @@ public interface UISyncer {
 
     float syncFloat(float initial);
 
-    BigInteger syncBigInt(BigInteger initial);
+    default BigInteger syncBigInt(BigInteger initial) {
+        return syncObject(initial, BIG_INT);
+    }
 
     <T> T syncObject(T initial, IByteBufSerializer<T> serializer, IByteBufDeserializer<T> deserializer);
+
+    default <T> T syncObject(T initial, IByteBufAdapter<T> adapter) {
+        return syncObject(initial, adapter, adapter);
+    }
 
     void readBuffer(ByteBuf buf);
 
     void writeBuffer(ByteBuf buf);
 
     boolean hasChanged();
-
-    void clear();
 }
