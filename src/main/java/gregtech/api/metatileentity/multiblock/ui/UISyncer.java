39e65bab46243776a62b6d8853385b38836e0294
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2025-03-04 01:14:45 -0700

    fix javadoc comments

--- src/main/java/gregtech/api/metatileentity/multiblock/ui/UISyncer.java
+++ src/main/java/gregtech/api/metatileentity/multiblock/ui/UISyncer.java
@@ -137,16 +137,16 @@ public interface UISyncer {
     }
 
     /**
-     * Syncs the elements of the collection to the internal buffer. <br />
+     * Syncs the elements of the array to the internal buffer. <br />
      * On the server, elements are serialized in the order of iteration. <br />
      * On the client, elements are deserialized in the same order. <br />
      * The array <b>will not</b> be modified, instead copied
      *
-     * @param initial      collection whose elements should be synced
+     * @param initial      array whose elements should be synced
      * @param serializer   serializer to write an element to the buffer
      * @param deserializer deserializer to read the elements to add them back to the collection
      * @param <T>          element type
-     * @return for server, the initial array, otherwise the synced array copy
+     * @return on the server, the initial array, otherwise the synced array copy
      */
     <T> T[] syncArray(T[] initial, IByteBufSerializer<T> serializer, IByteBufDeserializer<T> deserializer);
 
@@ -156,10 +156,10 @@ public interface UISyncer {
      * On the client, elements are deserialized in the same order. <br />
      * The array <b>will not</b> be modified, instead copied
      *
-     * @param initial collection whose elements should be synced
+     * @param initial array whose elements should be synced
      * @param adapter adapter for serialization/deserialization
      * @param <T>     element type
-     * @return for server, the initial array, otherwise the synced array copy
+     * @return on the server, the initial array, otherwise the synced array copy
      */
     default <T> T[] syncArray(T[] initial, IByteBufAdapter<T> adapter) {
         return syncArray(initial, adapter, adapter);
