cd36af4293352d3fc1f6c2092e387736b3ad99e8
Zorbatron <46525467+Zorbatron@users.noreply.github.com>
2025-05-11 01:40:56 -0400

    Add the network status of ME parts to TOP/WAILA/HWYLA

---
+++ src/main/java/gregtech/integration/theoneprobe/provider/AEMultiblockHatchProvider.java
@@ -0,0 +1,35 @@
+package gregtech.integration.theoneprobe.provider;
+
+import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
+import gregtech.common.metatileentities.multi.multiblockpart.appeng.MetaTileEntityAEHostablePart;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+
+import mcjty.theoneprobe.api.IProbeHitData;
+import mcjty.theoneprobe.api.IProbeInfo;
+import mcjty.theoneprobe.api.IProbeInfoProvider;
+import mcjty.theoneprobe.api.ProbeMode;
+
+public class AEMultiblockHatchProvider implements IProbeInfoProvider {
+
+    @Override
+    public String getID() {
+        return ":ae_multiblock_hatch_provider";
+    }
+
+    @Override
+    public void addProbeInfo(ProbeMode probeMode, IProbeInfo probeInfo, EntityPlayer entityPlayer, World world,
+                             IBlockState blockState, IProbeHitData probeHitData) {
+        if (blockState.getBlock().hasTileEntity(blockState) &&
+                world.getTileEntity(probeHitData.getPos()) instanceof IGregTechTileEntity gtte &&
+                gtte.getMetaTileEntity() instanceof MetaTileEntityAEHostablePart<?>aeHostablePart) {
+            if (aeHostablePart.isOnline()) {
+                probeInfo.text("{*gregtech.gui.me_network.online*}");
+            } else {
+                probeInfo.text("{*gregtech.gui.me_network.offline*}");
+            }
+        }
+    }
+}
