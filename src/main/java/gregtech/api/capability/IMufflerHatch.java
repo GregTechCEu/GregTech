0cfc4957a748e76946649513aca3209d8c9f9b2f
TechLord22 <37029404+TechLord22@users.noreply.github.com>
2025-05-17 19:25:21 -0400

    Remove Muffler Inventory and Recovery Mechanics (#2799)

--- src/main/java/gregtech/api/capability/IMufflerHatch.java
+++ src/main/java/gregtech/api/capability/IMufflerHatch.java
@@ -1,12 +1,6 @@
 package gregtech.api.capability;
 
-import net.minecraft.item.ItemStack;
-
-import java.util.List;
-
 public interface IMufflerHatch {
-
-    void recoverItemsTable(List<ItemStack> recoveryItems);
 
     /**
      * @return true if front face is free and contains only air blocks in 1x1 area
