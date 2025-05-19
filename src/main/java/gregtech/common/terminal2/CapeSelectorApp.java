56a604ef6cf28b34c73c64fb724aa3dfee530a0e
kumquat-ir <66188216+kumquat-ir@users.noreply.github.com>
2025-05-19 18:17:48 -0400

    cape selector finished

--- src/main/java/gregtech/common/terminal2/CapeSelectorApp.java
+++ src/main/java/gregtech/common/terminal2/CapeSelectorApp.java
@@ -1,10 +1,9 @@
 package gregtech.common.terminal2;
 
+import gregtech.api.mui.GTGuiTextures;
 import gregtech.api.terminal2.ITerminalApp;
 import gregtech.api.terminal2.Terminal2Theme;
 import gregtech.api.util.CapesRegistry;
-import gregtech.api.util.GTLog;
-import gregtech.client.renderer.texture.Textures;
 import gregtech.common.mui.widget.ColorableVScrollData;
 
 import net.minecraft.network.PacketBuffer;
@@ -12,6 +11,7 @@ import net.minecraft.util.ResourceLocation;
 
 import com.cleanroommc.modularui.api.drawable.IDrawable;
 import com.cleanroommc.modularui.api.widget.IWidget;
+import com.cleanroommc.modularui.drawable.DynamicDrawable;
 import com.cleanroommc.modularui.drawable.UITexture;
 import com.cleanroommc.modularui.factory.HandGuiData;
 import com.cleanroommc.modularui.screen.ModularPanel;
@@ -22,25 +22,28 @@ import com.cleanroommc.modularui.widgets.ButtonWidget;
 import com.cleanroommc.modularui.widgets.layout.Grid;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
+import java.util.function.Supplier;
 
-// todo: this needs testing on dedicated server once that no longer crashes (mui rc3)
 public class CapeSelectorApp implements ITerminalApp {
 
     private static final int SYNC_UNLOCKED_CAPES = 1;
     private static final int SELECT_CAPE = 2;
 
+    private CapeSelectorSyncHandler syncHandler;
+
     @Override
     public IWidget buildWidgets(HandGuiData guiData, PanelSyncManager guiSyncManager, ModularPanel panel) {
         UUID uuid = guiData.getPlayer().getPersistentID();
-        var syncHandler = new CapeSelectorSyncHandler(uuid);
+        syncHandler = new CapeSelectorSyncHandler(uuid);
         guiSyncManager.syncValue("cape_handler", syncHandler);
 
         var capeGrid = new Grid()
                 .sizeRel(1f)
                 .posRel(0.5f, 0.5f)
-                .minElementMargin(21)
+                .minElementMargin(18)
                 .scrollable(new ColorableVScrollData())
                 .nextRow();
         Terminal2Theme.COLOR_FOREGROUND_BRIGHT.bindScrollFG(capeGrid);
@@ -50,17 +53,25 @@ public class CapeSelectorApp implements ITerminalApp {
                 capeGrid.nextRow();
             }
 
-            capeGrid.child(new ButtonWidget<>()
+            IWidget capeButton = new ButtonWidget<>()
                     .size(40, 72)
+                    .posRel(0.5F, 0.5F)
                     .overlay(new UITexture(cape, 1f / 64, 1f / 32, 11f / 64, 17f / 32, false))
                     .background(Terminal2Theme.COLOR_BACKGROUND_2)
                     .disableHoverOverlay()
-                    .hoverBackground(Terminal2Theme.COLOR_DARK_4)
                     .onMousePressed(b -> {
-                        GTLog.logger.info("set {}", cape.toString());
                         syncHandler.syncToServer(SELECT_CAPE, buf -> buf.writeString(cape.toString()));
                         return true;
-                    }));
+                    });
+
+            capeGrid.child(new ParentWidget<>()
+                    .size(46, 78)
+                    .background(new DynamicDrawable(capeBackground(cape)))
+                    .overlay(new DynamicDrawable(capeForeground(cape)))
+                    .child(capeButton)
+                    .child(new DynamicDrawable(capeOverlay(cape)).asWidget()
+                            .size(24)
+                            .posRel(0.5F, 0.5F)));
         }
 
         return new ParentWidget<>()
@@ -72,31 +83,69 @@ public class CapeSelectorApp implements ITerminalApp {
 
     @Override
     public IDrawable getIcon() {
-        return new UITexture(Textures.GREGTECH_CAPE_TEXTURE, 32f / 64, 0, 46f / 64, 22f / 32, false);
+        return GTGuiTextures.CAPES_APP_ICON;
+    }
+
+    @Override
+    public void onOpen() {
+        syncHandler.syncUnlockedCapes();
+    }
+
+    @Override
+    public void dispose() {
+        syncHandler = null;
+    }
+
+    private Supplier<IDrawable> capeBackground(ResourceLocation cape) {
+        return () -> {
+            if (cape.equals(CapesRegistry.getPlayerCape(syncHandler.uuid))) {
+                return Terminal2Theme.COLOR_BRIGHT_1;
+            } else if (syncHandler.unlockedCapes.contains(cape)) {
+                return Terminal2Theme.COLOR_BACKGROUND_2;
+            }
+            return Terminal2Theme.COLOR_BRIGHT_3;
+        };
+    }
+
+    private Supplier<IDrawable> capeOverlay(ResourceLocation cape) {
+        return () -> syncHandler.unlockedCapes.contains(cape) ? IDrawable.EMPTY : GTGuiTextures.RECIPE_LOCK;
+    }
+
+    private Supplier<IDrawable> capeForeground(ResourceLocation cape) {
+        return () -> syncHandler.unlockedCapes.contains(cape) ? IDrawable.EMPTY : Terminal2Theme.COLOR_FOREGROUND_DARK;
     }
 
     private static final class CapeSelectorSyncHandler extends SyncHandler {
 
         private final UUID uuid;
-        private List<ResourceLocation> unlockedCapes;
+        private List<ResourceLocation> unlockedCapes = Collections.emptyList();
 
         public CapeSelectorSyncHandler(UUID uuid) {
             this.uuid = uuid;
         }
 
-//        @Override
-//        public void init(String key, PanelSyncManager syncManager) {
-//            super.init(key, syncManager);
-//            if (!getSyncManager().isClient()) {
-//                unlockedCapes = CapesRegistry.getUnlockedCapes(uuid);
-//                syncToClient(SYNC_UNLOCKED_CAPES, buf -> {
-//                    buf.writeVarInt(unlockedCapes.size());
-//                    for (ResourceLocation cape : unlockedCapes) {
-//                        buf.writeString(cape.toString());
-//                    }
-//                });
-//            }
-//        }
+        private void syncUnlockedCapes() {
+            if (!getSyncManager().isClient()) {
+                unlockedCapes = CapesRegistry.getUnlockedCapes(uuid);
+                syncToClient(SYNC_UNLOCKED_CAPES, buf -> {
+                    buf.writeVarInt(unlockedCapes.size());
+                    for (ResourceLocation cape : unlockedCapes) {
+                        buf.writeString(cape.toString());
+                    }
+                });
+            } else {
+                syncToServer(SYNC_UNLOCKED_CAPES);
+            }
+        }
+
+        @Override
+        public void detectAndSendChanges(boolean init) {
+            super.detectAndSendChanges(init);
+            if (init) {
+                // prevents flicker when opening the app on dedicated server
+                syncUnlockedCapes();
+            }
+        }
 
         @Override
         public void readOnClient(int id, PacketBuffer buf) {
@@ -112,7 +161,16 @@ public class CapeSelectorApp implements ITerminalApp {
         @Override
         public void readOnServer(int id, PacketBuffer buf) {
             if (id == SELECT_CAPE) {
-                CapesRegistry.giveCape(uuid, new ResourceLocation(buf.readString(Short.MAX_VALUE)));
+                ResourceLocation cape = new ResourceLocation(buf.readString(Short.MAX_VALUE));
+                if (unlockedCapes.contains(cape)) {
+                    if (cape.equals(CapesRegistry.getPlayerCape(uuid))) {
+                        CapesRegistry.giveCape(uuid, null);
+                    } else {
+                        CapesRegistry.giveCape(uuid, cape);
+                    }
+                }
+            } else if (id == SYNC_UNLOCKED_CAPES) {
+                syncUnlockedCapes();
             }
         }
     }
