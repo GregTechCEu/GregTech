0d624229d31f64f92c3c974c495b37b562455aa9
Zorbatron <46525467+Zorbatron@users.noreply.github.com>
2025-05-19 21:53:24 -0400

    Item slot jei dragging (ish, just logging for now :troll:)

--- src/main/java/gregtech/api/mui/widget/appeng/AEConfigSlot.java
+++ src/main/java/gregtech/api/mui/widget/appeng/AEConfigSlot.java
@@ -3,32 +3,39 @@ package gregtech.api.mui.widget.appeng;
 import gregtech.common.metatileentities.multi.multiblockpart.appeng.slot.IConfigurableSlot;
 
 import appeng.api.storage.data.IAEStack;
+import com.cleanroommc.modularui.api.drawable.IKey;
 import com.cleanroommc.modularui.screen.RichTooltip;
-import com.cleanroommc.modularui.screen.viewport.ModularGuiContext;
 import com.cleanroommc.modularui.widget.Widget;
 import org.jetbrains.annotations.Nullable;
 
 public class AEConfigSlot<T extends IAEStack<T>> extends Widget<AEConfigSlot<T>> {
 
-    private final IConfigurableSlot<T> config;
+    protected final IConfigurableSlot<T> backingSlot;
     private IConfigurableSlot<T> cached;
-    private final boolean isStocking;
+    protected final boolean isStocking;
 
-    public AEConfigSlot(IConfigurableSlot<T> config, boolean isStocking) {
-        super();
-        this.config = config;
+    public AEConfigSlot(IConfigurableSlot<T> backingSlot, boolean isStocking) {
+        this.backingSlot = backingSlot;
         this.isStocking = isStocking;
     }
 
     @Override
     public @Nullable RichTooltip getTooltip() {
-        if (config.getConfig() == null) {
-
+        if (backingSlot.getConfig() != null) {
+            return null;
         }
-    }
 
-    @Override
-    public void drawForeground(ModularGuiContext context) {
-        super.drawForeground(context);
+        RichTooltip tooltip = new RichTooltip(this);
+
+        tooltip.addLine(IKey.lang("gregtech.gui.config_slot"));
+        if (isStocking) {
+            tooltip.addLine(IKey.lang("gregtech.gui.config_slot.set_only"));
+        } else {
+            tooltip.addLine(IKey.lang("gregtech.gui.config_slot.set"));
+            tooltip.addLine(IKey.lang("gregtech.gui.config_slot.scroll"));
+        }
+        tooltip.addLine(IKey.lang("gregtech.gui.config_slot.remove"));
+
+        return tooltip;
     }
 }
--- src/main/java/gregtech/api/mui/widget/appeng/AEItemConfigSlot.java
+++ src/main/java/gregtech/api/mui/widget/appeng/AEItemConfigSlot.java
@@ -1,12 +1,43 @@
 package gregtech.api.mui.widget.appeng;
 
+import gregtech.api.util.GTLog;
 import gregtech.common.metatileentities.multi.multiblockpart.appeng.slot.ExportOnlyAEItemList;
 
-import appeng.api.storage.data.IAEItemStack;
+import net.minecraft.item.ItemStack;
 
-public class AEItemConfigSlot extends AEConfigSlot<IAEItemStack> {
+import appeng.api.storage.data.IAEItemStack;
+import com.cleanroommc.modularui.api.widget.Interactable;
+import com.cleanroommc.modularui.integration.jei.JeiGhostIngredientSlot;
+import com.cleanroommc.modularui.integration.jei.JeiIngredientProvider;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class AEItemConfigSlot extends AEConfigSlot<IAEItemStack> implements Interactable,
+                              JeiGhostIngredientSlot<ItemStack>,
+                              JeiIngredientProvider {
 
     public AEItemConfigSlot(ExportOnlyAEItemList itemList, int index) {
         super(itemList.getInventory()[index], itemList.isStocking());
+    }
+
+    @Override
+    public void onInit() {
+        size(18, 18);
+        getContext().getJeiSettings().addJeiGhostIngredientSlot(this);
+    }
+
+    @Override
+    public void setGhostIngredient(@NotNull ItemStack ingredient) {
+        GTLog.logger.info(ingredient);
+    }
+
+    @Override
+    public @Nullable ItemStack castGhostIngredientIfValid(@NotNull Object ingredient) {
+        return ingredient instanceof ItemStack stack ? stack : null;
+    }
+
+    @Override
+    public @Nullable Object getIngredient() {
+        return backingSlot.getConfig();
     }
 }
