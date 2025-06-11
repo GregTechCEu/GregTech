GregTechCEu/GregTech

commit 55b574c4c97f65d942055b44e853f716acb16ec6

use hasconfig

src/main/java/gregtech/api/mui/widget/appeng/AEConfigSlot.java
@@ -130,7 +130,7 @@ public abstract class AEConfigSlot<T extends IAEStack<T>> extends Widget<AEConfi
 
     @Override
     public boolean onMouseScroll(ModularScreen.UpOrDown scrollDirection, int scrollAmount) {
-        if (getSyncHandler().getConfig(index) == null || isStocking) return false;
+        if (getSyncHandler().hasConfig(index) || isStocking) return false;
 
         long newStackSize = getSyncHandler().getConfigAmount(index);
 

commit 2feeffe456ec3d468e9d59f69d1b9f6e2a673401

Fix regression in 55b574c4 which broke scrolling

src/main/java/gregtech/api/mui/widget/appeng/AEConfigSlot.java
@@ -130,7 +130,7 @@ public abstract class AEConfigSlot<T extends IAEStack<T>> extends Widget<AEConfi
 
     @Override
     public boolean onMouseScroll(ModularScreen.UpOrDown scrollDirection, int scrollAmount) {
-        if (getSyncHandler().hasConfig(index) || isStocking) return false;
+        if (!getSyncHandler().hasConfig(index) || isStocking) return false;
 
         long newStackSize = getSyncHandler().getConfigAmount(index);
 