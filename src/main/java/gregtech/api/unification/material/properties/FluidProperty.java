96b0dccfb3a2d22c3ae7a04af1f4d7e8a1964bf8
Zorbatron <46525467+Zorbatron@users.noreply.github.com>
2025-06-17 21:54:41 -0400

    Make all material property setters return themselves to allow method chaining (#2820)

--- src/main/java/gregtech/api/unification/material/properties/FluidProperty.java
+++ src/main/java/gregtech/api/unification/material/properties/FluidProperty.java
@@ -81,8 +81,9 @@ public class FluidProperty implements IMaterialProperty, FluidStorage {
     /**
      * @param primaryKey the key to use primarily
      */
-    public void setPrimaryKey(@NotNull FluidStorageKey primaryKey) {
+    public FluidProperty setPrimaryKey(@NotNull FluidStorageKey primaryKey) {
         this.primaryKey = primaryKey;
+        return this;
     }
 
     @Override
@@ -116,7 +117,8 @@ public class FluidProperty implements IMaterialProperty, FluidStorage {
      * @param solidifyingFluid The Fluid which solidifies into the material. If left null, it will be left as the
      *                         default value: the material's liquid.
      */
-    public void setSolidifyingFluid(@Nullable Fluid solidifyingFluid) {
+    public FluidProperty setSolidifyingFluid(@Nullable Fluid solidifyingFluid) {
         this.solidifyingFluid = solidifyingFluid;
+        return this;
     }
 }
