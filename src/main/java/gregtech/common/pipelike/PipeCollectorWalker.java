b0b87677b47f8d89d80d750cb02f0b3d5fda4872
Zorbatron <46525467+Zorbatron@users.noreply.github.com>
2025-08-05 14:41:40 -0400

    No need to null check here

--- src/main/java/gregtech/common/pipelike/PipeCollectorWalker.java
+++ src/main/java/gregtech/common/pipelike/PipeCollectorWalker.java
@@ -18,8 +18,7 @@ public class PipeCollectorWalker<T extends IPipeTile<?, ?>> extends PipeNetWalke
     public static void collectPipeNet(@NotNull World world, @NotNull BlockPos sourcePipe, @NotNull IPipeTile<?, ?> pipe,
                                       @NotNull Predicate<IPipeTile<?, ?>> pipeFunction) {
         PipeCollectorWalker<? extends IPipeTile<?, ?>> walker = (PipeCollectorWalker<? extends IPipeTile<?, ?>>) new PipeCollectorWalker<>(
-                world, sourcePipe, 0, pipe.getClass());
-        walker.pipeFunction = pipeFunction;
+                world, sourcePipe, 0, pipe.getClass(), pipeFunction);
         walker.traversePipeNet();
     }
 
@@ -30,28 +29,31 @@ public class PipeCollectorWalker<T extends IPipeTile<?, ?>> extends PipeNetWalke
      * Function to run on every pipe
      * If false is returned then halt the walker
      */
-    private Predicate<IPipeTile<?, ?>> pipeFunction;
+    @NotNull
+    private final Predicate<IPipeTile<?, ?>> pipeFunction;
 
     private BlockPos sourcePipe;
 
-    protected PipeCollectorWalker(World world, BlockPos sourcePipe, int walkedBlocks, Class<T> basePipeClass) {
+    protected PipeCollectorWalker(@NotNull World world, @NotNull BlockPos sourcePipe, int walkedBlocks,
+                                  @NotNull Class<T> basePipeClass, @NotNull Predicate<IPipeTile<?, ?>> pipeFunction) {
         super(world, sourcePipe, walkedBlocks);
         this.sourcePipe = sourcePipe;
         this.basePipeClass = basePipeClass;
+        this.pipeFunction = pipeFunction;
     }
 
     @Override
     protected PipeNetWalker<T> createSubWalker(World world, EnumFacing facingToNextPos, BlockPos nextPos,
                                                int walkedBlocks) {
-        PipeCollectorWalker<T> walker = new PipeCollectorWalker<>(world, nextPos, walkedBlocks, this.basePipeClass);
-        walker.pipeFunction = pipeFunction;
+        PipeCollectorWalker<T> walker = new PipeCollectorWalker<>(world, nextPos, walkedBlocks, this.basePipeClass,
+                pipeFunction);
         walker.sourcePipe = sourcePipe;
         return walker;
     }
 
     @Override
     protected void checkPipe(T pipeTile, BlockPos pos) {
-        if (this.pipeFunction != null && !this.pipeFunction.test(pipeTile)) {
+        if (!this.pipeFunction.test(pipeTile)) {
             this.root.stop();
         }
     }
