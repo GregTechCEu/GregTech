ab86a2549b81a7a5e8a66e721b5309a2da1a862a
Zorbatron <46525467+Zorbatron@users.noreply.github.com>
2025-08-26 00:03:13 -0400

    Instantiation of utility class

--- src/main/java/gregtech/common/pipelike/laser/LaserPipeProperties.java
+++ src/main/java/gregtech/common/pipelike/laser/LaserPipeProperties.java
@@ -2,5 +2,6 @@ package gregtech.common.pipelike.laser;
 
 public class LaserPipeProperties {
 
+    @SuppressWarnings("InstantiationOfUtilityClass")
     public static final LaserPipeProperties INSTANCE = new LaserPipeProperties();
 }
--- src/main/java/gregtech/common/pipelike/optical/OpticalPipeProperties.java
+++ src/main/java/gregtech/common/pipelike/optical/OpticalPipeProperties.java
@@ -2,5 +2,6 @@ package gregtech.common.pipelike.optical;
 
 public class OpticalPipeProperties {
 
+    @SuppressWarnings("InstantiationOfUtilityClass")
     public static final OpticalPipeProperties INSTANCE = new OpticalPipeProperties();
 }
