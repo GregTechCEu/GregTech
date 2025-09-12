4ff21f852a4126ff9888d9cf5635d09d50f49e81
Zorbatron <46525467+Zorbatron@users.noreply.github.com>
2025-09-12 10:08:02 -0400

    Add overload and javadocs for aforementioned extracted method

--- src/main/java/gregtech/api/util/FTBChunksUtil.java
+++ src/main/java/gregtech/api/util/FTBChunksUtil.java
@@ -11,16 +11,37 @@ import com.feed_the_beast.ftbutilities.data.ClaimedChunk;
 import com.feed_the_beast.ftbutilities.data.ClaimedChunks;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.UUID;
+
 public class FTBChunksUtil {
 
+    /**
+     * Check if a {@link BlockPos} is modifiable by a player abiding by FTB chunk rules, if this block is in a claimed
+     * chunk. <br/>
+     * Will always return {@code true} if
+     * <a href="https://www.curseforge.com/minecraft/mc-mods/ftb-utilities-forge">FTBUtilities</a> isn't present or the
+     * {@link BlockPos} isn't in a claimed chunk.
+     */
     public static boolean isBlockModifiableByPlayer(@NotNull World world, @NotNull BlockPos blockPos,
                                                     @NotNull EntityPlayer player) {
+        return isBlockModifiableByPlayer(world, blockPos, player.getUniqueID());
+    }
+
+    /**
+     * Check if a {@link BlockPos} is modifiable by a player abiding by FTB chunk rules, if this block is in a claimed
+     * chunk. <br/>
+     * Will always return {@code true} if
+     * <a href="https://www.curseforge.com/minecraft/mc-mods/ftb-utilities-forge">FTBUtilities</a> isn't present or the
+     * {@link BlockPos} isn't in a claimed chunk.
+     */
+    public static boolean isBlockModifiableByPlayer(@NotNull World world, @NotNull BlockPos blockPos,
+                                                    @NotNull UUID playerID) {
         if (!Mods.FTB_UTILITIES.isModLoaded()) return true;
 
         ClaimedChunks instance = ClaimedChunks.instance;
         ClaimedChunk claimedChunk = instance.getChunk(new ChunkDimPos(blockPos, world.provider.getDimension()));
         if (claimedChunk != null) {
-            ForgePlayer forgePlayer = instance.universe.getPlayer(player.getUniqueID());
+            ForgePlayer forgePlayer = instance.universe.getPlayer(playerID);
             EnumTeamStatus status = claimedChunk.getData().getEditBlocksStatus();
             return !claimedChunk.getTeam().hasStatus(forgePlayer, status);
         }
