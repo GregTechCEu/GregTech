8822d558a7c3ce26bdf12a5680716b5f39e7b73f
Tian mi <35869948+MCTian-mi@users.noreply.github.com>
2025-09-15 23:49:02 +0800

    Impl `Block#getItem` for BlockPipe (#2852)

--- src/main/java/gregtech/api/pipenet/block/BlockPipe.java
+++ src/main/java/gregtech/api/pipenet/block/BlockPipe.java
@@ -134,6 +134,15 @@ public abstract class BlockPipe<PipeType extends Enum<PipeType> & IPipeType<Node
 
     public abstract ItemStack getDropItem(IPipeTile<PipeType, NodeDataType> pipeTile);
 
+    @NotNull
+    @Override
+    @SuppressWarnings({ "deprecation", "unchecked" })
+    public ItemStack getItem(@NotNull World world, @NotNull BlockPos pos, @NotNull IBlockState state) {
+        var te = world.getTileEntity(pos);
+        if (!(te instanceof IPipeTile<?, ?>pipeTile)) return ItemStack.EMPTY;
+        return getDropItem((IPipeTile<PipeType, NodeDataType>) pipeTile);
+    }
+
     protected abstract NodeDataType getFallbackType();
 
     // TODO this has no reason to need an ItemStack parameter
