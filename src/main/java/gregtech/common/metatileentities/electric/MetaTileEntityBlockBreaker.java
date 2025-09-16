45a2956ade6c544acc8ea3270e48fe5005f1e6df
Zorbatron <46525467+Zorbatron@users.noreply.github.com>
2025-09-16 13:29:55 -0400

    Block breaker improvements (#2778)

--- src/main/java/gregtech/common/metatileentities/electric/MetaTileEntityBlockBreaker.java
+++ src/main/java/gregtech/common/metatileentities/electric/MetaTileEntityBlockBreaker.java
@@ -9,11 +9,15 @@ import gregtech.api.mui.GTGuis;
 import gregtech.api.util.BlockUtility;
 import gregtech.api.util.GTTransferUtils;
 import gregtech.api.util.GregFakePlayer;
+import gregtech.api.util.Mods;
+import gregtech.api.util.function.TriPredicate;
 import gregtech.client.renderer.texture.Textures;
 import gregtech.client.utils.RenderUtil;
+import gregtech.integration.ftb.utility.FTBChunksHelper;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
+import net.minecraft.block.BlockLiquid;
+import net.minecraft.block.BlockSkull;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.item.EntityItem;
@@ -29,6 +33,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
+import net.minecraftforge.common.util.FakePlayer;
 import net.minecraftforge.items.IItemHandlerModifiable;
 
 import codechicken.lib.raytracer.CuboidRayTraceResult;
@@ -44,6 +49,8 @@ import com.cleanroommc.modularui.value.sync.SyncHandlers;
 import com.cleanroommc.modularui.widgets.ItemSlot;
 import com.cleanroommc.modularui.widgets.SlotGroupWidget;
 import com.cleanroommc.modularui.widgets.layout.Grid;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
@@ -57,6 +64,32 @@ public class MetaTileEntityBlockBreaker extends TieredMetaTileEntity {
     private EnumFacing outputFacing;
     private int breakProgressTicksLeft;
     private float currentBlockHardness;
+    private static final List<@NotNull TriPredicate<@NotNull World, @NotNull BlockPos, @NotNull FakePlayer>> PREDICATE_LIST = new ObjectArrayList<>(
+            3);
+
+    static {
+        PREDICATE_LIST.add((world, blockPos, fakePlayer) -> !world.isAirBlock(blockPos));
+        PREDICATE_LIST.add(
+                (world, blockPos, fakePlayer) -> !(world.getBlockState(blockPos).getBlock() instanceof BlockLiquid));
+        PREDICATE_LIST.add((world, blockPos, fakePlayer) -> world.isBlockModifiable(fakePlayer, blockPos));
+        if (Mods.FTB_UTILITIES.isModLoaded()) {
+            PREDICATE_LIST.add(FTBChunksHelper::isBlockModifiableByPlayer);
+        }
+    }
+
+    /**
+     * Add a predicate check to the block breaker. Intended to be used to prevent a certain block from being broken.
+     * <br/>
+     * <b>Warning!</b> the {@link FakePlayer} passed to the predicate is not a real player, but a fake one with
+     * the same UUID as the real player who placed the block breaker. <br/>
+     * Return {@code false} to cancel a break attempt on this block. <br/>
+     * Return {@code true} to move onto the next predicate, and eventually break the block only if all other predicates
+     * returned {@code false}.
+     */
+    @SuppressWarnings("unused")
+    public static void registerBlockBreakerPredicate(@NotNull TriPredicate<@NotNull World, @NotNull BlockPos, @NotNull FakePlayer> predicate) {
+        PREDICATE_LIST.add(predicate);
+    }
 
     public MetaTileEntityBlockBreaker(ResourceLocation metaTileEntityId, int tier) {
         super(metaTileEntityId, tier);
@@ -80,72 +113,94 @@ public class MetaTileEntityBlockBreaker extends TieredMetaTileEntity {
     @Override
     public void update() {
         super.update();
-        if (!getWorld().isRemote && getOffsetTimer() % 5 == 0) {
-            pushItemsIntoNearbyHandlers(getOutputFacing());
-        }
-        if (!getWorld().isRemote) {
-            if (breakProgressTicksLeft > 0) {
-                --this.breakProgressTicksLeft;
-                if (breakProgressTicksLeft == 0 && energyContainer.getEnergyStored() >= getEnergyPerBlockBreak()) {
-                    BlockPos blockPos = getPos().offset(getFrontFacing());
-                    IBlockState blockState = getWorld().getBlockState(blockPos);
-                    EntityPlayer entityPlayer = GregFakePlayer.get((WorldServer) getWorld());
-                    float hardness = blockState.getBlockHardness(getWorld(), blockPos);
 
-                    if (hardness >= 0.0f && getWorld().isBlockModifiable(entityPlayer, blockPos) &&
-                            Math.abs(hardness - currentBlockHardness) < 0.5f) {
-                        List<ItemStack> drops = attemptBreakBlockAndObtainDrops(blockPos, blockState, entityPlayer);
-                        addToInventoryOrDropItems(drops);
-                    }
-                    this.breakProgressTicksLeft = 0;
-                    this.currentBlockHardness = 0.0f;
-                    energyContainer.removeEnergy(getEnergyPerBlockBreak());
-                }
-            }
+        World world = getWorld();
+        if (!world.isRemote) {
+            tryBreakBlock(world);
 
-            if (breakProgressTicksLeft == 0 && isBlockRedstonePowered()) {
-                BlockPos blockPos = getPos().offset(getFrontFacing());
-                IBlockState blockState = getWorld().getBlockState(blockPos);
-                EntityPlayer entityPlayer = GregFakePlayer.get((WorldServer) getWorld());
-                float hardness = blockState.getBlockHardness(getWorld(), blockPos);
-                boolean skipBlock = blockState.getMaterial() == Material.AIR ||
-                        blockState.getBlock().isAir(blockState, getWorld(), getPos());
-                if (hardness >= 0.0f && !skipBlock && getWorld().isBlockModifiable(entityPlayer, blockPos)) {
-                    this.breakProgressTicksLeft = getTicksPerBlockBreak(hardness);
-                    this.currentBlockHardness = hardness;
-                }
+            if (getOffsetTimer() % 5 == 0) {
+                pushItemsIntoNearbyHandlers(getOutputFacing());
             }
         }
     }
 
-    private void addToInventoryOrDropItems(List<ItemStack> drops) {
-        EnumFacing outputFacing = getOutputFacing();
-        double itemSpawnX = getPos().getX() + 0.5 + outputFacing.getXOffset();
-        double itemSpawnY = getPos().getY() + 0.5 + outputFacing.getYOffset();
-        double itemSpawnZ = getPos().getZ() + 0.5 + outputFacing.getZOffset();
+    protected void tryBreakBlock(@NotNull World world) {
+        BlockPos selfPos = getPos();
+        BlockPos lookingAtPos = selfPos.offset(getFrontFacing());
+        FakePlayer fakePlayer = GregFakePlayer.get((WorldServer) world, getOwner());
+
+        for (TriPredicate<World, BlockPos, FakePlayer> predicate : PREDICATE_LIST) {
+            if (!predicate.test(world, lookingAtPos, fakePlayer)) {
+                return;
+            }
+        }
+
+        IBlockState blockState = world.getBlockState(lookingAtPos);
+        if (breakProgressTicksLeft > 0 && --breakProgressTicksLeft == 0 &&
+                energyContainer.getEnergyStored() >= getEnergyPerBlockBreak()) {
+            float hardness = blockState.getBlockHardness(world, lookingAtPos);
+            if (hardness >= 0.0f && Math.abs(hardness - currentBlockHardness) < 0.5f) {
+                List<ItemStack> drops = attemptBreakBlockAndObtainDrops(world, lookingAtPos, blockState, fakePlayer);
+                addToInventoryOrDropItems(drops, world, getFrontFacing());
+            }
+
+            currentBlockHardness = 0.0f;
+            energyContainer.removeEnergy(getEnergyPerBlockBreak());
+        }
+
+        if (breakProgressTicksLeft == 0 && isBlockRedstonePowered()) {
+            float hardness = blockState.getBlockHardness(world, lookingAtPos);
+            if (hardness >= 0.0f) {
+                breakProgressTicksLeft = getTicksPerBlockBreak(hardness);
+                currentBlockHardness = hardness;
+            }
+        }
+    }
+
+    protected void addToInventoryOrDropItems(@NotNull List<ItemStack> drops, @NotNull World world,
+                                             @NotNull EnumFacing frontFacing) {
+        double itemSpawnX = getPos().getX() + 0.5 + frontFacing.getXOffset();
+        double itemSpawnY = getPos().getY() + 0.5 + frontFacing.getYOffset();
+        double itemSpawnZ = getPos().getZ() + 0.5 + frontFacing.getZOffset();
         for (ItemStack itemStack : drops) {
             ItemStack remainStack = GTTransferUtils.insertItem(exportItems, itemStack, false);
             if (!remainStack.isEmpty()) {
-                EntityItem entityitem = new EntityItem(getWorld(), itemSpawnX, itemSpawnY, itemSpawnZ, remainStack);
+                EntityItem entityitem = new EntityItem(world, itemSpawnX, itemSpawnY, itemSpawnZ, remainStack);
                 entityitem.setDefaultPickupDelay();
-                getWorld().spawnEntity(entityitem);
+                world.spawnEntity(entityitem);
             }
         }
     }
 
-    private List<ItemStack> attemptBreakBlockAndObtainDrops(BlockPos blockPos, IBlockState blockState,
-                                                            EntityPlayer entityPlayer) {
-        TileEntity tileEntity = getWorld().getTileEntity(blockPos);
-        boolean result = blockState.getBlock().removedByPlayer(blockState, getWorld(), blockPos, entityPlayer, true);
-        if (result) {
-            getWorld().playEvent(null, 2001, blockPos, Block.getStateId(blockState));
-            blockState.getBlock().onPlayerDestroy(getWorld(), blockPos, blockState);
-
+    protected @NotNull List<ItemStack> attemptBreakBlockAndObtainDrops(@NotNull World world,
+                                                                       @NotNull BlockPos lookingAtPos,
+                                                                       @NotNull IBlockState blockState,
+                                                                       @NotNull EntityPlayer entityPlayer) {
+        TileEntity tileEntity = world.getTileEntity(lookingAtPos);
+        Block block = blockState.getBlock();
+        if (block instanceof BlockSkull) {
             BlockUtility.startCaptureDrops();
-            blockState.getBlock().harvestBlock(getWorld(), entityPlayer, blockPos, blockState, tileEntity,
-                    ItemStack.EMPTY);
-            return BlockUtility.stopCaptureDrops();
+            boolean result = block.removedByPlayer(blockState, world, lookingAtPos, entityPlayer, true);
+            List<ItemStack> drops = BlockUtility.stopCaptureDrops();
+
+            if (result) {
+                world.playEvent(null, 2001, lookingAtPos, Block.getStateId(blockState));
+                block.onPlayerDestroy(world, lookingAtPos, blockState);
+
+                block.harvestBlock(world, entityPlayer, lookingAtPos, blockState, tileEntity, ItemStack.EMPTY);
+                return drops;
+            }
+        } else {
+            if (block.removedByPlayer(blockState, world, lookingAtPos, entityPlayer, true)) {
+                world.playEvent(null, 2001, lookingAtPos, Block.getStateId(blockState));
+                block.onPlayerDestroy(world, lookingAtPos, blockState);
+
+                BlockUtility.startCaptureDrops();
+                block.harvestBlock(world, entityPlayer, lookingAtPos, blockState, tileEntity, ItemStack.EMPTY);
+                return BlockUtility.stopCaptureDrops();
+            }
         }
+
         return Collections.emptyList();
     }
 
@@ -179,19 +234,19 @@ public class MetaTileEntityBlockBreaker extends TieredMetaTileEntity {
     }
 
     @Override
-    public void writeInitialSyncData(PacketBuffer buf) {
+    public void writeInitialSyncData(@NotNull PacketBuffer buf) {
         super.writeInitialSyncData(buf);
         buf.writeByte(getOutputFacing().getIndex());
     }
 
     @Override
-    public void receiveInitialSyncData(PacketBuffer buf) {
+    public void receiveInitialSyncData(@NotNull PacketBuffer buf) {
         super.receiveInitialSyncData(buf);
         this.outputFacing = EnumFacing.VALUES[buf.readByte()];
     }
 
     @Override
-    public void receiveCustomData(int dataId, PacketBuffer buf) {
+    public void receiveCustomData(int dataId, @NotNull PacketBuffer buf) {
         super.receiveCustomData(dataId, buf);
         if (dataId == UPDATE_OUTPUT_FACING) {
             this.outputFacing = EnumFacing.VALUES[buf.readByte()];
@@ -203,14 +258,15 @@ public class MetaTileEntityBlockBreaker extends TieredMetaTileEntity {
     public boolean isValidFrontFacing(EnumFacing facing) {
         // use direct outputFacing field instead of getter method because otherwise
         // it will just return SOUTH for null output facing
-        return super.isValidFrontFacing(facing) && facing != outputFacing;
+        return facing != outputFacing;
     }
 
+    @NotNull
     public EnumFacing getOutputFacing() {
         return outputFacing == null ? EnumFacing.SOUTH : outputFacing;
     }
 
-    public void setOutputFacing(EnumFacing outputFacing) {
+    public void setOutputFacing(@NotNull EnumFacing outputFacing) {
         this.outputFacing = outputFacing;
         if (!getWorld().isRemote) {
             notifyBlockUpdate();
@@ -220,32 +276,38 @@ public class MetaTileEntityBlockBreaker extends TieredMetaTileEntity {
     }
 
     @Override
-    public void setFrontFacing(EnumFacing frontFacing) {
+    public void setFrontFacing(@NotNull EnumFacing frontFacing) {
         super.setFrontFacing(frontFacing);
-        if (this.outputFacing == null) {
-            // set initial output facing as opposite to front
-            setOutputFacing(frontFacing.getOpposite());
-        }
+        // Set initial output facing as opposite to front
+        setOutputFacing(frontFacing.getOpposite());
     }
 
-    private int getEnergyPerBlockBreak() {
+    public int getEnergyPerBlockBreak() {
         return (int) GTValues.V[getTier()];
     }
 
-    private int getInventorySize() {
+    protected int getInventorySize() {
         int sizeRoot = (1 + getTier());
         return sizeRoot * sizeRoot;
     }
 
-    private int getTicksPerBlockBreak(float blockHardness) {
+    public int getTicksPerBlockBreak(float blockHardness) {
         int ticksPerOneDurability = 5;
         int totalTicksPerBlock = (int) Math.ceil(ticksPerOneDurability * blockHardness);
         float efficiencyMultiplier = 1.0f - getEfficiencyMultiplier();
         return (int) Math.ceil(totalTicksPerBlock * efficiencyMultiplier);
     }
 
-    private float getEfficiencyMultiplier() {
+    public float getEfficiencyMultiplier() {
         return 1.0f - MathHelper.clamp(1.0f - 0.2f * (getTier() - 1.0f), 0.1f, 1.0f);
+    }
+
+    public int getBreakProgressTicksLeft() {
+        return breakProgressTicksLeft;
+    }
+
+    public float getCurrentBlockHardness() {
+        return currentBlockHardness;
     }
 
     @Override
@@ -290,7 +352,8 @@ public class MetaTileEntityBlockBreaker extends TieredMetaTileEntity {
     }
 
     @Override
-    public void addInformation(ItemStack stack, @Nullable World player, List<String> tooltip, boolean advanced) {
+    public void addInformation(ItemStack stack, @Nullable World player, @NotNull List<String> tooltip,
+                               boolean advanced) {
         super.addInformation(stack, player, tooltip, advanced);
         tooltip.add(I18n.format("gregtech.machine.block_breaker.tooltip"));
         tooltip.add(I18n.format("gregtech.universal.tooltip.uses_per_op", getEnergyPerBlockBreak()));
