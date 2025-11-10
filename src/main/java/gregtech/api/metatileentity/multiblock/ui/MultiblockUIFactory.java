00c40e838f350fdf529d71516f365a89bf2c09f5
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2025-11-09 21:19:26 -0700

    fix lines having too much space

--- src/main/java/gregtech/api/metatileentity/multiblock/ui/MultiblockUIFactory.java
+++ src/main/java/gregtech/api/metatileentity/multiblock/ui/MultiblockUIFactory.java
@@ -360,7 +360,7 @@ public class MultiblockUIFactory {
                     .debugName("display_text")
                     .sizeRel(1f)
                     .alignment(Alignment.TopLeft)
-                    .margin(4, 4)
+                    .margin(4)
                     .autoUpdate(true)
                     .textBuilder(display::build));
         }
