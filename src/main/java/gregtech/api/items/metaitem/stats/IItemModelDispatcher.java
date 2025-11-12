1a662d91573e8a97943af74e5ffe6e75599f5de8
MCTian-mi <35869948+MCTian-mi@users.noreply.github.com>
2025-11-12 17:16:46 +0800

    format: use html javadoc

--- src/main/java/gregtech/api/items/metaitem/stats/IItemModelDispatcher.java
+++ src/main/java/gregtech/api/items/metaitem/stats/IItemModelDispatcher.java
@@ -1,17 +1,22 @@
 package gregtech.api.items.metaitem.stats;
 
+import gregtech.api.items.metaitem.MetaItem;
+
 import net.minecraft.item.ItemStack;
 
 @FunctionalInterface
 public interface IItemModelDispatcher extends IItemComponent {
 
-    /// Get the model index for the given item stack.
-    /// The index range will be checked at [gregtech.api.items.metaitem.MetaItem#getModelIndex(ItemStack)]
-    ///
-    /// @param itemStack The specific item stack.
-    /// @param maxIndex The max model index, from [gregtech.api.items.metaitem.MetaItem.MetaValueItem#getModelAmount()]`
-    /// - 1`
-    /// @return The model index for the specific stack, should be ranged between `0` (inclusive) and `maxIndex`
-    /// (inclusive).
+    /**
+     * Get the model index for the given item stack. <br>
+     * The index range will be checked at
+     * {@link MetaItem#getModelIndex(ItemStack)}.
+     *
+     * @param itemStack The specific item stack.
+     * @param maxIndex  The max model index, from
+     *                  {@link MetaItem.MetaValueItem#getModelAmount()} - 1.
+     * @return The model index for the specific stack, should be ranged between
+     *         {@code 0} (inclusive) and {@code maxIndex} (inclusive).
+     */
     int getModelIndex(ItemStack itemStack, int maxIndex);
 }
