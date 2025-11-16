c2a83bab3dce3dd73600f0937681baa539bdd4eb
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2025-11-16 00:22:34 -0700

    address potential `/ by 0` rename `r` to `factor`

--- src/main/java/gregtech/common/metatileentities/multi/electric/MetaTileEntityPowerSubstation.java
+++ src/main/java/gregtech/common/metatileentities/multi/electric/MetaTileEntityPowerSubstation.java
@@ -589,10 +589,10 @@ public class MetaTileEntityPowerSubstation extends MultiblockWithDisplayBase
         bars.add(b -> b.progress(() -> {
             BigInteger capacity = energyCapacityValue.getValue();
             BigInteger stored = energyStoredValue.getValue();
-            if (capacity.compareTo(stored) < 0) return 0;
-            double r = capacity.divide(stored).doubleValue();
-            if (r == 0) return 0;
-            return 1 / r;
+            if (stored.equals(BigInteger.ZERO)) return 0;
+            double factor = capacity.divide(stored).doubleValue();
+            if (factor == 0) return 0;
+            return 1 / factor;
         })
                 .texture(GTGuiTextures.PROGRESS_BAR_MULTI_ENERGY_YELLOW)
                 .tooltipBuilder(t -> {
