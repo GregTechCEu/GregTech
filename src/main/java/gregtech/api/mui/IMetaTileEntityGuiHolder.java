73e1f473a78d5de3ca9d330c013e1bf34a3a1082
Zorbatron <46525467+Zorbatron@users.noreply.github.com>
2025-11-29 00:15:54 -0500

    Add descriptive javadoc to IMetaTileEntityGuiHolder

--- src/main/java/gregtech/api/mui/IMetaTileEntityGuiHolder.java
+++ src/main/java/gregtech/api/mui/IMetaTileEntityGuiHolder.java
@@ -9,10 +9,15 @@ import net.minecraft.util.EnumHand;
 
 import codechicken.lib.raytracer.CuboidRayTraceResult;
 import com.cleanroommc.modularui.api.IGuiHolder;
+import com.cleanroommc.modularui.api.value.IStringValue;
 import com.cleanroommc.modularui.screen.ModularPanel;
 import com.cleanroommc.modularui.screen.ModularScreen;
 import com.cleanroommc.modularui.screen.UISettings;
+import com.cleanroommc.modularui.value.sync.InteractionSyncHandler;
 import com.cleanroommc.modularui.value.sync.PanelSyncManager;
+import com.cleanroommc.modularui.value.sync.SyncHandler;
+import com.cleanroommc.modularui.widgets.ButtonWidget;
+import com.cleanroommc.modularui.widgets.textfield.TextFieldWidget;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
@@ -37,6 +42,17 @@ public interface IMetaTileEntityGuiHolder extends IGuiHolder<MetaTileEntityGuiDa
         return true;
     }
 
+    /**
+     * Build and return a {@link ModularPanel} with widgets to display to the player.
+     * 
+     * @param data        Contains data about how the {@link MetaTileEntity} was opened, like the player, position of
+     *                    the MTE, and extra data from the server written in {@link #writeExtraGuiData(PacketBuffer)}.
+     * @param syncManager the sync manager for this panel. Is used to register extra {@link SyncHandler}s not used in
+     *                    methods that automatically register the sync handler like
+     *                    {@link ButtonWidget#syncHandler(InteractionSyncHandler)} or
+     *                    {@link TextFieldWidget#value(IStringValue)}.
+     * @param settings    contains miscellaneous settings related to this UI.
+     */
     @NotNull
     @Override
     ModularPanel buildUI(MetaTileEntityGuiData data, PanelSyncManager syncManager, UISettings settings);
